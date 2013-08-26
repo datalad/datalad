387679de33138ba0aca1e05671c67c0531358d18
Yaroslav Halchenko <debian@onerussian.com>
2013-08-26 15:46:32 -0400

    added helpers and common_args from testkraut/cmdline release/0.0.1-133-g79c163a

---
+++ datagit/cmdline/common_args.py
@@ -0,0 +1,35 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the testkraut package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+""""""
+
+__docformat__ = 'restructuredtext'
+
+# argument spec template
+#<name> = (
+#    <id_as_positional>, <id_as_option>
+#    {<ArgusmentParser.add_arguments_kwargs>}
+#)
+
+from ..utils import get_filecache_dir
+from ..cmdline.helpers import HelpAction
+
+help = (
+    'help', ('-h', '--help', '--help-np'),
+    dict(nargs=0, action=HelpAction,
+         help="""show this help message and exit. --help-np forcefully disables
+                 the use of a pager for displaying the help.""")
+)
+
+version = (
+    'version', ('--version',),
+    dict(action='version',
+         help="show program's version and license information and exit")
+)
+
+
---
+++ datagit/cmdline/helpers.py
@@ -0,0 +1,77 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the testkraut package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+""""""
+
+__docformat__ = 'restructuredtext'
+
+import argparse
+import re
+import sys
+
+class HelpAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if option_string == '--help':
+            # lets use the manpage on mature systems ...
+            try:
+                import subprocess
+                subprocess.check_call(
+                        'man %s 2> /dev/null' % parser.prog.replace(' ', '-'),
+                        shell=True)
+                sys.exit(0)
+            except (subprocess.CalledProcessError, OSError):
+                # ...but silently fall back if it doesn't work
+                pass
+        if option_string == '-h':
+            helpstr = "%s\n%s" \
+                    % (parser.format_usage(),
+                       "Use '--help' to get more comprehensive information.")
+        else:
+            helpstr = parser.format_help()
+        # better for help2man
+        helpstr = re.sub(r'optional arguments:', 'options:', helpstr)
+        helpstr = re.sub(r'positional arguments:\n.*\n', '', helpstr)
+        # convert all heading to have the first character uppercase
+        headpat = re.compile(r'^([a-z])(.*):$',  re.MULTILINE)
+        helpstr = re.subn(headpat,
+               lambda match: r'{0}{1}:'.format(match.group(1).upper(),
+                                             match.group(2)),
+               helpstr)[0]
+        # usage is on the same line
+        helpstr = re.sub(r'^usage:', 'Usage:', helpstr)
+        if option_string == '--help-np':
+            usagestr = re.split(r'\n\n[A-Z]+', helpstr, maxsplit=1)[0]
+            usage_length = len(usagestr)
+            usagestr = re.subn(r'\s+', ' ', usagestr.replace('\n', ' '))[0]
+            helpstr = '%s\n%s' % (usagestr, helpstr[usage_length:])
+        print helpstr
+        sys.exit(0)
+
+def parser_add_common_args(parser, pos=None, opt=None, **kwargs):
+    from . import common_args
+    for i, args in enumerate((pos, opt)):
+        if args is None:
+            continue
+        for arg in args:
+            arg_tmpl = getattr(common_args, arg)
+            arg_kwargs = arg_tmpl[2].copy()
+            arg_kwargs.update(kwargs)
+            if i:
+                parser.add_argument(*arg_tmpl[i], **arg_kwargs)
+            else:
+                parser.add_argument(arg_tmpl[i], **arg_kwargs)
+
+def parser_add_common_opt(parser, opt, names=None, **kwargs):
+    from . import common_args
+    opt_tmpl = getattr(common_args, opt)
+    opt_kwargs = opt_tmpl[2].copy()
+    opt_kwargs.update(kwargs)
+    if names is None:
+        parser.add_argument(*opt_tmpl[1], **opt_kwargs)
+    else:
+        parser.add_argument(*names, **opt_kwargs)
