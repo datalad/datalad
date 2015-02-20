9879a22cbcb3d5407d606db36dd3aaafbfa8c922
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-02-20 12:38:02 +0100

    Added logging and made use of the "Runner" to wrap system calls

--- datalad/support/annexrepo.py
+++ datalad/support/annexrepo.py
@@ -7,10 +7,10 @@ __author__ = 'Benjamin Poldrack'
 import os
 from os.path import join, exists
 
-
 from gitrepo import GitRepo
+import datalad.log
 
-from ..cmd import Runner
+from datalad.cmd import Runner
 
 
 class AnnexRepo(GitRepo):
@@ -29,11 +29,15 @@ class AnnexRepo(GitRepo):
         :param url: valid git url. See http://www.kernel.org/pub/software/scm/git/docs/git-clone.html#URLS
         :return:
         """
-
         super(AnnexRepo, self).__init__(path, url)
+
+        self.cmd_call_wrapper = Runner()
+        # TODO: Concept of when to set to "dry". Includes: What to do in gitrepo class?
+
 
         # Check whether an annex already exists at destination
         if not exists(join(self.path, '.git', 'annex')):
+            datalad.log.lgr.debug('No annex found in %s. Creating a new one ...' % self.path)
             self.annex_init()
 
     def annex_init(self):
@@ -46,8 +50,11 @@ class AnnexRepo(GitRepo):
         os.chdir(self.path)
         # TODO: change back afterwards?
 
-        os.system('git annex init')
-        # TODO: use the "Runner" for dry runs and getting output
+        status, output = self.cmd_call_wrapper.getstatusoutput('git annex init')
+        datalad.log.lgr.info('\"git annex init\" outputs:\n %s' % output)
+        if status != 0:
+            datalad.log.lgr.error('git annex init returned status %d.' % status)
+
 
     def dummy_annex_command(self):
         """
