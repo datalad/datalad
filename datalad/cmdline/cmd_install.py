e167b6b7110dcffccae5c1faf407ce4913ed2c9d
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-02-23 14:38:43 +0100

    - delete outcommented stuff 	- expand vars and user in path 	- correct order of args in doc

--- datalad/cmdline/cmd_install.py
+++ datalad/cmdline/cmd_install.py
@@ -10,7 +10,6 @@
 
 """
 
-# For some reason argparse manages to remove all the formatting for me here.
 """
 some more bits...
 
@@ -18,7 +17,7 @@ more TODO
 
 Examples:
 
-$ datalad install http://psydata.ovgu.de/forrest_gump/.git /foo/bar
+$ datalad install /foo/bar http://psydata.ovgu.de/forrest_gump/.git
 
 TODO: See TODO.org
 
@@ -47,56 +46,6 @@ from .helpers import parser_add_common_args
 
 def setup_parser(parser):
 
-    # common options
-#    parser.add_argument(
-#        "-a", "--addurl-opts", default="",
-#        help="Additional options to pass to 'git annex addurl', e.g. --fast")
-#    parser.add_argument(
-#        "-i", "--include", type=RegexpType(),
-#        help="Include links which match regular expression (by HREF). "
-#             "Otherwise all are considered")
-#    parser.add_argument(
-#        "-e", "--exclude", type=RegexpType(),
-#        help="Exclude links which match regular expression (by HREF)")
-#     parser.add_argument(
-#         "--use-a", action="store_true",
-#         help="Use name provided in the link, not filename given by the server")
-
-    # TODO: we might want few modes than just dry:
-    #   act [default] as now without dry-run
-    #   monitor -- the one like dry BUT which would collect all "interesting"
-    #              entries and if anything to be actually done -- spit them
-    #              out or email?
-    # parser.add_argument(
-    #     "-n", "--dry-run", action="store_true",
-    #     help="No git-annex is invoked, commands are only printed to the screen")
-    #
-    # parser.add_argument(
-    #     "--cache", action="store_true",
-    #     help="Either to cache fetching of pages and parsing out urls")
-    #
-    # parser.add_argument(
-    #     "--existing", choices=('check', 'skip'), default='check',
-    #     help="How to deal with files already known. 'skip' would entirely skip "
-    #     "file without checking if it was modified or not. 'check' would "
-    #     "proceed normally updating the file(s) if changed")
-
-#    parser.add_argument(
-#        "--tests", action="store_true",
-#        help="Do not do anything but run tests")
-#
-#     parser.add_argument(
-#         '-l', '--log-level',
-#         choices=['critical', 'error', 'warning', 'info', 'debug'] + [str(x) for x in range(1, 10)],
-#         default='warning',
-#         help="""level of verbosity. Integers provide even more debugging information""")
-# 
-#     if __debug__:
-#         # borrowed from Michael's bigmess
-#         parser.add_argument(
-#             '--dbg', action='store_true', dest='common_debug',
-#             help="do not catch exceptions and show exception traceback")
-
     parser.add_argument(
         "destination", metavar='dir',
         help="path where to store the retrieved dataset")
@@ -113,6 +62,7 @@ def run(args):
 
     lgr.debug("Command line arguments: %r" % args)
 
-    ds = Dataset(args.destination, args.source)
+    path = os.path.expandvars(os.path.expanduser(args.destination))
+    ds = Dataset(path, args.source)
 
 
