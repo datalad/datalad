96456a028ac73c29574045c72186cf96c74a5c26
Yaroslav Halchenko <debian@onerussian.com>
2015-02-26 12:01:12 -0500

    BF: emacs mode setting should now be python not python-mode
    have achieved by running
    
    git-sedi 'ode: python-mode' 'ode: python'
    
    where git-sedi is
    
    git-sedi () {
            git grep -l  | xargs sed -i -e "s///g"
    }

--- datalad/support/network.py
+++ datalad/support/network.py
@@ -1,4 +1,4 @@
-# emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
 # ex: set sts=4 ts=4 sw=4 noet:
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
--- datalad/tests/test_main.py
+++ datalad/tests/test_main.py
@@ -1,4 +1,4 @@
-#emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil; coding: utf-8 -*-
+#emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil; coding: utf-8 -*-
 #ex: set sts=4 ts=4 sw=4 noet:
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
