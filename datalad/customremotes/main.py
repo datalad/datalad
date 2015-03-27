e18dfd3a12966991622ceefea5a658f592018f9f
Yaroslav Halchenko <debian@onerussian.com>
2015-03-27 11:12:11 -0400

    RF: keep archive special remote functionality to its own file

--- datalad/customremotes/main.py
+++ datalad/customremotes/main.py
@@ -21,9 +21,10 @@ from datalad import log
 from datalad.log import lgr
 
 import datalad.cmdline as mvcmd
-from datalad.cmdline import helpers
-from datalad.cmdline.main import _license_info
+from ..cmdline import helpers
+from ..cmdline.main import _license_info
 
+from ..utils import setup_exceptionhook
 
 def setup_parser():
     # setup cmdline args parser
@@ -49,7 +50,7 @@ def setup_parser():
     if __debug__:
         parser.add_argument(
             '--dbg', action='store_true', dest='common_debug',
-            help="do not catch exceptions and show exception traceback")
+            help="Catch exceptions and fall into debugger upon exception")
 
     parser.add_argument(
         "command", nargs="?",
@@ -57,45 +58,53 @@ def setup_parser():
     )
     return parser
 
+def _main(args, backend=None):
+    """Unprotected portion"""
+    # TODO: For now we have only one, but we might need to actually become aware
+    # of multiple and provide multiple helpers/symlinks
+    # OR consider implementing ultimate handler of all dl+ URLs within a
+    # single beast -- probably wouldn't fly since we do might need different
+    # initializations etc.
+    assert(backend is not None)
+    if backend == "archive":
+        from .archive import AnnexArchiveCustomRemote
+        remote = AnnexArchiveCustomRemote()
+    else:
+        raise ValueError("I don't know anything about %r backend. "
+                         "Known are: %s" % backends)
+
+    # TODO: handle args -- not used ATM
+    # Generic commands to support:
+    #   get-uri-prefix
+    #   get-uri [options] with [options] being backend specific
+    #           e.g. for archive --key KEY(archive) --file FILE
+    command = args.command
+    if command == 'get-uri-prefix':
+        print remote.url_prefix
+    elif args.command is None:
+        # If no command - run the special remote
+        remote.main()
+    else:
+        raise ValueError("Unknown command %s" % command)
+
 def main(args=None, backend=None):
     import sys
-    # TODO: disable redirect lgr output to stderr if it is currently stdout
+    # TODO: redirect lgr output to stderr if it is stdout and not "forced"
+    # by env variable...
     #
     parser = setup_parser()
     # parse cmd args
     args = parser.parse_args(args)
-    # run the function associated with the selected command
-    try:
-        # TODO: For now we have only one, but we might need to actually become aware
-        # of multiple and provide multiple helpers/symlinks
-        # OR consider implementing ultimate handler of all dl+ URLs within a
-        # single beast -- probably wouldn't fly since we do might need different
-        # initializations etc.
-        assert(backend is not None)
-        if backend == "archive":
-            from datalad.customremotes.base import AnnexArchiveCustomRemote
-            remote = AnnexArchiveCustomRemote()
-        else:
-            raise ValueError("I don't know anything about %r backend. "
-                             "Known are: %s" % backends)
 
-        # TODO: handle args -- not used ATM
-        # Generic commands to support:
-        #   get-uri-prefix
-        #   get-uri [options] with [options] being backend specific
-        #           e.g. for archive --key KEY(archive) --file FILE
-        command = args.command
-        if command == 'get-uri-prefix':
-            print remote.url_prefix
-        elif args.command is None:
-            # If no command - run the special remote
-            remote.main()
-        else:
-            raise ValueError("Unknown command %s" % command)
-
-    except Exception as exc:
-        lgr.error('%s (%s)' % (str(exc), exc.__class__.__name__))
-        if args.common_debug:
-            import pdb
-            pdb.post_mortem()
-        sys.exit(1)
+    if args.common_debug:
+        # So we could see/stop clearly at the point of failure
+        setup_exceptionhook()
+        _main(args, backend)
+    else:
+        # Otherwise - guard and only log the summary. Postmortem is not
+        # as convenient if being caught in this ultimate except
+        try:
+            _main(args, backend)
+        except Exception as exc:
+            lgr.error('%s (%s)' % (str(exc), exc.__class__.__name__))
+            sys.exit(1)
