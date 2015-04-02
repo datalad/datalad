0c21e3853696acdaec1d81540d85a9a94294a013
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-04-02 09:12:35 +0200

    Reworked git tests

--- datalad/tests/test_gitrepo.py
+++ datalad/tests/test_gitrepo.py
@@ -19,7 +19,7 @@ from nose.tools import assert_raises, assert_is_instance, assert_true
 from git.exc import GitCommandError
 
 from datalad.support.gitrepo import GitRepo
-from datalad.tests.utils import with_tempfile, with_testrepos, assert_cwd_unchanged
+from datalad.tests.utils import with_tempfile, with_testrepos, assert_cwd_unchanged, ok_clean_git
 from datalad.cmd import Runner
 
 
@@ -60,20 +60,13 @@ def test_GitRepo_instance_brand_new(path):
 def test_GitRepo_add(path):
 
     gr = GitRepo(path)
-
-    cwd = os.getcwd()
-    os.chdir(path)
     filename = "test_git_add.dat"
-    f = open(filename, 'w')
+    f = open(os.path.join(path, filename), 'w')
     f.write("File to add to git")
     f.close()
-
     gr.git_add([filename])
-    runner = Runner()
-    st, out = runner.run(['git', 'ls-files'], return_output=True)
-    assert_true(out.__str__().find(filename) > -1, "%s not successfully added to %s" % (filename, path))
 
-    os.chdir(cwd)
+    assert_true(gr.get_indexed_files().__contains__(filename), "%s not successfully added to %s" % (filename, path))
 
 
 @assert_cwd_unchanged
@@ -81,21 +74,14 @@ def test_GitRepo_add(path):
 def test_GitRepo_commit(path):
 
     gr = GitRepo(path)
-
-    cwd = os.getcwd()
-    os.chdir(path)
     filename = "test_git_add.dat"
-    f = open(filename, 'w')
+    f = open(os.path.join(path, filename), 'w')
     f.write("File to add to git")
     f.close()
 
     gr.git_add([filename])
     gr.git_commit("Testing GitRepo.git_commit().")
-    runner = Runner()
-    st, out = runner.run(['git', 'status'], return_output=True)
-    assert_true(out.__str__().find("nothing to commit, working directory clean") > -1, "commit to %s failed." % path)
-
-    os.chdir(cwd)
+    ok_clean_git(path, annex=False, untracked=[])
 
 
 @with_testrepos(flavors=['local'])
