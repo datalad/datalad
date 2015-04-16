f9209e6ef99143034bfc0b4aacccbdbf9f81f821
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-04-16 12:13:46 +0200

    PEP8

--- datalad/tests/test_annexrepo.py
+++ datalad/tests/test_annexrepo.py
@@ -11,16 +11,18 @@
 """
 
 import os
-import platform
-from git.exc import GitCommandError
 
-from nose.tools import assert_raises, assert_is_instance, assert_true, assert_equal, assert_false, assert_in, assert_not_in
+from git.exc import GitCommandError
+from nose.tools import assert_raises, assert_is_instance, assert_true, \
+    assert_equal, assert_false, assert_in, assert_not_in
 
 from datalad.support.annexrepo import AnnexRepo, kwargs_to_options
-from datalad.tests.utils import with_tempfile, with_testrepos, assert_cwd_unchanged, ignore_nose_capturing_stdout, \
-    on_windows, ok_clean_git_annex_proxy, swallow_logs, swallow_outputs, in_, with_tree,\
+from datalad.tests.utils import with_tempfile, with_testrepos, \
+    assert_cwd_unchanged, ignore_nose_capturing_stdout, on_windows,\
+    ok_clean_git_annex_proxy, swallow_logs, swallow_outputs, in_, with_tree,\
     get_most_obscure_supported_name, ok_clean_git
-from datalad.support.exceptions import CommandNotAvailableError, FileInGitError, FileNotInAnnexError, CommandError
+from datalad.support.exceptions import CommandNotAvailableError,\
+    FileInGitError, FileNotInAnnexError, CommandError
 
 # For now (at least) we would need to clone from the network
 # since there are troubles with submodules on Windows.
@@ -38,17 +40,16 @@ def test_AnnexRepo_instance_from_clone(src, dst):
     assert_is_instance(ar, AnnexRepo, "AnnexRepo was not created.")
     assert_true(os.path.exists(os.path.join(dst, '.git', 'annex')))
 
-    # do it again should raise GitCommandError since git will notice there's already a git-repo at that path
-    # and therefore can't clone to `dst`
+    # do it again should raise GitCommandError since git will notice
+    # there's already a git-repo at that path and therefore can't clone to `dst`
     with swallow_logs() as cm:
         assert_raises(GitCommandError, AnnexRepo, dst, src)
         assert("already exists" in cm.out)
 
 
-# TODO: enable local as well whenever/if ever submodule issue gets resolved for windows
 @ignore_nose_capturing_stdout
 @assert_cwd_unchanged
-@with_testrepos(flavors=local_flavors)  # 'network' doesn't make sense for this test
+@with_testrepos(flavors=local_flavors)
 def test_AnnexRepo_instance_from_existing(path):
 
     ar = AnnexRepo(path)
@@ -81,21 +82,25 @@ def test_AnnexRepo_get(src, dst):
     assert_true(ar.file_has_content("test-annex.dat")[0][1])
 
     f = open(testfile_abs, 'r')
-    assert_equal(f.readlines(), ['123\n'], "test-annex.dat's content doesn't match.")
+    assert_equal(f.readlines(), ['123\n'],
+                 "test-annex.dat's content doesn't match.")
 
 
 @assert_cwd_unchanged
 @with_testrepos
 @with_tempfile
 def test_AnnexRepo_crippled_filesystem(src, dst):
-    # TODO: This test is rudimentary, since platform not really determines filesystem.
-    # For now this should work for the buildbots. Nevertheless: Find a better way to test it.
+    # TODO: This test is rudimentary, since platform does not really determine
+    # the filesystem. For now this should work for the buildbots.
+    # Nevertheless: Find a better way to test it.
 
     ar = AnnexRepo(dst, src)
     if on_windows:
-        assert_true(ar.is_crippled_fs(), "Detected non-crippled filesystem on windows.")
+        assert_true(ar.is_crippled_fs(),
+                    "Detected non-crippled filesystem on windows.")
     else:
-        assert_false(ar.is_crippled_fs(), "Detected crippled filesystem on non-windows.")
+        assert_false(ar.is_crippled_fs(),
+                     "Detected crippled filesystem on non-windows.")
 
 
 @assert_cwd_unchanged
@@ -105,12 +110,16 @@ def test_AnnexRepo_is_direct_mode(path):
     ar = AnnexRepo(path)
     dm = ar.is_direct_mode()
     if on_windows:
-        assert_true(dm, "AnnexRepo.is_direct_mode() returned false on windows.")
+        assert_true(dm,
+                    "AnnexRepo.is_direct_mode() returned false on windows.")
     else:
-        assert_false(dm, "AnnexRepo.is_direct_mode() returned true on non-windows")
-    # Note: In fact this test isn't totally correct, since you always can switch to direct mode.
-    # So not being on windows doesn't necessarily mean we are in indirect mode. But how to obtain a "ground truth"
-    # to test against, without making test of is_direct_mode() dependent on set_direct_mode() and vice versa?
+        assert_false(dm,
+                     "AnnexRepo.is_direct_mode() returned true on non-windows")
+    # Note: In fact this test isn't totally correct, since you always can
+    # switch to direct mode. So not being on windows doesn't necessarily mean
+    # we are in indirect mode. But how to obtain a "ground truth" to test
+    # against, without making test of is_direct_mode() dependent on
+    # set_direct_mode() and vice versa?
 
 
 @assert_cwd_unchanged
@@ -123,7 +132,8 @@ def test_AnnexRepo_set_direct_mode(src, dst):
     assert_true(ar.is_direct_mode(), "Switching to direct mode failed.")
     if ar.is_crippled_fs():
         assert_raises(CommandNotAvailableError, ar.set_direct_mode, False)
-        assert_true(ar.is_direct_mode(), "Indirect mode on crippled fs detected. Shouldn't be possible.")
+        assert_true(ar.is_direct_mode(),
+            "Indirect mode on crippled fs detected. Shouldn't be possible.")
     else:
         ar.set_direct_mode(False)
         assert_false(ar.is_direct_mode(), "Switching to indirect mode failed.")
@@ -143,12 +153,15 @@ def test_AnnexRepo_annex_add(src, annex_path):
     f.close()
     ar.annex_add(filename)
     if not ar.is_direct_mode():
-        assert_true(os.path.islink(filename_abs), "Annexed file is not a link.")
+        assert_true(os.path.islink(filename_abs),
+                    "Annexed file is not a link.")
     else:
-        assert_false(os.path.islink(filename_abs), "Annexed file is link in direct mode.")
+        assert_false(os.path.islink(filename_abs),
+                     "Annexed file is link in direct mode.")
     key = ar.get_file_key(filename)
     assert_false(key == '')
-    # could test for the actual key, but if there's something and no exception raised, it's fine anyway.
+    # could test for the actual key, but if there's something
+    # and no exception raised, it's fine anyway.
 
 
 @assert_cwd_unchanged
@@ -169,7 +182,9 @@ def test_AnnexRepo_get_file_key(src, annex_path):
     ar = AnnexRepo(annex_path, src)
 
     # test-annex.dat should return the correct key:
-    assert_equal(ar.get_file_key("test-annex.dat"), 'SHA256E-s4--181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b.dat')
+    assert_equal(
+        ar.get_file_key("test-annex.dat"),
+        'SHA256E-s4--181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b.dat')
 
     # test.dat is actually in git
     # should raise Exception; also test for polymorphism
