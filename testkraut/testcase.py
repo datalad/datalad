3d273a8bfb7ce14a38f760c2b2167ff065f3dedf
Yaroslav Halchenko <debian@onerussian.com>
2015-04-20 10:30:17 -0400

    ENH: pep8 - some haders fixed up, require numpy in requirements, some spaces

--- testkraut/testcase.py
+++ testkraut/testcase.py
@@ -1,11 +1,11 @@
 # TKLIGHT: # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
 #   See COPYING file distributed along with the testkraut package for the
 #   copyright and license terms.
 #
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Unittest compliant API for testkraut test cases"""
 
 __docformat__ = 'restructuredtext'
