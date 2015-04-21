3d35693364bfa3df4ee09e2a7423b8cb26412b44
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-04-21 10:50:51 +0200

    Temp. change for debugging Travis failure

--- datalad/support/annexrepo.py
+++ datalad/support/annexrepo.py
@@ -238,7 +238,6 @@ class AnnexRepo(GitRepo):
         # TODO: When to expect stderr?
         # on crippled filesystem for example (think so)?
 
-
     @kwargs_to_options
     @normalize_paths
     def annex_get(self, files, options=[]):
@@ -256,8 +255,8 @@ class AnnexRepo(GitRepo):
 
         # don't capture stderr, since it provides progress display
         self._run_annex_command('get', annex_options=options + files,
-                                log_stdout=True, log_stderr=False,
-                                log_online=True, expect_stderr=True)
+                                log_stdout=True, log_stderr=True,
+                                log_online=True, expect_stderr=False)
 
     @normalize_paths
     def annex_add(self, files, backend=None, options=[]):
