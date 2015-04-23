644bac52aa5d8178b500b6b5183b6db3066b8d63
Yaroslav Halchenko <debian@onerussian.com>
2015-04-22 20:58:32 -0400

    ENH: minor pep8ing

--- datalad/tests/test_archives.py
+++ datalad/tests/test_archives.py
@@ -2,23 +2,23 @@
 #ex: set sts=4 ts=4 sw=4 noet:
 
 import os
-from os.path import join as opj, exists, lexists, isdir
+from os.path import join as opj, exists
 
 from .utils import assert_true, assert_false, eq_, \
-     with_tree, with_tempfile, swallow_outputs, on_windows
+    with_tree, with_tempfile, swallow_outputs, on_windows
 
 from ..support.archives import decompress_file, compress_files, unixify_path
 
 from .utils import get_most_obscure_supported_name, assert_raises
 
-fn_inarchive_obscure = get_most_obscure_supported_name()
-fn_archive_obscure = fn_inarchive_obscure.replace('a', 'b')
+fn_in_archive_obscure = get_most_obscure_supported_name()
+fn_archive_obscure = fn_in_archive_obscure.replace('a', 'b')
 fn_archive_obscure_ext = fn_archive_obscure + '.tar.gz'
 
 tree_simplearchive = dict(
     tree=(
         (fn_archive_obscure_ext, (
-            (fn_inarchive_obscure, '2 load'),
+            (fn_in_archive_obscure, '2 load'),
             ('3.txt', '3 load'))),),
     prefix='datalad-')
 
@@ -54,7 +54,7 @@ def check_decompress_file(leading_directories, path):
                                   % leading_directories)
 
     assert_true(exists(opj(testpath, '3.txt')))
-    assert_true(exists(opj(testpath, fn_inarchive_obscure)))
+    assert_true(exists(opj(testpath, fn_in_archive_obscure)))
     with open(opj(testpath, '3.txt')) as f:
         eq_(f.read(), '3 load')
 
@@ -69,20 +69,21 @@ def test_decompress_file():
             ('d1', (
                 ('d2', (
                     ('f1', 'f1 load'),
-                    ),
-                ),
+                    ),),
             ))))
 @with_tempfile()
 def check_compress_file(ext, path, name):
     archive = name + ext
-    compress_files([os.path.basename(path)], archive, path=os.path.dirname(path))
+    compress_files([os.path.basename(path)], archive,
+                   path=os.path.dirname(path))
     assert_true(exists(archive))
     name_extracted = name + "_extracted"
     decompress_file(archive, name_extracted, leading_directories='strip')
     assert_true(exists(opj(name_extracted, 'empty')))
     assert_true(exists(opj(name_extracted, 'd1', 'd2', 'f1')))
 
+
 def test_compress_file():
     yield check_compress_file, '.tar.gz'
     yield check_compress_file, '.tar'
-    #yield check_compress_file, '.zip'
+    # yield check_compress_file, '.zip'
