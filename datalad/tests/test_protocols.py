74a52b8ee504025ec9834a6162c634bd5d0ebcf0
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-05-05 16:25:00 +0200

    More tests.

--- datalad/tests/test_protocols.py
+++ datalad/tests/test_protocols.py
@@ -8,12 +8,13 @@
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Test command call protocols
 
-Note: DryRunProtocol is already (kind of) tested within test_cmd.py
+Note: DryRunProtocol and NullProtocol are already (kind of) tested within
+      test_cmd.py
 """
 
 import os
 from nose.tools import ok_, eq_, assert_is, assert_equal, assert_greater, \
-    assert_raises, assert_in, assert_is_instance
+    assert_raises, assert_in, assert_is_instance, assert_true, assert_false
 
 from ..support.protocol import DryRunProtocol, DryRunExternalsProtocol, \
     NullProtocol, ExecutionTimeProtocol, ExecutionTimeExternalsProtocol, \
@@ -21,6 +22,41 @@ from ..support.protocol import DryRunProtocol, DryRunExternalsProtocol, \
 from ..support.gitrepo import GitRepo
 from ..cmd import Runner
 from .utils import with_tempfile
+
+
+@with_tempfile
+def test_protocol_commons(protocol_file):
+
+    for protocol_class in [DryRunProtocol, DryRunExternalsProtocol,
+                           ExecutionTimeProtocol,
+                           ExecutionTimeExternalsProtocol, NullProtocol]:
+        protocol = protocol_class()
+        assert_is_instance(protocol, ProtocolInterface)
+        assert_equal(len(protocol), 0)
+
+        protocol.add_section(['some_command', 'some_option'],
+                             Exception("Whatever exception"))
+        protocol.add_section(['another_command'], None)
+        assert_equal(len(protocol), 2 if protocol_class != NullProtocol else 0)
+
+        # test iterable:
+        assert_raises(AssertionError, assert_raises, TypeError, iter,
+                      protocol)
+        for section in protocol:
+            assert_in('command', section)
+        for item in range(len(protocol)):
+            assert_is_instance(protocol.__getitem__(item), dict)
+
+        # test __str__:
+        str_ = str(protocol)
+
+        # test write_to_file:
+        protocol.write_to_file(protocol_file)
+        read_str = ''
+        with open(protocol_file, 'r') as f:
+            for line in f.readlines():
+                read_str += line
+        assert_equal(str_, read_str)
 
 
 @with_tempfile
@@ -64,34 +100,74 @@ def test_ExecutionTimeProtocol(path1, path2):
     assert_greater(timer_protocol[2]['duration'], 0)
 
 
+@with_tempfile
+@with_tempfile
+def test_ExecutionTimeExternalsProtocol(path1, path2):
+
+    timer_protocol = ExecutionTimeExternalsProtocol()
+    runner = Runner(protocol=timer_protocol)
+
+    # test external command:
+    cmd = ['git', 'init']
+    os.mkdir(path1)
+    runner.run(cmd, cwd=path1)
+    assert_equal(len(timer_protocol), 1, str(runner.protocol))
+    assert_equal(cmd, timer_protocol[0]['command'])
+    assert_greater(timer_protocol[0]['end'], timer_protocol[0]['start'])
+    assert_greater(timer_protocol[0]['duration'], 0)
+    assert_is(timer_protocol[0]['exception'], None)
+
+    # now with exception, since path2 doesn't exist yet:
+    try:
+        runner.run(cmd, cwd=path2)
+    except Exception, e:
+        catched_exception = e
+    finally:
+        assert_equal(len(timer_protocol), 2)
+        assert_equal(cmd, timer_protocol[1]['command'])
+        assert_greater(timer_protocol[1]['end'], timer_protocol[1]['start'])
+        assert_greater(timer_protocol[1]['duration'], 0)
+        assert_is(timer_protocol[1]['exception'], catched_exception)
+
+    # test callable (no entry added):
+    new_runner = Runner(cwd=path2, protocol=timer_protocol)
+    git_repo = GitRepo(path2, runner=new_runner)
+    assert_equal(len(timer_protocol), 2)
+
 
 @with_tempfile
-def test_protocol_commons(protocol_file):
+def test_DryRunProtocol(path):
 
-    for protocol_class in [DryRunProtocol, DryRunExternalsProtocol,
-                           ExecutionTimeExternalsProtocol, NullProtocol]:
-        protocol = protocol_class()
-        assert_is_instance(protocol, ProtocolInterface)
+    protocol = DryRunProtocol()
+    runner = Runner(protocol=protocol, cwd=path)
+    cmd = ['git', 'init']
 
-        protocol.add_section(['some_command', 'some_option'],
-                             Exception("Whatever exception"))
-        protocol.add_section(['another_command'], None)
+    # path doesn't exist, so an actual run would raise Exception,
+    # but a dry run wouldn't:
+    assert_raises(AssertionError, assert_raises, Exception, runner.run, cmd)
+    assert_equal(len(protocol), 1)
 
-        # test iterable:
-        assert_raises(AssertionError, assert_raises, TypeError, iter,
-                      protocol)
-        for section in protocol:
-            assert_in('command', section)
-        for item in range(len(protocol)):
-            assert_is_instance(protocol.__getitem__(item), dict)
+    # callable is also not executed, but recorded in the protocol:
+    git_repo = GitRepo(path, runner=runner)
+    assert_false(os.path.exists(path))
+    assert_false(os.path.exists(os.path.join(path, '.git')))
+    assert_equal(len(protocol), 2)
 
-        # test __str__:
-        str_ = str(protocol)
 
-        # test write_to_file:
-        protocol.write_to_file(protocol_file)
-        read_str = ''
-        with open(protocol_file, 'r') as f:
-            for line in f.readlines():
-                read_str += line
-        assert_equal(str_, read_str)
+@with_tempfile
+def test_DryRunExternalsProtocol(path):
+
+    protocol = DryRunExternalsProtocol()
+    runner = Runner(protocol=protocol, cwd=path)
+    cmd = ['git', 'init']
+
+    # path doesn't exist, so an actual run would raise Exception,
+    # but a dry run wouldn't:
+    assert_raises(AssertionError, assert_raises, Exception, runner.run, cmd)
+    assert_equal(len(protocol), 1)
+
+    # callable is executed and not recorded in the protocol:
+    git_repo = GitRepo(path, runner=runner)
+    assert_true(os.path.exists(path))
+    assert_true(os.path.exists(os.path.join(path, '.git')))
+    assert_equal(len(protocol), 1)
