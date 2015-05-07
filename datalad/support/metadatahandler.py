0986b3f5ff9e6f550463283a021a7de3a017af35
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-05-07 17:21:08 +0200

    Nothing new. Still drafting ...

--- datalad/support/metadatahandler.py
+++ datalad/support/metadatahandler.py
@@ -10,18 +10,25 @@
 """
 
 import logging
+from os.path import join as opj
 from abc import ABCMeta, abstractmethod, abstractproperty
 
 from rdflib import Graph, URIRef, Literal
 from rdflib.namespace import RDF, FOAF
+import rdflib.namespace
+
 
 
 lgr = logging.getLogger('datalad.metadata')
+
 
 class MetadataHandler(object):
     """Interface for metadata handlers.
     """
     __metaclass__ = ABCMeta
+
+    # TODO: rdf datalad namespace for Collections, Handles, standard metadata
+    # if possible, just use existing ones, like RDF.
 
     def __init__(self, path):
         """
@@ -54,7 +61,11 @@ class DefaultHandler(MetadataHandler):
         # either take all other files or a specific one like 'metadata'
         # and try whether we can read it.
 
-        # add to the graph
+        ######temporarily do just something valid:
+        meta.parse(opj(self._path, 'metadata'))
+
+        # add to the graph (join at handles' node?! => in Handle-class)
+        # TODO: How to this? Check rdflib for such operations.
 
         return meta
 
@@ -62,4 +73,10 @@ class DefaultHandler(MetadataHandler):
         if not isinstance(meta, Graph):
             lgr.error("Argument is not a Graph: %s" % type(meta))
             raise TypeError("Argument is not a Graph: %s" % type(meta))
+
+        # as above: just store for now. Has to be refined later on, regarding
+        # what to store where.
+        meta.serialize(opj(self._path, 'metadata'), format="turtle")
+
+        # TODO: Where to commit?
 
