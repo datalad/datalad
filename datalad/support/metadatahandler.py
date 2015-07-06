51af197df6b80d3aed395ab1b020a8386e5208d3
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-07-06 13:12:15 +0200

    Redesign I: Adding abstract class MetadataImporter

--- datalad/support/metadatahandler.py
+++ datalad/support/metadatahandler.py
@@ -13,243 +13,145 @@ import logging
 from os.path import join as opj
 from abc import ABCMeta, abstractmethod
 
-from rdflib import Graph, Literal, Namespace, BNode
-from rdflib.namespace import RDF, FOAF
+from rdflib import Graph, Literal, Namespace, BNode, URIRef
+from rdflib.namespace import RDF, RDFS, FOAF, XSD, DCTERMS
 from rdflib.exceptions import ParserError
 
-# define datalad namespace:
+# define needed namespaces:
 DLNS = Namespace('http://www.datalad.org/terms/')
-"""Namespace for datalad rdf triples.
+"""Namespace for datalad terms.
 """
+PROV = Namespace('http://www.w3.org/ns/prov#')
+DCAT = Namespace('http://www.w3.org/ns/dcat#')
+DCTYPES = Namespace('http://purl.org/dc/dcmitype/>')
+PAV = Namespace('http://purl.org/pav/')
+EMP = Namespace('#')
 
 lgr = logging.getLogger('datalad.metadata')
 
 
-class MetadataHandler(object):
-    """Interface for metadata handlers.
+class MetadataImporter(object):
+    """Base class for importers of various metadata formats.
 
-    To be implemented by any handler, that aims to provide support for a
-    certain metadata format.
+    This abstract class has to be implemented, in order to provide datalad
+    support for a certain metadata format. Besides the interface, enforced by
+    this class, it provides basic functionality and data structure for that
+    purpose.
+    It provides a dictionary `self._graphs` to store imported rdf-graphs in
+    as well as a default implementation to store them within handle or
+    collection repositories.
+
+    Note: If you are about to implement a derivation from this class, make sure
+    you read datalad-metadata.rst first for a description of the
+    'datalad descriptor' and the general approach to metadata in datalad.
+    Furthermore, be aware that only `import_data` lacks a default implementation.
     """
+
     __metaclass__ = ABCMeta
 
+    def __init__(self, target, about_class=None, about_uri=None):
+        """Constructor
+
+        Note: In case the metadata to be imported is about a handle or
+        collection, the constructor initializes `self._graphs['datalad']`,
+        which is to be used for the standard statements of the 'datalad
+        descriptor'. You can add additional graphs to the `self._graphs`
+        dictionary.
+
+        Parameters:
+        -----------
+        target_class: HandleRepo or CollectionRepo
+          the type of repo, the metadata is to be imported to;
+          either "Handle" or "Collection"
+          Note (todo): It may become handy at some point to have no target repo
+          at all.
+        about_class: str
+          the kind of entity, the metadata is about. In case of "Handle" or
+          "Collection" a corresponding datalad graph has to be generated.
+          If the imported metadata is about a sub-entity of the repo's content,
+          datalad doesn't know (and doesn't need to know) that class of things.
+          In that case, it's up to the importer to decide, whether or not this
+          class needs some special treatment.
+        about_uri: str
+          URI of the entity, the metadata is about. In case of handles or
+          collections this may be the path to its repo in local file system or
+          its url, if it is known by the caller. If it is about a collection or
+          handle importing metadata about itself, it uses `DLNS.this` instead.
+        """
+        self._target = target
+        self._about_class = about_class
+        self._about_uri = about_uri
+
+        self._graphs = dict()
+        if self._about_class == 'Handle' or self._about_class == 'Collection':
+            self._init_datalad_graph()
+
+    def _init_datalad_graph(self):
+        """Convenience method to init the datalad descriptor graph"""
+
+        self._graphs['datalad'] = Graph()
+        self._graphs['datalad'].bind('prov', PROV)
+        self._graphs['datalad'].bind('dcat', DCAT)
+        self._graphs['datalad'].bind('dctypes', DCTYPES)
+        self._graphs['datalad'].bind('dct', DCTERMS)
+        self._graphs['datalad'].bind('pav', PAV)
+        self._graphs['datalad'].bind('foaf', FOAF)
+        self._graphs['datalad'].bind('dlns', DLNS)
+        self._graphs['datalad'].bind('', EMP)
+        # TODO: Find a better prefix than ''. When stored, then parsed with
+        # rdflib and stored again, it turns to the path of the file,
+        # it was stored in!
+
+        if self._about_class == 'Handle':
+            self._graphs['datalad'].add((URIRef(self._about_uri),
+                                         RDF.type, DLNS.Handle))
+        elif self._about_class == 'Collection':
+            self._graphs['datalad'].add((URIRef(self._about_uri),
+                                         RDF.type, DLNS.Collection))
+
     @abstractmethod
-    def get_graph(self, uri=None, class_=None, file_=None, identifier=None,
-                  data=None):
-        """Get a representation of the metadata of a resource.
+    def import_data(self, files=None, data=None):
+        """The actual import routine
 
-        This should return a (named) rdflib.Graph containing the metadata. This
-        graph is required to contain a certain set of statements, that are
-        defined as the "datalad handle descriptor" or
-        "datalad collection descriptor" respectively in
-        'docs/datalad-metadata.rst'.
-        The datalad namespace (http://www.datalad.org/terms/) is available as
-        a rdflib.namespace `DLNS`.
+        Has to be able to import data from files, given their paths, as well as
+        just the content of the files provided as strings. The latter is
+        necessary, in case the actual files are accessible via a remote of a
+        repository only.
+
+        This means: Either `files` or `data` has to be provided by the caller.
 
         Parameters:
         -----------
-        uri: rdflib.URIRef
-            the resource, the metadata is about. This is needed to have this
-            as a node (kind of a root) in the graph. Will usually be the uri of
-            a collection or handle repository.
-        class_: rdflib.term.URIRef
-            the class of the resource at `uri`, i.e. the term to be used to
-            describe this resource. For now, it's either `DLNS.Handle` or
-            `DLNS.Collection`.
-        file_: str or list of str
-            path to the file or directory containing the metadata. May also be
-            a list of files.
-        identifier: str
-            optionally used to create a named graph
-        data: str
-            optionally parse `data` instead of any file
-
-        Note: Either `file_` or `data` must be provided.
+        files: str or list of str
+          a path to the file or directory to be imported or a list containing
+          such paths.
+        data: str or list of str
         """
         pass
 
-    def set(self, meta, file_):
-        """Write metadata to file.
+    @abstractmethod
+    def store_data(self, path):
+        """Store the imported metadata within a given directory
 
-        Note: By now, this doesn't need to be implemented, since the metadata
-        is provided by the handle/collection publisher/maintainer in the very
-        format the handler is implementing. So, changes should probably be done
-        without datalad. The only thing datalad writes metadata to, is a cache.
-        This may change once we discover it's needed.
+        This routine stores the metadata imported by `import_data` within the
+        directory `path`. The datalad standard statements as described in
+        'datalad-metadata.rst' have to be stored in a file called 'datalad.ttl'
+        in turtle.
+        Additional statements can be stored in additional files,
+        also using turtle and the ending ".ttl". Any additional file can be
+        read by datalad separately, allowing for smaller rdf-graphs to be build,
+        whenever there is no need to have a runtime representation of all its
+        metadata.
+
+        The default implementation just stores every graph stored in
+        `self._graphs['key']` in the file 'key.ttl'.
 
         Parameters:
         -----------
-        meta: rdflib.Graph
-        file_: str or list of str
-            path to the file or directory containing the metadata. May also be
-            a list of files.
+        path: str
+          path to the directory to save the metadata in.
         """
-        raise NotImplementedError
 
-import json
-import dateutil.parser
-
-
-class JSONExampleHandler(MetadataHandler):
-    """Implementation of a very simple JSON metadata format.
-
-    Just an example to show, how things are supposed to work.
-    For this format a JSON file, named 'metadata.json' is accepted and may
-    contain some keys that are 'translated' to datalad rdf-representation as
-    follows:
-
-    'authors': a list of authors' names
-        (There is no split for first and last name here)
-        translates to datalad property 'authoredBy'
-
-    'comment':
-        translates to datalad property 'description'
-
-    'date':
-        translates to datalad property 'released'
-
-    'name':
-        translates to datalad property 'title'
-
-    'release':
-        translates to datalad property 'version'
-
-    Note: These datalad properties as well as this format are just a demo.
-    Nothing well defined, nothing thoughtfully considered to be reasonable.
-    Just demonstrating the principle.
-
-    See also RDFExampleHandler for comparison.
-    """
-
-    def __init__(self, uri, class_):
-        super(JSONExampleHandler, self).__init__(uri, class_)
-
-    def get_graph(self, file_='metadata.json', identifier=None, data=None):
-        meta = Graph(identifier=identifier)
-        meta.bind('dlns', DLNS)
-        meta.bind('foaf', FOAF)
-
-        if data is None:
-            json_dict = json.load(open(opj(self._uri, file_), 'r'))
-        else:
-            json_dict = json.loads(data)
-
-        for author in json_dict['authors']:
-            # Create a node for the author;
-            # could also be a URIRef in case a homepage is provided or sth.
-            authors_node = BNode()
-            meta.add((authors_node, RDF.type, FOAF.Person))
-            meta.add((authors_node, FOAF.name, Literal(author)))
-
-            # make it a author of the thing (to be queried for):
-            meta.add((self._class, DLNS.authoredBy, authors_node))
-            # Note: Actually, needs to be a rdf list (first, rest)
-            # Not important for now.
-
-        if json_dict['comment']:
-            meta.add((self._class, DLNS.description,
-                      Literal(json_dict['comment'])))
-        if json_dict['date']:
-            date = dateutil.parser.parse(json_dict['date'])
-            meta.add((self._class, DLNS.released, Literal(date)))
-        if json_dict['name']:
-            meta.add((self._class, DLNS.title, Literal(json_dict['name'])))
-        if json_dict['release']:
-            meta.add((self._class, DLNS.version, Literal(json_dict['release'])))
-        for t in json_dict['topic']:
-            meta.add((self._class, DLNS.tag, Literal(json_dict['topic'])))
-
-        return meta
-
-
-class RDFExampleHandler(MetadataHandler):
-    """An example to show an implementation of a MetadataHandler.
-
-    By now just accepts a turtle file named 'metadata.rdf' containing the
-    'datalad descriptor' implicitly defined in `JSONExampleHandler`.
-    """
-
-    def __init__(self, uri, class_):
-        super(RDFExampleHandler, self).__init__(uri, class_)
-
-    def get_graph(self, file_='metadata.rdf', identifier=None, data=None):
-        meta = Graph(identifier=identifier)
-
-        if data is None:
-            try:
-                meta.parse(opj(self._uri, file_), format="turtle")
-            except IOError, ioe:
-                lgr.warning("Failed to read metadata file: %s" % ioe)
-            except ParserError, pe:
-                lgr.error("Failed to parse metadata file: %s" % pe)
-        else:
-            try:
-                meta.parse(data=data, format="turtle")
-            except ParserError, pe:
-                lgr.error("Failed to parse metadata file: %s" % pe)
-
-        return meta
-
-    def set(self, meta, file_='metadata.rdf'):
-        if not isinstance(meta, Graph):
-            lgr.error("Argument is not a Graph: %s" % type(meta))
-            raise TypeError("Argument is not a Graph: %s" % type(meta))
-
-        meta.serialize(opj(self._uri, file_), format="turtle")
-
-
-class DefaultHandler(MetadataHandler):
-    # DefaultHandler may be should scan for file types and instantiate a
-    # Handler for each; then join the graphs. Would probably need a method to
-    # read a single file in the handler interface.
-
-    def __init__(self, uri, class_):
-        super(DefaultHandler, self).__init__(uri, class_)
-
-    def get_graph(self, file_=None, identifier=None, data=None):
-        return Graph(identifier=identifier)
-
-    def set(self, meta, file_=None):
-        pass
-
-
-class CacheHandler(MetadataHandler):
-    """Handler to care for collection's metadata cache, managed by datalad
-
-    By now stores the cached graph just as a turtle-file. This may change,
-    depending on what turns out to be the fastest parseable format.
-    """
-    # TODO: May be to be (re-)implemented, once we decided about a
-    # 'datalad descriptor'
-
-    def __init__(self):
-        super(CacheHandler, self).__init__()
-
-    def get_graph(self, uri=None, class_=None, file_=None, identifier=None,
-                  data=None):
-        meta = Graph(identifier=identifier)
-        if data is None:
-            meta.parse(file_, format="turtle")
-        else:
-            meta.parse(data=data, format="turtle")
-        return meta
-
-    def set(self, meta, file_=None):
-        meta.serialize(file_, format="turtle")
-
-
-class PlainTextHandler(MetadataHandler):
-
-    def __init__(self, uri, class_):
-        super(PlainTextHandler, self).__init__(uri, class_)
-
-    def get_graph(self, file_=None, identifier=None, data=None):
-        meta = Graph(identifier=identifier)
-
-        self._uri
-        self._class
-
-
-# TODO: Things probably to add:
-# class 'real' JSONHandler
-# class W3CDescriptorHandler
+        for key in self._graphs:
+            self._graphs[key].serialize(opj(path, key + '.ttl'),
+                                        format="turtle")
