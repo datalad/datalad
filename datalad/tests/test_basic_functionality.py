7dcfdf00af2734039445f0537c8cfe7d0ccf7106
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-08-27 10:55:57 +0200

    Add tests for querying metadata in collections and metacollections.

--- datalad/tests/test_basic_functionality.py
+++ datalad/tests/test_basic_functionality.py
@@ -13,11 +13,16 @@ from os.path import join as opj
 
 from nose import SkipTest
 from nose.tools import assert_raises, assert_equal, assert_false, assert_in, \
-    assert_not_in
+    assert_not_in, assert_is_instance
+from six import iterkeys
 
 from ..support.handlerepo import HandleRepo
-from ..support.collectionrepo import CollectionRepo, CollectionRepoHandleBackend
-from ..tests.utils import ok_clean_git, with_tempfile, ok_
+from ..support.collectionrepo import CollectionRepo, CollectionRepoBackend, \
+    CollectionRepoHandleBackend
+from ..support.collection import Collection, MetaCollection
+from ..support.metadatahandler import PlainTextImporter, PAV, PROV, DCTERMS, \
+    DCTYPES, DLNS, DCAT, FOAF, EMP, Literal, URIRef
+from ..tests.utils import ok_clean_git, with_tempfile, ok_, with_tree
 from ..utils import get_local_file_url
 
 # Note: For the actual commands use the following to determine paths to
@@ -219,3 +224,171 @@ def test_uninstall_handle(m_path, c_path, h_path, install_path):
 
     ok_clean_git(local_master.path, annex=False)
     assert_equal(local_master.get_handle_list(), [])
+
+
+@with_tempfile
+@with_tempfile
+@with_tree([
+    ('AUTHORS', "Benjamin Poldrack <benjaminpoldrack@gmail.com>\n#\n# \n# "
+                "bla, bla\n<justanemail@address.tl>\nsomeone else\n"
+                "digital native <https://www.myfancypage.com/digital>"),
+    ('LICENSE', "This is a license file\n with several lines."),
+    ('README', "Read this to have a clue what the repository is about.\n"
+               "This is metadata on a collection.")
+    ])
+def test_query_collection(c_path, h_path, md_hdl):
+
+    # setup the collection to be queried:
+    h_repo = HandleRepo(h_path)
+    c_repo = CollectionRepo(c_path, name="MyCollection")
+    c_repo.add_handle(h_repo, "MyHandle")
+    c_repo.add_metadata_src_to_handle(PlainTextImporter, "MyHandle", md_hdl)
+    collection = Collection(CollectionRepoBackend(c_repo))
+
+    # TODO: Bindings should be done in collection class:
+    # collection.conjunctive_graph.bind('prov', PROV)
+    # collection.conjunctive_graph.bind('dcat', DCAT)
+    # collection.conjunctive_graph.bind('dctypes', DCTYPES)
+    # collection.conjunctive_graph.bind('dct', DCTERMS)
+    # collection.conjunctive_graph.bind('pav', PAV)
+    # collection.conjunctive_graph.bind('foaf', FOAF)
+    # collection.conjunctive_graph.bind('dlns', DLNS)
+    # collection.conjunctive_graph.bind('', EMP)
+    collection.conjunctive_graph.namespace_manager = collection.meta.namespace_manager
+
+    # query for a handle, which is authored by a person named
+    # "Benjamin Poldrack":
+    query_handle_certain_author = \
+        """SELECT ?g ?r {GRAPH ?g {?r rdf:type dlns:Handle .
+                                   ?r pav:createdBy ?p .
+                                 ?p foaf:name "Benjamin Poldrack" .}}"""
+    # same query, but "Benjamin Poldrack" authored SOMETHING in the handle,
+    # not just the handle itself:
+    alt_query_1 = \
+        """SELECT ?g ?r {GRAPH ?g {?r pav:createdBy ?p .
+                                   ?p foaf:name "Benjamin Poldrack" .}}"""
+
+    # TODO: handle or content, but not deeper down
+    alt_query_2 = \
+        """SELECT ?g ?r {{GRAPH ?g {?r rdf:type dlns:Handle .
+                                   ?r pav:createdBy ?p .
+                                   ?p foaf:name "Benjamin Poldrack" .}}
+                            UNION {GRAPH ?g {?r rdf:type dlns:Handle .
+                                   ?r dct:hasPart ?c .
+                                   ?c pav:createdBy ?p .
+                                   ?p foaf:name "Benjamin Poldrack" .}}}"""
+
+    results = collection.conjunctive_graph.query(query_handle_certain_author)
+    # should result in the handle "MyHandle" and its location:
+    assert_in((Literal("MyHandle"), URIRef(get_local_file_url(h_path))),
+              results)
+    assert_equal(len(results), 1)
+
+    results2 = collection.conjunctive_graph.query(alt_query_1)
+    # 2 results, one per each entity authored by "Benjamin Poldrack".
+    # Both results contain the handle's name, but only one its location
+    # (which is its uri). The uri of the content entity is artificial and of
+    # no use here.
+    # Note: This uri construction has to change.
+    import os
+    content_uri = URIRef(get_local_file_url(os.getcwd() + '/#content'))
+
+    assert_equal(len(results2), 2)
+    assert_in((Literal("MyHandle"), URIRef(get_local_file_url(h_path))),
+              results2)
+    assert_in((Literal("MyHandle"), content_uri), results2)
+
+    results3 = collection.conjunctive_graph.query(alt_query_2)
+    # 2 results, one resulting from the entity 'handle' itself
+    # and one from the content of this handle. The select returns name and
+    # location of the handle in both cases.
+    assert_equal(len(results2), 2)
+    assert_equal(set(results3),
+                 {(Literal("MyHandle"), URIRef(get_local_file_url(h_path)))})
+
+
+@with_tempfile
+@with_tempfile
+@with_tempfile
+@with_tempfile
+@with_tempfile
+@with_tree([
+    ('AUTHORS', "Benjamin Poldrack <benjaminpoldrack@gmail.com>\n#\n# \n# "
+                "bla, bla\n<justanemail@address.tl>\nsomeone else\n"
+                "digital native <https://www.myfancypage.com/digital>"),
+    ('LICENSE', "This is a license file\n with several lines."),
+    ('README', "Read this to have a clue what the repository is about.\n"
+               "This is metadata on a collection.")
+    ])
+@with_tree([
+    ('CONTRIBUTORS', "Benjamin Poldrack <benjaminpoldrack@gmail.com>\n#\n# \n"
+                     "# bla, bla\n<justanemail@address.tl>\nanother one\n"
+                     "digital native <https://www.myfancypage.com/digital>"),
+    ('LICENSE', "http://example.com/license"),
+    ('README', "Read this to have a clue what the repository is about. "
+               "This is metadata on a handle.")
+    ])
+def test_query_metacollection(m_path, c_path1, c_path2, h_path1, h_path2,
+                              md1, md2):
+
+    # create the master collection; register two collections, each containing
+    # one handle and metadata:
+    m_path = opj(m_path, 'localcollection')
+    local_master = CollectionRepo(m_path, name='local')
+    c_repo1 = CollectionRepo(c_path1, name="collection1")
+    c_repo2 = CollectionRepo(c_path2, name="collection2")
+    h_repo1 = HandleRepo(h_path1)
+    h_repo2 = HandleRepo(h_path2)
+    c_repo1.add_handle(h_repo1, name="handle1")
+    c_repo2.add_handle(h_repo2, name="handle2")
+    c_repo1.add_metadata_src_to_handle(PlainTextImporter, "handle1", md1)
+    c_repo2.add_metadata_src_to_handle(PlainTextImporter, "handle2", md2)
+    local_master.git_remote_add("collection1", c_path1)
+    local_master.git_fetch("collection1")
+    local_master.git_remote_add("collection2", c_path2)
+    local_master.git_fetch("collection2")
+
+    # now, retrieve the metacollection to be queried (all known collections):
+    collections = [local_master.get_backend_from_branch(remote + "/master")
+                   for remote in local_master.git_get_remotes()]
+    collections.append(local_master.get_backend_from_branch())
+
+    metacollection = MetaCollection(collections)
+
+    assert_equal(set(iterkeys(metacollection)),
+                 {"collection1", "collection2", "local"})
+    [assert_is_instance(metacollection[key], Collection)
+     for key in iterkeys(metacollection)]
+
+    # TODO: prefix bindings! see test above
+    metacollection.conjunctive_graph.namespace_manager = \
+        Collection(CollectionRepoBackend(c_repo1)).meta.namespace_manager
+
+    # query it:
+    # query for a handle, which is authored by a person named
+    # "Benjamin Poldrack":
+    query_handle_certain_author1 = \
+        """SELECT ?g ?r {GRAPH ?g {?r rdf:type dlns:Handle .
+                                   ?r pav:createdBy ?p .
+                                   ?p foaf:name "Benjamin Poldrack" .}}"""
+    results = \
+        metacollection.conjunctive_graph.query(query_handle_certain_author1)
+    # returns both handles and their locations:
+    assert_equal(len(results), 2)
+    assert_in((Literal("handle1"), URIRef(get_local_file_url(h_path1))),
+              results)
+    assert_in((Literal("handle2"), URIRef(get_local_file_url(h_path2))),
+              results)
+
+    # query for author "another one" should return handle2 only:
+    query_handle_certain_author2 = \
+        """SELECT ?g ?r {GRAPH ?g {?r rdf:type dlns:Handle .
+                                   ?r pav:createdBy ?p .
+                                   ?p foaf:name "another one" .}}"""
+    results2 = \
+        metacollection.conjunctive_graph.query(query_handle_certain_author2)
+    # returns both handles and their locations:
+    assert_equal(len(results2), 1)
+    assert_in((Literal("handle2"), URIRef(get_local_file_url(h_path2))),
+              results)
+
