13083bb0ffb104b5f1b7bc98adbda6850c1c4130
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-09-10 16:16:38 +0200

    Make publish_handle repeatable ;)

--- datalad/interface/publish_handle.py
+++ datalad/interface/publish_handle.py
@@ -18,9 +18,10 @@ from .base import Interface
 from ..support.param import Parameter
 from ..support.constraints import EnsureStr, EnsureBool, EnsureNone
 from ..support.handlerepo import HandleRepo
-from ..support.collection import MetaCollection
+from ..support.annexrepo import AnnexRepo
 from ..support.metadatahandler import CustomImporter, URIRef, Literal, DLNS, \
     EMP, RDF, PAV, PROV, FOAF, DCTERMS
+from ..consts import REPO_STD_META_FILE, HANDLE_META_DIR
 from ..cmdline.helpers import get_repo_instance
 from ..log import lgr
 from appdirs import AppDirs
@@ -49,10 +50,15 @@ class PublishHandle(Interface):
             args=('url',),
             doc="public url of the published handle",
             nargs="?",
-            constraints=EnsureStr() | EnsureNone()
-        ))
+            constraints=EnsureStr() | EnsureNone()),
+        remote=Parameter(
+            args=('remote',),
+            doc="name for the remote to add to the local handle",
+            nargs="?",
+            constraints=EnsureStr() | EnsureNone())
+        )
 
-    def __call__(self, target, handle=curdir, url=None):
+    def __call__(self, target, handle=curdir, url=None, remote=None):
 
         local_handle_repo = get_repo_instance(
             abspath(expandvars(expanduser(handle))), HandleRepo)
@@ -66,10 +72,12 @@ class PublishHandle(Interface):
             url = target
 
         # prepare publish branch in local handle:
-        local_handle_repo.git_checkout("publish", '-b')
+        # check for existing publish branches:
+        num_pub = len([b for b in local_handle_repo.git_get_branches()
+                      if b.startswith("publish")])
+        p_branch = "publish%d" % (num_pub + 1)
+        local_handle_repo.git_checkout(p_branch, '-b')
 
-        from ..support.metadatahandler import CustomImporter
-        from ..consts import REPO_STD_META_FILE, HANDLE_META_DIR
         importer = CustomImporter('Handle', 'Handle', DLNS.this)
         importer.import_data(opj(local_handle_repo.path, HANDLE_META_DIR))
         graphs = importer.get_graphs()
@@ -82,14 +90,19 @@ class PublishHandle(Interface):
                 graphs[graph_name].add((new_uri, p, o))
 
         importer.store_data(opj(local_handle_repo.path, HANDLE_META_DIR))
-        local_handle_repo.add_to_git(opj(local_handle_repo.path, HANDLE_META_DIR))
+        local_handle_repo.add_to_git(opj(local_handle_repo.path,
+                                         HANDLE_META_DIR))
 
-        from ..support.annexrepo import AnnexRepo
         # create target annex:
         published_handle = AnnexRepo(target, create=True)
 
         # add as remote to local:
-        local_handle_repo.git_remote_add("published", url)
+        if remote is None:
+            num_pub_remotes = len([r for r in
+                                   local_handle_repo.git_get_remotes()
+                                   if r.startswith("publish")])
+            remote = "published%d" % (num_pub_remotes + 1)
+        local_handle_repo.git_remote_add(remote, url)
 
         # TODO: may be use git annex testremote?
 
@@ -98,20 +111,22 @@ class PublishHandle(Interface):
         # out in target; in general we can't explicitly allow for the local
         # repo to push
         published_handle.git_checkout("TEMP", '-b')
-        local_handle_repo.git_push("published +publish:master")
+        local_handle_repo.git_push("%s +%s:master" % (remote, p_branch))
         published_handle.git_checkout("master")
         # published_handle._git_custom_command('', "git branch -D TEMP")
         # "TEMP" not found? => because it's empty?
 
         # 2. sync, to get locations for files, that are not present in
         # local handle
-        local_handle_repo._annex_custom_command('', "git annex sync published")
+        local_handle_repo._annex_custom_command('', "git annex sync %s"
+                                                % remote)
         published_handle._annex_custom_command('', "git annex sync")
 
         # 3. copy locally available files:
         for file in local_handle_repo.get_annexed_files():
             if local_handle_repo.file_has_content(file):
-                local_handle_repo._annex_custom_command('', "git annex copy %s --to=published" % file)
+                local_handle_repo._annex_custom_command(
+                    '', "git annex copy %s --to=%s" % (file, remote))
 
         # 4. get everything else
         published_handle.annex_get('.')
