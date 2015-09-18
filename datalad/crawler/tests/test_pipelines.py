6a5cf905b94a3ecd83903f6e1d8d8a8895ed62d7
Yaroslav Halchenko <debian@onerussian.com>
2015-09-18 09:35:05 -0400

    RF: just minor extension to test on openfmri pipeline

--- datalad/crawler/tests/test_pipelines.py
+++ datalad/crawler/tests/test_pipelines.py
@@ -13,9 +13,15 @@ from ..newmain import crawl_url
 from ..matches import *
 from ..pipeline import run_pipeline
 
+from ...tests.utils import eq_, ok_
+
 class DataSink(object):
     def __init__(self):
         self.data = []
+
+    def get_fields(self, *keys):
+        return [(d[k] for k in keys) for d in self.data]
+
     def __call__(self, **data):
         # ??? for some reason didn't work when I made entire thing a list
         self.data.append(data)
@@ -25,30 +31,42 @@ class DataSink(object):
 def test_basic_openfmri_top_pipeline():
     sink1 = DataSink()
     sink2 = DataSink()
+    sink_licenses = DataSink()
     pipeline = [
         crawl_url("https://openfmri.org/data-sets"),
         a_href_match(".*/dataset/(?P<dataset_dir>ds0*(?P<dataset>[1-9][0-9]*))$"),
         # if we wanted we could instruct to crawl inside
-        [crawl_url(),
-         # and collect all URLs under "AWS Link"
-         css_match('.field-name-field-aws-link a',
-                   xpaths={'url': '@href',
-                           'url_text': 'text()'}),
-         sink2],
+        [
+            crawl_url(),
+            [# and collect all URLs under "AWS Link"
+                css_match('.field-name-field-aws-link a',
+                           xpaths={'url': '@href',
+                                   'url_text': 'text()'}),
+                sink2
+             ],
+            [# and license information
+                css_match('.field-name-field-license a',
+                           xpaths={'url': '@href',
+                                   'url_text': 'text()'}),
+                sink_licenses
+            ],
+        ],
         sink1
     ]
 
     run_pipeline(pipeline)
     # we should have collected all the URLs to the datasets
     urls = [e['url'] for e in sink1.data]
-    assert(len(urls) > 20)  # there should be at least 20 listed there
-    assert(all([url.startswith('https://openfmri.org/dataset/ds00') for url in urls]))
+    ok_(len(urls) > 20)  # there should be at least 20 listed there
+    ok_(all([url.startswith('https://openfmri.org/dataset/ds00') for url in urls]))
     # got our dataset_dir entries as well
-    assert(all([e['dataset_dir'].startswith('ds0') for e in sink1.data]))
+    ok_(all([e['dataset_dir'].startswith('ds0') for e in sink1.data]))
 
     # and sink2 should collect everything downloadable from under AWS Link section
     # test that we got all needed tags etc propagated properly!
-    all_aws_entries = [(d['dataset'], d['url_text'], d['url']) for d in sink2.data]
-    assert(len(all_aws_entries) > len(urls)) # that we have at least as many ;-)
+    all_aws_entries = sink2.get_fields('dataset', 'url_text', 'url')
+    ok_(len(all_aws_entries) > len(urls))  # that we have at least as many ;-)
     #print('\n'.join(map(str, all_aws_entries)))
-
+    all_licenses = sink_licenses.get_fields('dataset', 'url_text', 'url')
+    eq_(len(all_licenses), len(urls))
+    #print('\n'.join(map(str, all_licenses)))
