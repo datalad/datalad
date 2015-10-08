76f9e8e5909b38809584f453d6ce3f61ace51b36
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-10-08 13:41:40 +0200

    Remove debug messages

--- datalad/resources/git_ssh.sh
+++ datalad/resources/git_ssh.sh
@@ -7,10 +7,8 @@ else
 	PORT=22
 fi
 CONTROL_MASTER="/var/run/user/$(id -u)/datalad/$1:$PORT $1"
-echo "DEBUG: socket: $CONTROL_MASTER" >&2
 shift 1
 COMMAND="ssh -S $CONTROL_MASTER $@"
-echo "DEBUG: command: $COMMAND" >&2
 $COMMAND
 
 
