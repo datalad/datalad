87801513d9e24656f411aa151e8830dfea79ab93
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2015-10-13 10:28:52 +0200

    BF: Adapting GIT_SSH

--- datalad/resources/git_ssh.sh
+++ datalad/resources/git_ssh.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if test "$1" = "-p"
 then
-	PORT=$2
+	PORT=:$2
 	shift 2
 else
-	PORT=22
+    PORT=""
 fi
-CONTROL_MASTER="/var/run/user/$(id -u)/datalad/$1:$PORT $1"
+CONTROL_MASTER="/var/run/user/$(id -u)/datalad/$1$PORT $1"
 shift 1
 COMMAND="ssh -S $CONTROL_MASTER $@"
 $COMMAND
