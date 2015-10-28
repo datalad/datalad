22807aa31f6216199036139a1750d0152b50e82d
Yaroslav Halchenko <debian@onerussian.com>
2015-10-28 12:43:15 -0400

    BF: Reintroducing commit ebd8f54ee71746b051bfbf1bdb23695f3bce55dc

--- datalad/tests/utils.py
+++ datalad/tests/utils.py
@@ -411,40 +411,44 @@ else:
 from .utils_testrepos import BasicAnnexTestRepo, BasicHandleTestRepo, \
     BasicGitTestRepo, MetadataPTHandleTestRepo, BasicCollectionTestRepo
 
-_basic_annex_test_repo = BasicAnnexTestRepo()
-_basic_handle_test_repo = BasicHandleTestRepo()
-_basic_collection_test_repo = BasicCollectionTestRepo()
-_basic_git_test_repo = BasicGitTestRepo()
-_md_pt_handle_test_repo = MetadataPTHandleTestRepo()
-
-_TESTREPOS = {'basic_annex':
-                  {'network': 'git://github.com/datalad/testrepo--basic--r1',
-                   'local': _basic_annex_test_repo.path,
-                   'local-url': _basic_annex_test_repo.url},
-              'basic_annex_handle':
-                  {'local': _basic_handle_test_repo.path,
-                   'local-url': _basic_handle_test_repo.url},
-              'basic_git':
-                  {'local': _basic_git_test_repo.path,
-                   'local-url': _basic_git_test_repo.url},
-              'basic_git_collection':
-                  {'local': _basic_collection_test_repo.path,
-                   'local-url': _basic_collection_test_repo.url},
-              'meta_pt_annex_handle':
-                  {'local': _md_pt_handle_test_repo.path,
-                   'local-url': _md_pt_handle_test_repo.url}}
+_TESTREPOS = None
 
 
 def _get_testrepos_uris(regex, flavors):
+    global _TESTREPOS
+    # we should instantiate those whenever test repos actually asked for
+    # TODO: just absorb all this lazy construction within some class
+    if not _TESTREPOS:
+        _basic_annex_test_repo = BasicAnnexTestRepo()
+        _basic_handle_test_repo = BasicHandleTestRepo()
+        _basic_collection_test_repo = BasicCollectionTestRepo()
+        _basic_git_test_repo = BasicGitTestRepo()
+        _md_pt_handle_test_repo = MetadataPTHandleTestRepo()
+        _TESTREPOS = {'basic_annex':
+                        {'network': 'git://github.com/datalad/testrepo--basic--r1',
+                         'local': _basic_annex_test_repo.path,
+                         'local-url': _basic_annex_test_repo.url},
+                      'basic_annex_handle':
+                        {'local': _basic_handle_test_repo.path,
+                         'local-url': _basic_handle_test_repo.url},
+                      'basic_git':
+                        {'local': _basic_git_test_repo.path,
+                         'local-url': _basic_git_test_repo.url},
+                      'basic_git_collection':
+                        {'local': _basic_collection_test_repo.path,
+                         'local-url': _basic_collection_test_repo.url},
+                      'meta_pt_annex_handle':
+                        {'local': _md_pt_handle_test_repo.path,
+                         'local-url': _md_pt_handle_test_repo.url}}
+        # assure that now we do have those test repos created -- delayed
+        # their creation until actually used
+        if not on_windows:
+            _basic_annex_test_repo.create()
+            _basic_handle_test_repo.create()
+            _basic_collection_test_repo.create()
+            _basic_git_test_repo.create()
+            _md_pt_handle_test_repo.create()
     uris = []
-    # assure that now we do have those test repos created -- delayed
-    # their creation until actually used
-    if not on_windows:
-        _basic_annex_test_repo.create()
-        _basic_handle_test_repo.create()
-        _basic_collection_test_repo.create()
-        _basic_git_test_repo.create()
-        _md_pt_handle_test_repo.create()
     for name, spec in iteritems(_TESTREPOS):
         if not re.match(regex, name):
             continue
