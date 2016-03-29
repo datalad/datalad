75a6d06fa57e91cb009d3ff3464110bc47268632
Yaroslav Halchenko <debian@onerussian.com>
2016-03-28 23:59:08 -0400

    ENH: if running from under git __version__ will be based on git-describe

--- datalad/version.py
+++ datalad/version.py
@@ -9,4 +9,23 @@
 """Defines version to be imported in the module and obtained from setup.py
 """
 
+from os.path import lexists, dirname, join as opj
+
+# Hard coded version, to be done by release process
 __version__ = '0.1.dev0'
+
+if lexists(opj(dirname(dirname(__file__)), '.git')):
+    # If under git -- attempt to deduce a better "dynamic" version following git
+    try:
+        import sys
+        from subprocess import Popen, PIPE
+        git = Popen(['git', 'describe', '--abbrev=4', '--dirty', '--match', '[0-9]*\.*'],
+                    stdout=PIPE, stderr=sys.stderr)
+        if git.wait() != 0:
+            raise OSError
+        line = git.stdout.readlines()[0]
+        # Just take describe and replace initial '-' with .dev to be more "pythonish"
+        __version__ = line.strip().decode('ascii').replace('-', '.dev', 1)
+    except:
+        __version__ += ".giterror"
+
