0bbc93cf787c15e1ecbf295dd95289d2efa36708
Yaroslav Halchenko <debian@onerussian.com>
2016-04-22 17:55:20 -0400

    BF+TST: import of Dataset -- I have no clue how that import worked reliably before

--- datalad/interface/tests/test_ls.py
+++ datalad/interface/tests/test_ls.py
@@ -12,10 +12,15 @@
 
 __docformat__ = 'restructuredtext'
 
+from glob import glob
+
+from datalad.support.gitrepo import GitRepo
+from datalad.support.annexrepo import AnnexRepo
 from ...api import ls
 from ...utils import swallow_outputs
 from ...tests.utils import assert_equal, assert_in
 from ...tests.utils import use_cassette
+from ...tests.utils import with_tempfile
 
 from datalad.downloaders.tests.utils import get_test_providers
 
@@ -28,6 +33,19 @@ def test_ls_s3():
         get_test_providers(url)
 
     with swallow_outputs() as cmo:
-        assert_equal(ls(url), None) # not output ATM
+        assert_equal(ls(url), None)  # not output ATM
         assert_in('Bucket info:', cmo.out)
 test_ls_s3.tags = ['network']
+
+
+@with_tempfile
+def test_ls_repos(toppath):
+    # smoke test pretty much
+    GitRepo(toppath + '1', create=True)
+    AnnexRepo(toppath + '2', create=True)
+    with swallow_outputs() as cmo:
+        ls(glob(toppath + '*'))
+        assert_equal(len(cmo.out.rstrip().split('\n')), 2)
+        assert_in('[annex]', cmo.out)
+        assert_in('[git]', cmo.out)
+        assert_in('master', cmo.out)
