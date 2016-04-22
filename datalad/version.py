b8fb6e87c64910cc0d52e35f32df4e86de59cffa
Yaroslav Halchenko <debian@onerussian.com>
2016-04-22 16:51:28 -0400

    ENH: provide __full_version__ possibly with git treeish and __version__ without

--- datalad/version.py
+++ datalad/version.py
@@ -9,14 +9,16 @@
 """Defines version to be imported in the module and obtained from setup.py
 """
 
-from os.path import lexists, dirname, join as opj
+from os.path import lexists, dirname, join as opj, curdir
 
 # Hard coded version, to be done by release process
 __version__ = '0.1.dev1'
 
 # NOTE: might cause problems with "python setup.py develop" deployments
 #  so I have even changed buildbot to use  pip install -e .
-if lexists(opj(dirname(dirname(__file__)), '.git')):
+moddir = dirname(__file__)
+projdir = curdir if moddir == 'datalad' else dirname(moddir)
+if lexists(opj(projdir, '.git')):
     # If under git -- attempt to deduce a better "dynamic" version following git
     try:
         import sys
@@ -24,12 +26,15 @@ if lexists(opj(dirname(dirname(__file__)), '.git')):
         from os.path import dirname
         git = Popen(['git', 'describe', '--abbrev=4', '--dirty', '--match', '[0-9]*\.*'],
                     stdout=PIPE, stderr=PIPE,
-                    cwd=dirname(dirname(__file__)))
+                    cwd=projdir)
         if git.wait() != 0:
-            raise OSError
+            raise OSError("Could not run git describe")
         line = git.stdout.readlines()[0]
         _ = git.stderr.readlines()
         # Just take describe and replace initial '-' with .dev to be more "pythonish"
-        __version__ = line.strip().decode('ascii').replace('-', '.dev', 1)
+        __full_version__ = line.strip().decode('ascii').replace('-', '.dev', 1)
+        # To follow PEP440 we can't have all the git fanciness
+        __version__ = __full_version__.split('-')[0]
     except:
-        __version__ += ".giterror"
+        # just stick to the hard-coded
+        __full_version__ = __version__
