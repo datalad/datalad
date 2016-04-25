bb4d7c2de972abfbaddd286af1b3e1be1b64f1be
Michael Hanke <michael.hanke@gmail.com>
2016-04-25 16:29:58 +0200

    No static interface imports anymore (closes: gh-408)
    This doesn't implement a clever lazy-importing scheme, but it
    nevertheless remove all static interface imports. In a nutshell:
    
    Static imports are replaced with definitions of what needs importing,
    plus an optional definition of target names for Python API and cmdline
    interface.
    
    This moves us into a position to make import more clever/leaner/lazier
    if needed in the future.

--- datalad/api.py
+++ datalad/api.py
@@ -8,37 +8,43 @@
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Python DataLad API exposing user-oriented commands (also available via CLI)"""
 
+from importlib import import_module as _impmod
+
 from .interface.base import update_docstring_with_parameters as _update_docstring
 from .interface.base import get_interface_groups as _get_interface_groups
 from .interface.base import dedent_docstring as _dedent_docstring
-# TODO:  make those lazy!  ATM importing api requires importing nearly everything
-# Ideally all the bindings/docstrings should be generated upon the first
-# access to them from within api module
-from . import interface as _interfaces
 from .distribution.dataset import Dataset
 
 # auto detect all available interfaces and generate a function-based
 # API from them
 
 for _grp_name, _grp_descr, _interfaces in _get_interface_groups():
-    for _intfcls in _interfaces:
-        _intf = _intfcls
+    for _intfspec in _interfaces:
+        # turn the interface spec into an instance
+        _mod = _impmod(_intfspec[0], package='datalad')
+        _intf = getattr(_mod, _intfspec[1])
         _spec = getattr(_intf, '_params_', dict())
 
         # FIXME no longer using an interface class instance
         # convert the parameter SPEC into a docstring for the function
         _update_docstring(_intf.__call__, _spec,
-                          prefix=_dedent_docstring(_intfcls.__doc__),
-                          suffix=_dedent_docstring(_intfcls.__call__.__doc__))
+                          prefix=_dedent_docstring(_intf.__doc__),
+                          suffix=_dedent_docstring(_intf.__call__.__doc__))
         # register the function in the namespace, using the name of the
         # module it is defined in
-        globals()[_intf.__module__.split('.')[-1]] = _intf.__call__
+        if len(_intfspec) > 3:
+            api_name = _intfspec[3]
+        else:
+            api_name = _intf.__module__.split('.')[-1]
+        globals()[api_name] = _intf.__call__
         # cleanup namespace
+        del _mod
+        del _intfspec
         del _intf
-        del _intfcls
 
 # be nice and clean up the namespace properly
 del _interfaces
+del _impmod
 del _get_interface_groups
 del _grp_name
 del _grp_descr
