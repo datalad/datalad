163cb5e953ab14b44d5354cd9ac24b2204c79769
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-05-27 01:52:14 +0200

    Travis: Add needed hosts to user's known_hosts

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -3,4 +3,6 @@
 echo "127.0.0.1  datalad-test" > /etc/hosts
 apt-get install openssh-client
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts
+ssh-keyscan -H datalad-test >> ~/.ssh/known_hosts
 
