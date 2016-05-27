89022505ce6875466fb0b78770bc0886a40b794d
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-05-27 05:31:32 +0200

    Travis: Missed localhost in /etc/hosts

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "127.0.0.1  datalad-test" > /etc/hosts
+echo "127.0.0.1  datalad-test localhost" > /etc/hosts
 apt-get install openssh-client
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
 ssh-keyscan -H localhost >> ~/.ssh/known_hosts
