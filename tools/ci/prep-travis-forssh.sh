0b79ab4a514296a8831afbad20b8afec18537c62
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-05-31 10:50:00 +0200

    Travis ...

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -5,5 +5,5 @@ apt-get install openssh-client
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
 ssh-keyscan -H localhost >> ~/.ssh/known_hosts
 ssh-keyscan -H datalad-test >> ~/.ssh/known_hosts
-ssh-copy-id -i /tmp/dl-test-ssh-id localhost
-ssh-copy-id -i /tmp/dl-test-ssh-id datalad-test
+echo -ne '\n' | ssh-copy-id -i /tmp/dl-test-ssh-id localhost
+echo -ne '\n' | ssh-copy-id -i /tmp/dl-test-ssh-id datalad-test
