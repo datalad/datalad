c9aa2e1c2cf269ba9f8f29a625fade885dddde59
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-05-31 17:03:57 +0200

    Travis: Add datalad-tests

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -2,9 +2,9 @@
 
 echo "127.0.0.1  datalad-test localhost" > /etc/hosts
 apt-get install openssh-client
-echo -e "Host localhost\n\tNoHostAuthenticationForLocalhost yes" >> ~/.ssh/config
+echo -e "Host localhost datalad-tests\n\tNoHostAuthenticationForLocalhost yes" >> ~/.ssh/config
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
 #ssh-keyscan -H localhost >> ~/.ssh/known_hosts
-ssh-keyscan -H datalad-test >> ~/.ssh/known_hosts
+#ssh-keyscan -H datalad-test >> ~/.ssh/known_hosts
 #echo -ne '\n' | ssh-copy-id -i /tmp/dl-test-ssh-id localhost
-echo -ne '\n' | ssh-copy-id -i /tmp/dl-test-ssh-id datalad-test
+#echo -ne '\n' | ssh-copy-id -i /tmp/dl-test-ssh-id datalad-test
