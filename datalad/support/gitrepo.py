df3c40d65e8d481ebdd8f6436c8ab6e18b2aa770
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-05-31 12:19:26 +0200

    TMP: Temporarily circumnavigating a bug in GitPython

--- datalad/support/gitrepo.py
+++ datalad/support/gitrepo.py
@@ -27,7 +27,8 @@ from six import string_types
 from functools import wraps
 
 import git
-from git.exc import GitCommandError, NoSuchPathError, InvalidGitRepositoryError
+from git.exc import GitCommandError, NoSuchPathError, \
+    InvalidGitRepositoryError, BadName
 from git.objects.blob import Blob
 
 from datalad import ssh_manager
@@ -811,11 +812,29 @@ class GitRepo(object):
                 #       with rm.repo.git.custom_environment(GIT_SSH="wrapper_script"):
                 with rm.repo.git.custom_environment(
                         GIT_SSH_COMMAND="ssh -S %s" % cnct.ctrl_path):
+                    try:
+                        rm.push(refspec=refspec, progress=progress, **kwargs)
+                        # TODO: progress +kwargs
+                    except BadName as e:
+                        lgr.error("GitPython reported BadName Exception: %s" % e)
+                        # TODO: There seems to be a bug in GitPython, which leads
+                        # to a BadName exception on a forced update. It somehow
+                        # cannot resolve the short SHA of the OLD COMMIT,
+                        # reported by Git AFTER the push (forced update) was done.
+                        # Document and watch it!
+                        pass
+            else:
+                try:
                     rm.push(refspec=refspec, progress=progress, **kwargs)
                     # TODO: progress +kwargs
-            else:
-                rm.push(refspec=refspec, progress=progress, **kwargs)
-                # TODO: progress +kwargs
+                except BadName as e:
+                    lgr.error("GitPython reported BadName Exception: %s" % e)
+                    # TODO: There seems to be a bug in GitPython, which leads
+                    # to a BadName exception on a forced update. It somehow
+                    # cannot resolve the short SHA of the OLD COMMIT,
+                    # reported by Git AFTER the push (forced update) was done.
+                    # Document and watch it!
+                    pass
 
     def git_get_remote_url(self, name, push=False):
         """Get the url of a remote.
