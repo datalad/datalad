7c0ebc0d05ca137f004cab4c7f22b9f57a4d1aab
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-06-01 02:05:08 +0200

    More on Travis SSH setup...

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -2,11 +2,16 @@
 
 echo "127.0.0.1  datalad-test localhost" > /etc/hosts
 apt-get install openssh-client
-echo -e "Host localhost datalad-tests\n\tNoHostAuthenticationForLocalhost yes" >> ~/.ssh/config
+echo "DEBUG:"
+ls -al ~/.ssh
+echo -e "Host localhost\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host datalad-test\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo "DEBUG:"
+ls -al ~/.ssh
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
-cat /tmp/dl-test-ssh-id.pub >> ~/.ssh/authorized_keys
-chmod og-wx ~/.ssh/authorized_keys
+ssh-copy-id -i /tmp/dl-test-ssh-id localhost
+ssh-copy-id -i /tmp/dl-test-ssh-id datalad-test
+#cat /tmp/dl-test-ssh-id.pub >> ~/.ssh/authorized_keys
+#chmod og-wx ~/.ssh/authorized_keys
 #ssh-keyscan -H localhost >> ~/.ssh/known_hosts
 #ssh-keyscan -H datalad-test >> ~/.ssh/known_hosts
-#echo -ne '\n' | ssh-copy-id -i /tmp/dl-test-ssh-id localhost
-#echo -ne '\n' | ssh-copy-id -i /tmp/dl-test-ssh-id datalad-test
