a8605776f8b424aa4a8202590f144dc6edde4ce6
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-06-01 03:19:20 +0200

    More Travis SSH debugging

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -4,8 +4,9 @@ echo "127.0.0.1  datalad-test localhost" > /etc/hosts
 apt-get install openssh-client
 echo -e "Host localhost\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 echo -e "Host datalad-test\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo "DEBUG:"
-cat ~/.ssh/config
+echo "DEBUG: /etc/ssh/sshd_config:"
+cat /etc/ssh/sshd_config
+chmod 755 ~/.ssh
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
 cat /tmp/dl-test-ssh-id.pub >> ~/.ssh/authorized_keys
-chmod og-wx ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/authorized_keys
