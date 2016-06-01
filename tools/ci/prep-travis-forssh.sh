51efc49e2231e8beee419f9ecf522b76541f960a
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-06-01 11:28:21 +0200

    ...

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -8,8 +8,13 @@ echo -e "Host localhost\n\tStrictHostKeyChecking no\n\tIdentityFile /tmp/dl-test
 echo -e "Host datalad-test\n\tStrictHostKeyChecking no\n\tIdentityFile /tmp/dl-test-ssh-id\n" >> ~/.ssh/config
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
 cat /tmp/dl-test-ssh-id.pub >> ~/.ssh/authorized_keys
+echo "DEBUG: Exit of cat: $?"
+echo "DEBUG: $(ls -sl $HOME/.ssh/authorized_keys)"
 chmod 644 ~/.ssh/authorized_keys
 eval $(ssh-agent)
 ssh-add /tmp/dl-test-ssh-id
-
+echo "DEBUG: test connection ..."
+ssh -v localhost exit
+# don't wait for travis to timeout:
+exit 1
 
