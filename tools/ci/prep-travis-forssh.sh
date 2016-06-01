1cc3bb1c0375a969e73dc345cddc1cb4db5e8685
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-06-01 10:51:12 +0200

    BF: Travis SSH: Add IdentityFile to hosts' config

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -2,9 +2,10 @@
 
 echo "127.0.0.1  datalad-test localhost" > /etc/hosts
 apt-get install openssh-client
-echo -e "Host localhost\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-echo -e "Host datalad-test\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+mkdir -p ~/.ssh
 chmod 755 ~/.ssh
+echo -e "Host localhost\n\tStrictHostKeyChecking no\n\tIdentityFile /tmp/dl-test-ssh-id\n" >> ~/.ssh/config
+echo -e "Host datalad-test\n\tStrictHostKeyChecking no\n\tIdentityFile /tmp/dl-test-ssh-id\n" >> ~/.ssh/config
 ssh-keygen -f /tmp/dl-test-ssh-id -N ""
 cat /tmp/dl-test-ssh-id.pub >> ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/authorized_keys
