2bee9c53f129cd9f695c930173b1c8f88d65148e
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2016-06-01 15:40:56 +0200

    Travis: Why does 'sudo' not work?

--- tools/ci/prep-travis-forssh.sh
+++ tools/ci/prep-travis-forssh.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-sudo echo "127.0.0.1  datalad-test" >> /etc/hosts
-###### why "ssh: Could not resolve hostname datalad-test: Name or service not known"?
-echo "DEBUG:"
-sudo cat /etc/hosts
-sudo apt-get install openssh-client
-echo "DEBUG:"
-sudo cat /etc/hosts
-exit 1
-######
 mkdir -p ~/.ssh
 echo -e "Host localhost\n\tStrictHostKeyChecking no\n\tIdentityFile /tmp/dl-test-ssh-id\n" >> ~/.ssh/config
 echo -e "Host datalad-test\n\tStrictHostKeyChecking no\n\tIdentityFile /tmp/dl-test-ssh-id\n" >> ~/.ssh/config
