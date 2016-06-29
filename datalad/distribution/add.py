cc3f8302c1e7a2d5a28f6622f46f43f3e08fa795
Michael Hanke <michael.hanke@gmail.com>
2016-06-29 11:30:33 +0200

    RF: Proposed API for `add`

---
+++ datalad/distribution/add.py
@@ -0,0 +1,58 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the datalad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""High-level interface for adding dataset components
+
+"""
+
+__docformat__ = 'restructuredtext'
+
+import logging
+from datalad.interface.base import Interface
+from datalad.support.constraints import EnsureStr, EnsureNone
+from datalad.distribution.dataset import EnsureDataset, datasetmethod
+from datalad.support.param import Parameter
+
+lgr = logging.getLogger('datalad.distribution.add')
+
+
+class Add(Interface):
+    """Add a component to an existing dataset
+
+    """
+
+    _params_ = dict(
+        dataset=Parameter(
+            args=("-d", "--dataset"),
+            doc="""specify the dataset to perform the add operation on.  If
+            no dataset is given, an attempt is made to identify the dataset
+            based on the current working directory and/or the `path` given""",
+            constraints=EnsureDataset() | EnsureNone()),
+        path=Parameter(
+            args=("path",),
+            doc="""path/name of the component to be added. The component
+            must either exist on the filesystem already, or a `source`
+            has to be provided.""",
+            nargs="*",
+            constraints=EnsureStr() | EnsureNone()),
+        source=Parameter(
+            args=("-s", "--source",),
+            doc="url or local path of the to be added component's source",
+            constraints=EnsureStr() | EnsureNone()),
+        add_data_to_git=Parameter(
+            args=("--add-data-to-git",),
+            action='store_true',
+            doc="""flag whether to add data directly to Git, instead of
+            tracking data identity only.  Usually this is not desired,
+            as it inflates dataset sizes and impacts flexibility of data
+            transport"""))
+
+    @staticmethod
+    @datasetmethod(name='add')
+    def __call__(dataset=None, path=None, source=None, add_data_to_git=False):
+        raise NotImplementedError
