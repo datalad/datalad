3f3b524ae2b33d8ebf8c7e0837b001d36cea81ca
Gergana Alteva <glalteva@gmail.com>
2016-07-11 15:19:48 -0400

    RF: renamed file

--- datalad/interface/crawl_init.py
+++ datalad/interface/crawl_init.py
@@ -11,9 +11,20 @@
 __docformat__ = 'restructuredtext'
 
 
+
+from os.path import exists, isdir
+from .base import Interface
 from datalad import cfg
 from datalad.config import ConfigManager
 from datalad.consts import CRAWLER_META_CONFIG_PATH, CRAWLER_META_DIR
+from datalad.support.param import Parameter
+from datalad.support.constraints import EnsureStr, EnsureChoice, EnsureNone
+from datalad.crawler.pipeline import initiate_pipeline_config
+
+from logging import getLogger
+lgr = getLogger('datalad.api.crawl')
+
+from .. import cfg
 
 
 class CrawlInit(Interface):
@@ -27,24 +38,17 @@ class CrawlInit(Interface):
         a_href_match=.*\.(tar.*|dat)) \
           # within a dataset having no .datalad/crawl/crawl.cfg
     """
-_params_ = dict(
+    _params_ = dict(
+        init=Parameter(
+            args=("--init",),
+            action="store_true",
+            doc="""flag if user can provide arguments needed for template to initate .datalad/crawl/crawl.cfg"""),
+    )
 
+    @staticmethod
+    def __call__(path=None, dry_run=False, is_pipeline=False, is_template=False, chdir=None, init=False):
 
-
-
-
-
-
-
-
-
-
-init = Parameter(
-args=("--init",),
-action="store_true",
-doc="""flag if user can provide arguments needed for template to initate .datalad/crawl/crawl.cfg"""),
-
-    if init:
-        url = input("URL: ")
-        a_href_match = input("a_href_match: ")
-        extract_tarballs = input("extract_tarballs: ")
+        if init:
+            url = input("URL: ")
+            a_href_match = input("a_href_match: ")
+            extract_tarballs = input("extract_tarballs: ")
