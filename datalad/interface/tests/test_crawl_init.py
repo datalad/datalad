310170312ecd510c1bd7fcd7c86401251aad423b
Gergana Alteva <glalteva@gmail.com>
2016-07-17 20:33:06 -0400

    TST: parametric testing implemented

--- datalad/interface/tests/test_crawl_init.py
+++ datalad/interface/tests/test_crawl_init.py
@@ -16,55 +16,37 @@ from datalad.tests.utils import with_tempfile, chpwd
 from datalad.consts import CRAWLER_META_CONFIG_PATH, CRAWLER_META_DIR
 
 
-# TODO: generalize 3 first tests into 1 parametric... reference git grep 'yield_test'
-
-
 @with_tempfile(mkdir=True)
-def test_crawl_init_openfmri(tmpdir):
-    ar = AnnexRepo(tmpdir)
-    with chpwd(tmpdir):
-        crawl_init(template='openfmri', template_func='superdataset_pipeline')
-        eq_(exists(CRAWLER_META_DIR), True)
-        eq_(exists(CRAWLER_META_CONFIG_PATH), True)
-        f = open(CRAWLER_META_CONFIG_PATH, 'r')
-        contents = f.read()
-        eq_(contents, '[crawl:pipeline]\ntemplate = openfmri\nfunc = superdataset_pipeline\n\n')
-
-
-@with_tempfile(mkdir=True)
-def test_crawl_init_args_dict(tmpdir):
-    ar = AnnexRepo(tmpdir)
-    with chpwd(tmpdir):
-        crawl_init({'dataset': 'ds000001'}, template='openfmri')
-        eq_(exists(CRAWLER_META_DIR), True)
-        eq_(exists(CRAWLER_META_CONFIG_PATH), True)
-        f = open(CRAWLER_META_CONFIG_PATH, 'r')
-        contents = f.read()
-        eq_(contents, '[crawl:pipeline]\ntemplate = openfmri\n_dataset = ds000001\n\n')
-
-
-@with_tempfile(mkdir=True)
-def test_crawl_init_args_list(tmpdir):
-    ar = AnnexRepo(tmpdir)
-    with chpwd(tmpdir):
-        crawl_init(['dataset=ds000001', 'versioned_urls=True'], template='openfmri')
-        eq_(exists(CRAWLER_META_DIR), True)
-        eq_(exists(CRAWLER_META_CONFIG_PATH), True)
-        f = open(CRAWLER_META_CONFIG_PATH, 'r')
-        contents = f.read()
-        eq_(contents, '[crawl:pipeline]\ntemplate = openfmri\n_dataset = ds000001\n_versioned_urls = True\n\n')
-
-
-@with_tempfile(mkdir=True)  # passes
-def test_crawl_init_error(tmpdir):
-    ar = AnnexRepo(tmpdir)
-    with chpwd(tmpdir):
-        assert_raises(ValueError, crawl_init, args=tmpdir)
-
-
-@with_tempfile(mkdir=True)
-def test_crawl_init_wrong_args(tmpdir):
+def _test_crawl_init(args, template, template_func, target_value, tmpdir):
     ar = AnnexRepo(tmpdir, create=True)
     with chpwd(tmpdir):
-        # incorrect argument -- should blow
-        assert_raises(RuntimeError, crawl_init, ['dataset=Baltimore', 'pie=True'], template='openfmri')
+        crawl_init(args=args, template=template, template_func=template_func)
+        eq_(exists(CRAWLER_META_DIR), True)
+        eq_(exists(CRAWLER_META_CONFIG_PATH), True)
+        f = open(CRAWLER_META_CONFIG_PATH, 'r')
+        contents = f.read()
+        eq_(contents, target_value)
+
+
+def test_crawl_init():
+    yield _test_crawl_init, None, 'openfmri', 'superdataset_pipeline', \
+          '[crawl:pipeline]\ntemplate = openfmri\nfunc = superdataset_pipeline\n\n'
+    yield _test_crawl_init, {'dataset': 'ds000001'}, 'openfmri', None, \
+          '[crawl:pipeline]\ntemplate = openfmri\n_dataset = ds000001\n\n'
+    yield _test_crawl_init, ['dataset=ds000001', 'versioned_urls=True'], 'openfmri', None, \
+          '[crawl:pipeline]\ntemplate = openfmri\n_dataset = ds000001\n_versioned_urls = True\n\n'
+
+
+@with_tempfile(mkdir=True)
+def _test_crawl_init_error(args, template, template_func, target_value, tmpdir):
+        ar = AnnexRepo(tmpdir)
+        with chpwd(tmpdir):
+            assert_raises(target_value, crawl_init, args=args, template=template, template_func=template_func)
+
+
+def test_crawl_init_error():
+    yield _test_crawl_init_error, 'tmpdir', None, None, ValueError
+    yield _test_crawl_init_error, ['dataset=Baltimore', 'pie=True'], 'openfmri', None, RuntimeError
+
+
+
