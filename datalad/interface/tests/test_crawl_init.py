1d5e2d76cae4e4cad8416bf8635bdcbd78398c0a
Gergana Alteva <glalteva@gmail.com>
2016-07-18 13:08:58 -0400

    TST: more tests added to increase coverage

--- datalad/interface/tests/test_crawl_init.py
+++ datalad/interface/tests/test_crawl_init.py
@@ -7,8 +7,11 @@
 #
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
+
 from nose.tools import eq_, assert_raises
+from mock import patch
 from ...api import crawl_init
+from collections import OrderedDict
 from os import remove
 from os.path import exists
 from datalad.support.annexrepo import AnnexRepo
@@ -49,4 +52,22 @@ def test_crawl_init_error():
     yield _test_crawl_init_error, ['dataset=Baltimore', 'pie=True'], 'openfmri', None, RuntimeError
 
 
+@with_tempfile(mkdir=True)
+def _test_crawl_init_error_patch(return_value, exc, exc_msg, d):
 
+    ar = AnnexRepo(d, create=True)
+    with patch('datalad.interface.crawl_init.load_pipeline_from_template',
+               return_value=lambda dataset: return_value) as cm:
+        with chpwd(d):
+            try:
+                crawl_init(args=['dataset=Baltimore'], template='openfmri')
+            except Exception as e:
+                eq_(type(e), exc)
+                eq_(str(e), exc_msg)
+
+            cm.assert_called_with('openfmri', None, return_only=True, kwargs=OrderedDict([('dataset', 'Baltimore')]))
+
+
+def test_crawl_init_error_patch():
+    yield _test_crawl_init_error_patch, [], ValueError, "returned pipeline is empty"
+    yield _test_crawl_init_error_patch, {1: 2}, ValueError, "pipeline should be represented as a list. Got: {1: 2}"
