7e4ac315507a4aceee6120ea641b172ca6545ed6
Yaroslav Halchenko <debian@onerussian.com>
2016-08-12 14:31:23 -0400

    ENH: allow cookies db fail to access them, but still to function + more logging

--- datalad/support/cookies.py
+++ datalad/support/cookies.py
@@ -13,7 +13,13 @@ import shelve
 import appdirs
 import os.path
 from six import PY2
+
 from .network import get_tld
+from datalad.dochelpers import exc_str
+
+import logging
+lgr = logging.getLogger('datalad.cookies')
+
 
 # FIXME should make into a decorator so that it closes the cookie_db upon exiting whatever func uses it
 class CookiesDB(object):
@@ -42,8 +48,12 @@ class CookiesDB(object):
         if not os.path.exists(cookies_dir):
             os.makedirs(cookies_dir)
 
-        db = self._cookies_db = shelve.open(filename, writeback=True)
-        atexit.register(db.close)
+        lgr.debug("Opening cookies DB %s", filename)
+        try:
+            db = self._cookies_db = shelve.open(filename, writeback=True)
+            atexit.register(db.close)
+        except Exception as exc:
+            lgr.warning("Failed to open cookies DB %s: %s", filename, exc_str(exc))
 
     def _get_provider(self, url):
         if self._cookies_db is None:
@@ -54,13 +64,27 @@ class CookiesDB(object):
         return tld
 
     def __getitem__(self, url):
-        return self._cookies_db[self._get_provider(url)]
+        try:
+            return self._cookies_db[self._get_provider(url)]
+        except Exception as exc:
+            lgr.warning("Failed to get a cookie for %s: %s",
+                        url, exc_str(exc))
+            return None
 
     def __setitem__(self, url, value):
-        self._cookies_db[self._get_provider(url)] = value
+        try:
+            self._cookies_db[self._get_provider(url)] = value
+        except Exception as exc:
+            lgr.warning("Failed to set a cookie for %s: %s",
+                        url, exc_str(exc))
 
     def __contains__(self, url):
-        return self._get_provider(url) in self._cookies_db
+        try:
+            return self._get_provider(url) in self._cookies_db
+        except Exception as exc:
+            lgr.warning("Failed to check for having a cookie for %s: %s",
+                        url, exc_str(exc))
+            return None
 
 
 # TODO -- convert into singleton pattern for CookiesDB
