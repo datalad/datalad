42d9313ae32cec5c67a8618bbb356df4f26b8da9
Michael Hanke <michael.hanke@gmail.com>
2016-09-25 10:32:09 +0200

    TST: Document current non-reproducibility of tarballs

--- datalad/export/tests/test_tarball.py
+++ datalad/export/tests/test_tarball.py
@@ -11,10 +11,12 @@
 
 from datalad.api import Dataset
 from datalad.api import export
-from nose.tools import assert_true, assert_equal, assert_raises
+from nose.tools import assert_true, assert_not_equal, assert_raises
 from datalad.tests.utils import with_tree, with_tempfile
 from datalad.utils import chpwd
+from datalad.utils import md5sum
 import os
+import time
 from os.path import join as opj
 
 
@@ -38,8 +40,20 @@ def test_failure(path):
 @with_tree(_dataset_template)
 def test_tarball(path):
     ds = Dataset(opj(path, 'ds')).create(force=True)
+    ds.save(auto_add_changes=True)
     with chpwd(path):
         ds.export('tarball')
-    assert_true(os.path.exists(opj(path, 'datalad_{}.tar.gz'.format(ds.id))))
-    ds.export('tarball', output=opj(path, 'myexport'))
-    assert_true(os.path.exists(opj(path, 'myexport.tar.gz'.format(ds.id))))
+    default_outname = opj(path, 'datalad_{}.tar.gz'.format(ds.id))
+    assert_true(os.path.exists(default_outname))
+    custom_outname = opj(path, 'myexport.tar.gz')
+    # feed in without extension
+    ds.export('tarball', output=custom_outname[:-7])
+    assert_true(os.path.exists(custom_outname))
+    custom1_md5 = md5sum(custom_outname)
+    # encodes the original tarball filename -> different checksum, despit
+    # same content
+    assert_not_equal(md5sum(default_outname), custom1_md5)
+    time.sleep(1.1)
+    ds.export('tarball', output=custom_outname)
+    # encodes mtime -> different checksum, despit same content and name
+    assert_not_equal(md5sum(custom_outname), custom1_md5)
