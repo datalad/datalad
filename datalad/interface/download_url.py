9ae60ccd59faa4e6a88ff04809cdecea5f6de675
Michael Hanke <michael.hanke@gmail.com>
2016-09-27 09:34:38 +0200

    BF: Rectify missing import, but place in file where it is actually needed

--- datalad/interface/download_url.py
+++ datalad/interface/download_url.py
@@ -72,7 +72,7 @@ class DownloadURL(Interface):
           downloaded successfully files
         """
 
-        from ..downloaders import Providers
+        from ..downloaders.providers import Providers
 
         urls = assure_list_from_str(urls)
 
