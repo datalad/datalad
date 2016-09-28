24357eb59730ccea54680c8d8202e417a9492471
Michael Hanke <michael.hanke@gmail.com>
2016-09-28 13:56:50 +0200

    Import env var support description as code

--- datalad/interface/common_cfg.py
+++ datalad/interface/common_cfg.py
@@ -60,10 +60,90 @@ definitions = {
         'destination': 'global',
     },
     'datalad.locations.cache': {
-        'ui': ('question',
-               {'title': 'Cache directory',
-                'text': 'Where should datalad cache files?'}),
+        'ui': ('question', {
+               'title': 'Cache directory',
+               'text': 'Where should datalad cache files?'}),
         'destination': 'global',
         'default': dirs.user_cache_dir,
+    },
+    'datalad.exc.str.tblimit': {
+        'ui': ('question', {
+               'title': 'This flag is used by the datalad extract_tb function which extracts and formats stack-traces. It caps the number of lines to DATALAD_EXC_STR_TBLIMIT of pre-processed entries from traceback.'}),
+    },
+    'datalad.tests.keeptemp': {
+        'ui': ('yesno', {
+               'title': 'Function rmtemp will not remove temporary file/directory created for testing if this flag is set'}),
+        'type': EnsureBool(),
+    },
+    'datalad.tests.tempdir': {
+        'ui': ('question', {
+               'title': 'Create a temporary directory at location specified by this flag. It is used by tests to create a temporary git directory while testing git annex archives etc'}),
+    },
+    'datalad.tests.nonetwork': {
+        'ui': ('yesno', {
+               'title': 'Skips network tests completely if this flag is set Examples include test for s3, git_repositories, openfmri etc'}),
+        'type': EnsureBool(),
+    },
+    'datalad.tests.ssh': {
+        'ui': ('yesno', {
+               'title': 'Skips SSH tests if this flag is **not** set'}),
+        'type': EnsureBool(),
+    },
+    'datalad.tests.noteardown': {
+        'ui': ('yesno', {
+               'title': 'Does not execute teardown_package which cleans up temp files and directories created by tests if this flag is set'}),
+        'type': EnsureBool(),
+    },
+    'datalad.api.always.render': {
+        'ui': ('yesno', {
+               'title': 'Would make api functions always use a version with cmdline output renderer (i.e. the one with `_` suffix)'}),
+        'type': EnsureBool(),
+    },
+    'datalad.logtraceback': {
+        'ui': ('question', {
+               'title': 'Runs TraceBack function with collide set to True, if this flag is set to "collide". This replaces any common prefix between current traceback log and previous invocation with "..."'}),
+    },
+    'datalad.usecassette': {
+        'ui': ('question', {
+               'title': 'Specifies the location of the file to record network transactions by the VCR module. Currently used by when testing custom special remotes'}),
+    },
+    'datalad.cmd.protocol': {
+        'ui': ('question', {
+               'title': 'Specifies the protocol number used by the Runner to note shell command or python function call times and allows for dry runs. "externals-time" for ExecutionTimeExternalsProtocol, "time" for ExecutionTimeProtocol and "null" for NullProtocol. Any new DATALAD_CMD_PROTOCOL has to implement datalad.support.protocol.ProtocolInterface'}),
+    },
+    'datalad.cmd.protocol.prefix': {
+        'ui': ('question', {
+               'title': 'Sets a prefix to add before the command call times are noted by DATALAD_CMD_PROTOCOL.'}),
+    },
+    'datalad.protocol.remote': {
+        'ui': ('yesno', {
+               'title': 'Binary flag to specify whether to test protocol interactions of custom remote with annex'}),
+        'type': EnsureBool(),
+    },
+    'datalad.loglevel': {  # -> datalad.log.level?
+        'ui': ('question', {
+               'title': 'Used for control the verbosity of logs printed to stdout while running datalad commands/debugging'}),
+    },
+    'datalad.log.timestamp': {
+        'ui': ('yesno', {
+               'title': 'Used to add timestamp to datalad logs'}),
+        'type': EnsureBool(),
+    },
+    'datalad.run.cmdline.tests': {  # -> datalad.tests.runcmdline?
+        'ui': ('yesno', {
+               'title': 'Binary flag to specify if shell testing using shunit2 to be carried out'}),
+        'type': EnsureBool(),
+    },
+    'datalad.temp.fs': {
+        'ui': ('question', {
+               'title': 'Specify the temporary file system to use as loop device for testing DATALAD_TESTS_TEMPDIR creation'}),
+    },
+    'datalad.temp.fs.size': {
+        'ui': ('question', {
+               'title': 'Specify the size of temporary file system to use as loop device for testing DATALAD_TESTS_TEMPDIR creation'}),
+    },
+    'datalad.nonlo': {
+        'ui': ('question', {
+               'title': 'Specifies network interfaces to bring down/up for testing. Currently used by travis.'}),
     },
 }
