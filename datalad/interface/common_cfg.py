7045a147508b59ae52129c423ac07f2c384faec0
Michael Hanke <michael.hanke@gmail.com>
2016-09-28 13:26:08 +0200

    Sort definitions by name

--- datalad/interface/common_cfg.py
+++ datalad/interface/common_cfg.py
@@ -18,13 +18,6 @@ dirs = AppDirs("datalad", "datalad.org")
 
 
 definitions = {
-    'datalad.locations.cache': {
-        'ui': ('question',
-               {'title': 'Cache directory',
-                'text': 'Where should datalad cache files?'}),
-        'destination': 'global',
-        'default': dirs.user_cache_dir,
-    },
     # this is actually used in downloaders, but kept cfg name original
     'datalad.crawl.cache': {
         'ui': ('yesno', {
@@ -33,25 +26,19 @@ definitions = {
         'destination': 'local',
         'type': bool,
     },
-    'datalad.crawl.dryrun': {
-        'ui': ('yesno', {
-               'title': 'Crawler dry-run',
-               'text': 'Should the crawler ... I AM NOT QUITE SURE WHAT?'}),
-        'destination': 'local',
-        'type': EnsureBool(),
-    },
-    'datalad.externals.nda.dbserver': {
-        'ui': ('question', {
-               'title': 'NDA database server',
-               'text': 'Hostname of the database server'}),
-        'destination': 'global',
-    },
     'datalad.crawl.default_backend': {
         'ui': ('question', {
                'title': 'Default annex backend',
                # XXX we could add choices... but might get out of sync
                'text': 'Content hashing method to be used by git-annex'}),
         'destination': 'dataset',
+    },
+    'datalad.crawl.dryrun': {
+        'ui': ('yesno', {
+               'title': 'Crawler dry-run',
+               'text': 'Should the crawler ... I AM NOT QUITE SURE WHAT?'}),
+        'destination': 'local',
+        'type': EnsureBool(),
     },
     'datalad.crawl.init_direct': {
         'ui': ('question', {
@@ -65,5 +52,18 @@ definitions = {
                'text': 'Should the crawler tidy up datasets (git gc, repack, clean)?'}),
         'destination': 'global',
         'type': EnsureBool(),
+    },
+    'datalad.externals.nda.dbserver': {
+        'ui': ('question', {
+               'title': 'NDA database server',
+               'text': 'Hostname of the database server'}),
+        'destination': 'global',
+    },
+    'datalad.locations.cache': {
+        'ui': ('question',
+               {'title': 'Cache directory',
+                'text': 'Where should datalad cache files?'}),
+        'destination': 'global',
+        'default': dirs.user_cache_dir,
     },
 }
