5eeb0b76efae20cddb9a1ab84faf1dd81b0ecab0
Michael Hanke <michael.hanke@gmail.com>
2016-10-16 11:24:00 +0200

    NF: Proposal for datalad-native meta data format
    Which invents as little as possible. Objectives:
    
    1) Compact
    2) Suitable for hand-editing (we have two JSON-based ones already)
    3) Flexible enough to be able to capture pretty much anything I could
    think of
    
    Choosing Debian's RFC822 felt natural. Pretty much all these files are
    composed by hand (that proof enough).
    
    The idea is to merged the various ideas from Debian's metadata files
    into a single spec file (control, copyright, ...). If necessary, we
    could have multiple files of course, but it doesn't look like we need it
    (yet).
    
    At this point, the implementation represents the minimal technical
    infrastructure to support this format on a demo level, and to get the
    discussion going.
    
    My goal is to implement support for the relevant parts from here:
    https://wiki.debian.org/UpstreamMetadata#Fields
    
    Main advantage would be stellar metadata support when generating Debian
    packages for datalad datasets.
    
    I don't think we need any more than that, and it is already somewhat
    based on the DOAP vocabulary. At this point, however, I don't see the
    need for YAML, and stuck to RFC822.
    
    If we would ever want to, we could even adopt this as an extension:
    https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
    (not saying we need it!).

---
+++ datalad/metadata/parsers/tests/test_rfc822.py
@@ -0,0 +1,81 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the datalad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Test BIDS meta data parser """
+
+from os.path import join as opj
+from simplejson import dumps
+from datalad.distribution.dataset import Dataset
+from datalad.metadata.parsers.datalad_rfc822 import MetadataParser
+from nose.tools import assert_true, assert_false, assert_equal
+from datalad.tests.utils import with_tree, with_tempfile
+
+
+@with_tree(tree={'.datalad': {'control': ''}})
+def test_has_metadata(path):
+    ds = Dataset(path)
+    p = MetadataParser(ds)
+    assert_true(p.has_metadata())
+    assert_equal(p.get_core_metadata_filenames(),
+                 [opj(path, '.datalad', 'control')])
+
+
+@with_tempfile(mkdir=True)
+def test_has_no_metadata(path):
+    ds = Dataset(path)
+    p = MetadataParser(ds)
+    assert_false(p.has_metadata())
+    assert_equal(p.get_core_metadata_filenames(), [])
+
+
+@with_tree(tree={'.datalad': {'control': """\
+Package: studyforrest_phase2
+Version: 1.0.0-rc3
+Description: Basic summary
+ A text with arbitrary length and content that can span multiple
+ .
+ paragraphs (this is a new one)
+License: CC0
+ The person who associated a work with this deed has dedicated the work to the
+ public domain by waiving all of his or her rights to the work worldwide under
+ copyright law, including all related and neighboring rights, to the extent
+ allowed by law.
+ .
+ You can copy, modify, distribute and perform the work, even for commercial
+ purposes, all without asking permission.
+Maintainer: Mike One <mike@example.com>,
+            Anna Two <anna@example.com>,
+Homepage: http://studyforrest.org
+"""}})
+def test_get_metadata(path):
+
+    ds = Dataset(path)
+    meta = MetadataParser(ds).get_metadata('ID')
+    assert_equal(
+        dumps(meta, sort_keys=True, indent=2),
+        """\
+{
+  "@context": "http://schema.org/",
+  "@id": "ID",
+  "author": [
+    "Mike One <mike@example.com>",
+    "Anna Two <anna@example.com>"
+  ],
+  "dcterms:conformsTo": "http://docs.datalad.org/metadata.html#v0-1",
+  "description": [
+    "Basic summary",
+    "A text with arbitrary length and content that can span multiple\\nparagraphs (this is a new one)"
+  ],
+  "license": [
+    "CC0",
+    "The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.\\nYou can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission."
+  ],
+  "name": "studyforrest_phase2",
+  "type": "Dataset"
+}""")
+
