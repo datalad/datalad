f6edfe82389a51a174576737e08f22b509f6a5e9
Michael Hanke <michael.hanke@gmail.com>
2017-01-25 09:53:22 +0100

    DOC: Some minor update

--- datalad/distribution/publish.py
+++ datalad/distribution/publish.py
@@ -125,8 +125,8 @@ def _publish_dataset(ds, remote, refspec, paths, annex_copy_options):
             ds.config.get('remote.{}.annex-ignore', False):
         lgr.info("Publishing data of dataset {0} ...".format(ds))
         pblshd = ds.repo.copy_to(files=paths,
-                                remote=remote,
-                                options=annex_copy_options)
+                                 remote=remote,
+                                 options=annex_copy_options)
         published += pblshd
 
     return published, skipped
@@ -136,8 +136,22 @@ class Publish(Interface):
     """Publish a dataset to a known :term:`sibling`.
 
     This makes the last saved state of a dataset available to a sibling
-    or special remote data store of the dataset which must already exist
-    and be known to the dataset.
+    or special remote data store of a dataset. Any target sibling must already
+    exist and be known to the dataset.
+
+    Optionally, it is possible to limit publication to change sets relative
+    to a particular point in the version history of a dataset (e.g. a release
+    tag). By default, the state of the local dataset is evaluated against the
+    last known state of the target sibling. Actual publication is only attempted
+    if there was a change compared to the reference state, in order to speed up
+    processing of large collections of datasets. Evaluation with respect to
+    a particular "historic" state is only supported in conjunction with a
+    specified reference dataset. Change sets are also evaluated recursively, i.e.
+    only those subdatasets are published where a change was recorded that is
+    reflected in to current state of the top-level reference dataset.
+
+    Only publication of saved changes is supported. Any unsaved changes in a
+    dataset (hierarchy) have to be saved before publication.
 
     .. note::
       Power-user info: This command uses :command:`git push`, and :command:`git annex copy`
@@ -152,23 +166,19 @@ class Publish(Interface):
     #        upstream set up before, so you can use just "datalad publish" next
     #        time.
 
-    # TODO: Doc!
-
     _params_ = dict(
         dataset=Parameter(
             args=("-d", "--dataset"),
             metavar='DATASET',
-            doc="""specify the dataset to publish. If no dataset is given, an
-            attempt is made to identify the dataset based on the current
-            working directory""",
+            doc="""specify the (top-level) dataset to be published. If no dataset
+            is given, the datasets are determined based on the input arguments""",
             constraints=EnsureDataset() | EnsureNone()),
         to=Parameter(
             args=("--to",),
             metavar='LABEL',
-            doc="""sibling name identifying the publication target. If no
-            destination is given an attempt is made to identify the target
-            based on the dataset's configuration (i.e. a set up tracking
-            branch)""",
+            doc="""name of the target sibling. If no name is given an attempt is
+            made to identify the target based on the dataset's configuration
+            (i.e. a configured tracking branch)""",
             # TODO: See TODO at top of class!
             constraints=EnsureStr() | EnsureNone()),
         since=Parameter(
@@ -236,7 +246,6 @@ class Publish(Interface):
             raise ValueError(
                 'cannot publish content that is not available locally: %s',
                 unavailable_paths)
-        results = []
 
         # here is the plan
         # 1. figure out remote to publish to
@@ -248,7 +257,6 @@ class Publish(Interface):
         ds_remote_info = {}
         for ds_path in content_by_ds:
             ds = Dataset(ds_path)
-            # TODO move detection of publication dependencies into this loop too!
             if to is None:
                 # we need an upstream remote, if there's none given. We could
                 # wait for git push to complain, but we need to explicitly
@@ -281,7 +289,6 @@ class Publish(Interface):
             else:
                 # all good: remote given and is known
                 ds_remote_info[ds_path] = {'remote': to}
-            # TODO more per repo apriori checks here?
 
         if dataset and since:
             # remove all unmodified components from the spec
