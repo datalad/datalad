c19fd44411d77474df91eeb8cff0a6e613b0b068
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2017-02-14 14:45:21 +0100

    BF: AnnexRepo.commit didn't work with 'git annex proxy' in direct mode when used with explicit paths to commit

--- datalad/support/annexrepo.py
+++ datalad/support/annexrepo.py
@@ -39,6 +39,7 @@ from git import InvalidGitRepositoryError
 
 from datalad import ssh_manager
 from datalad.dochelpers import exc_str
+from datalad.dochelpers import borrowdoc
 from datalad.utils import linux_distribution_name
 from datalad.utils import nothing_cm
 from datalad.utils import auto_repr
@@ -1560,30 +1561,10 @@ class AnnexRepo(GitRepo, RepoInterface):
             self._batched.close()
         super(AnnexRepo, self).precommit()
 
-    def commit(self, msg=None, options=None, _datalad_msg=False):
-        """
-
-        Parameters
-        ----------
-        msg: str
-        options: list of str
-          cmdline options for git-commit
-        """
+    @borrowdoc(GitRepo)
+    def commit(self, msg=None, options=None, _datalad_msg=False, careless=True):
         self.precommit()
-        if self.is_direct_mode():
-            if _datalad_msg:
-                msg = self._get_prefixed_commit_msg(msg)
-            if not msg:
-                if options:
-                    if "--allow-empty-message" not in options:
-                        options.append("--allow-empty-message")
-                else:
-                    options = ["--allow-empty-message"]
-
-            self.proxy(['git', 'commit'] + (['-m', msg] if msg else []) +
-                       (options if options else []), expect_stderr=True)
-        else:
-            super(AnnexRepo, self).commit(msg, options, _datalad_msg=_datalad_msg)
+        super(AnnexRepo, self).commit(msg, options, _datalad_msg=_datalad_msg)
 
     @normalize_paths(match_return_type=False)
     def remove(self, files, force=False, **kwargs):
