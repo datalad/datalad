5258caf4a91e2f42d00b132fb00c5e65fa5bd2ac
Michael Hanke <michael.hanke@gmail.com>
2017-04-23 10:22:56 +0200

    DOC: Basic documentation for the result helpers

--- datalad/interface/results.py
+++ datalad/interface/results.py
@@ -25,6 +25,23 @@ lgr = logging.getLogger('datalad.interface.results')
 
 def get_status_dict(action=None, ds=None, path=None, type_=None, logger=None,
                     refds=None, status=None, message=None, **kwargs):
+    """Helper to create a result dictionary.
+
+    Most arguments match their key in the resulting dict. Only exceptions are
+    listed here.
+
+    Parameters
+    ----------
+    ds : Dataset instance
+      If given, the `path` and `type` values are populated with the path of the
+      datasets and 'dataset' as the type. Giving additional values for both
+      keys will overwrite these pre-populated values.
+
+    Returns
+    -------
+    dict
+    """
+
     d = {}
     if action:
         d['action'] = action
@@ -53,11 +70,18 @@ def get_status_dict(action=None, ds=None, path=None, type_=None, logger=None,
 def results_from_paths(paths, action=None, type_=None, logger=None, refds=None,
                        status=None, message=None):
     """
+    Helper to yield analog result dicts for each path in a sequence.
+
     Parameters
     ----------
     message: str
       A result message. May contain `%s` which will be replaced by the
       respective `path`.
+
+    Returns
+    -------
+    generator
+
     """
     for p in assure_list(paths):
         yield get_status_dict(
@@ -66,15 +90,22 @@ def results_from_paths(paths, action=None, type_=None, logger=None, refds=None,
 
 
 def is_ok_dataset(r):
+    """Convenience test for a non-failure dataset-related result dict"""
     return r.get('status', None) == 'ok' and r.get('type', None) == 'dataset'
 
 
 class ResultXFM(object):
+    """Abstract definition of the result transformer API"""
     def __call__(self, res):
+        """This is called with one result dict at a time"""
         raise NotImplementedError
 
 
 class YieldDatasets(ResultXFM):
+    """Result transformer to return a Dataset instance from matching result.
+
+    `None` is return for any other result.
+    """
     def __call__(self, res):
         if res.get('type', None) == 'dataset':
             return Dataset(res['path'])
@@ -83,6 +114,11 @@ class YieldDatasets(ResultXFM):
 
 
 class YieldRelativePaths(ResultXFM):
+    """Result transformer to return relative paths for a result
+
+    Relative paths are determined from the 'refds' value in the result. If
+    no such value is found, `None` is returned.
+    """
     def __call__(self, res):
         refpath = res.get('refds', None)
         if refpath:
@@ -90,7 +126,14 @@ class YieldRelativePaths(ResultXFM):
 
 
 class YieldField(ResultXFM):
+    """Result transformer to return an arbitrary value from a result dict"""
     def __init__(self, field):
+        """
+        Parameters
+        ----------
+        field : str
+          Key of the field to return.
+        """
         self.field = field
 
     def __call__(self, res):
@@ -100,6 +143,9 @@ class YieldField(ResultXFM):
             lgr.debug('rejected by return value configuration: %s', res)
 
 
+# a bunch of convenience labels for common result transformers
+# the API `result_xfm` argument understand any of these labels and
+# applied the corresponding callable
 known_result_xfms = {
     'datasets': YieldDatasets(),
     'paths': YieldField('path'),
@@ -108,6 +154,25 @@ known_result_xfms = {
 
 
 def annexjson2result(d, ds, **kwargs):
+    """Helper to convert an annex JSON result to a datalad result dict
+
+    Info from annex is rather heterogenous, partly because some of it
+    our support functions are faking.
+
+    This helper should be extended with all needed special cases to
+    homogenize the information.
+
+    Parameters
+    ----------
+    d : dict
+      Annex info dict.
+    ds : Dataset instance
+      Used to determine absolute paths for `file` results. This dataset
+      is not used to set `refds` in the result, pass this as a separate
+      kwarg if needed.
+    **kwargs
+      Passes as-is to `get_status_dict`. Must not contain `refds`.
+    """
     lgr.debug('received JSON result from annex: %s', d)
     res = get_status_dict(**kwargs)
     res['status'] = 'ok' if d.get('success', False) is True else 'error'
