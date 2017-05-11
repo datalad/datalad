1d79db9ef8bb147d85ae646eb16ab40d21fa739e
Yaroslav Halchenko <debian@onerussian.com>
2017-05-11 10:47:12 -0400

    BF: quote all paths in the vars definitions in the hook

--- datalad/distribution/tests/test_create_sibling.py
+++ datalad/distribution/tests/test_create_sibling.py
@@ -72,7 +72,7 @@ def _test_correct_publish(target_path, rootds=False, flat=True):
 
     hook_path = _path_(target_path, '.git/hooks/post-update')
     ok_file_has_content(hook_path,
-                        '.*\ndsdir=%s\n.*' % target_path,
+                        '.*\ndsdir="%s"\n.*' % target_path,
                         re_=True,
                         flags=re.DOTALL)
     # correct ls_json command in hook content (path wrapped in "quotes)
