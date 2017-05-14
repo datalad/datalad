7c6b6b76a90bc3d59d48506ede13cf2ec9570c17
Michael Hanke <michael.hanke@gmail.com>
2017-05-14 14:39:17 +0200

    DOC: Sort plumbing commands in help

--- datalad/interface/__init__.py
+++ datalad/interface/__init__.py
@@ -75,7 +75,7 @@ _group_plumbing = (
         ('datalad.distribution.clone', 'Clone'),
         ('datalad.distribution.create_test_dataset', 'CreateTestDataset',
          'create-test-dataset'),
+        ('datalad.distribution.siblings', 'Siblings', 'siblings'),
         ('datalad.support.sshrun', 'SSHRun', 'sshrun'),
         ('datalad.distribution.subdatasets', 'Subdatasets', 'subdatasets'),
-        ('datalad.distribution.siblings', 'Siblings', 'siblings'),
     ])
