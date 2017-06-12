c51446bd5ebb0f49e98a99e2fc1277e926bf1b4f
Michael Hanke <michael.hanke@gmail.com>
2017-06-12 14:14:20 +0200

    ENH: Make `diff` work in direct mode and with direct mode subdatasets

--- datalad/interface/diff.py
+++ datalad/interface/diff.py
@@ -73,7 +73,8 @@ def _translate_type(mode, ap, prop):
 
 def _parse_git_diff(dspath, diff_thingie=None, paths=None,
                     ignore_submodules='none', staged=False):
-    cmd = ['git', 'diff', '--raw',
+    # use '--work-tree=.' to get direct omde to cooperate
+    cmd = ['git', '--work-tree=.', 'diff', '--raw',
            # file names NULL terminated
            '-z',
            # how to treat submodules (see git diff docs)
