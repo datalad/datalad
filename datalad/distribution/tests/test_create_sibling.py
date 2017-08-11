d44fa0b8c585411fd3ce58775bc8fedc317619da
Yaroslav Halchenko <debian@onerussian.com>
2017-08-11 09:38:59 -0400

    BF: fixing  (with Ben"s help) for the bugs with unicode and doctest in borrowed code

--- datalad/distribution/tests/test_create_sibling.py
+++ datalad/distribution/tests/test_create_sibling.py
@@ -14,6 +14,7 @@ from os import chmod
 import stat
 import re
 from os.path import join as opj, exists
+from six import text_type
 
 from ..dataset import Dataset
 from datalad.api import publish, install, create_sibling
@@ -170,7 +171,7 @@ def test_target_ssh_simple(origin, src_path, target_rootpath):
             name="local_target_alt",
             sshurl="ssh://localhost",
             target_dir=target_path)
-    ok_(str(cm.exception).startswith(
+    ok_(text_type(cm.exception).startswith(
         "Target path %s already exists. And it fails to rmdir" % target_path))
     if src_is_annex:
         target_description = AnnexRepo(target_path, create=False).get_description()
