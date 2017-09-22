e344563050466a317bbae6c5be62ab7a786f8adc
Michael Hanke <michael.hanke@gmail.com>
2017-09-22 12:23:29 -0400

    TST: Aggregation of aggregated metadata

--- datalad/metadata/tests/test_aggregation.py
+++ datalad/metadata/tests/test_aggregation.py
@@ -36,20 +36,49 @@ from datalad.tests.utils import swallow_outputs
 from datalad.tests.utils import skip_direct_mode
 
 
-@with_tempfile(mkdir=True)
+_dataset_hierarchy_template = {
+    'origin': {
+        'dataset_description.json': """
+{
+    "Name": "mother_äöü東"
+}""",
+        'sub': {
+            'dataset_description.json': """
+{
+    "Name": "child_äöü東"
+}""",
+            'subsub': {
+                'dataset_description.json': """
+            {
+    "Name": "grandchild_äöü東"
+}"""}}}}
+
+
+@with_tree(tree=_dataset_hierarchy_template)
 @skip_direct_mode  #FIXME
 def test_basic_aggregate(path):
     # TODO give datasets some more metadata to actually aggregate stuff
-    base = Dataset(path).create()
-    sub = base.create('sub')
+    base = Dataset(opj(path, 'origin')).create(force=True)
+    sub = base.create('sub', force=True)
     base.metadata(sub.path, init=dict(homepage='this'), apply2global=True)
-    subsub = base.create(opj('sub', 'subsub'))
+    subsub = base.create(opj('sub', 'subsub'), force=True)
+    base.add('.', recursive=True)
     ok_clean_git(base.path)
     base.aggregate_metadata(recursive=True)
     ok_clean_git(base.path)
     direct_meta = base.metadata(recursive=True, return_type='list')
+    # loose the deepest dataset
+    sub.uninstall('subsub', check=False)
+    # no we should eb able to reaggregate metadata, and loose nothing
+    # because we can aggregate aggregated metadata of subsub from sub
+    base.aggregate_metadata(recursive=True)
+    # same result for aggregate query than for (saved) direct query
+    agg_meta = base.metadata(recursive=True, return_type='list')
+    for d, a in zip(direct_meta, agg_meta):
+        print(d['path'], a['path'])
+        assert_dict_equal(d, a)
     # no we can throw away the subdataset tree, and loose no metadata
-    base.uninstall('sub', recursive=True)
+    base.uninstall('sub', recursive=True, check=False)
     assert(not sub.is_installed())
     ok_clean_git(base.path)
     # same result for aggregate query than for (saved) direct query
