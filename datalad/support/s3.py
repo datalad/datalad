d85b0591a63111ed2b933e2211d971f14fc4b5df
Yaroslav Halchenko <debian@onerussian.com>
2017-11-28 22:41:19 -0500

    ENH: gen_bucket_test2_obscurenames_versioned

--- datalad/support/s3.py
+++ datalad/support/s3.py
@@ -1,4 +1,4 @@
-# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8  -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
@@ -24,8 +24,8 @@ import logging
 import datalad.log  # Just to have lgr setup happen this one used a script
 lgr = logging.getLogger('datalad.s3')
 
-from ..dochelpers import exc_str
-from .exceptions import DownloadError, AccessDeniedError
+from datalad.dochelpers import exc_str
+from datalad.support.exceptions import DownloadError, AccessDeniedError
 
 from six.moves.urllib.request import urlopen, Request
 from six.moves.urllib.parse import urlparse, urlunparse
@@ -52,7 +52,7 @@ def _get_bucket_connection(credential):
     # with different resources. Thus for now just making an option which
     # one to use
     # do full shebang with entering credentials
-    from ..downloaders.credentials import AWS_S3
+    from datalad.downloaders.credentials import AWS_S3
     credential = AWS_S3(credential, None)
     if not credential.is_known:
         credential.enter_new()
@@ -260,6 +260,30 @@ def gen_bucket_test1_dirs():
     files("d1/file1.txt")
     # and then delete it and place it back
     files("d1", load="smth")
+
+
+def gen_bucket_test2_obscurenames_versioned():
+    # in principle bucket name could also contain ., but boto doesn't digest it
+    # well
+    bucket_name = 'datalad-test2-obscurenames-versioned'
+    bucket = gen_test_bucket(bucket_name)
+    bucket.configure_versioning(True)
+
+    # Enable web access to that bucket to everyone
+    bucket.configure_website('index.html')
+    set_bucket_public_access_policy(bucket)
+
+    files = VersionedFilesPool(bucket)
+
+    # http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingMetadata.html
+    files("f 1", load="")
+    files("f [1][2]")
+    files(u"юникод")
+    files(u"юни/код")
+    # all fancy ones at once
+    files("f!-_.*'( )")
+    # the super-fancy which aren't guaranteed to be good idea (as well as [] above)
+    files("f &$=@:+,?;")
 
 
 def get_versioned_url(url, guarantee_versioned=False, return_all=False, verify=False,
