47a5ab985e85c4870888ef3c9424549978a73ea2
Michael Hanke <michael.hanke@gmail.com>
2017-12-26 10:01:46 +0100

    Trying to install pybids straight from github until next release
    We need the last few PRs to make this work, not sure if that syntax will
    work.
    
    BF: setuptools doesn't like simple VCS URLs in setup.py

--- setup.py
+++ setup.py
@@ -100,7 +100,9 @@ requires = {
         'exifread',  # EXIF metadata
         'python-xmp-toolkit',  # XMP metadata, also requires 'exempi' to be available locally
         'pydicom',  # DICOM metadata
-        'pybids',  # BIDS metadata
+        # ATM we need an unreleased version of pybids, added to requirements-devel.txt
+        # TODO revert
+        #'pybids',  # BIDS metadata
         'Pillow',  # generic image metadata
         'nibabel',  # NIfTI metadata
         'pandas',  # bids2scidata export
