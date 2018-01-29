b93a511ac7c0fa3f67a5812541305b918b55d475
Michael Hanke <michael.hanke@gmail.com>
2018-01-29 08:08:37 +0100

    RF: Store unique content props also per source

--- datalad/metadata/metadata.py
+++ datalad/metadata/metadata.py
@@ -494,9 +494,12 @@ def _get_metadata(ds, types, global_meta=None, content_meta=None, paths=None):
                 unique_cm[k] = vset
 
         if unique_cm:
-            dsmeta['unique_content_properties'] = {
+            # per source storage here too
+            ucp = dsmeta.get('unique_content_properties', {})
+            ucp[mtype_key] = {
                 k: sorted(v) if len(v) > 1 else list(v)[0]
                 for k, v in unique_cm.items()}
+            dsmeta['unique_content_properties'] = ucp
 
     # always identify the effective vocabulary - JSON-LD style
     if context:
