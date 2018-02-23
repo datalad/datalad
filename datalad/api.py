c14c079566e35c6a403ae8f2c226fd37cf6501bd
Michael Hanke <michael.hanke@gmail.com>
2018-02-23 13:26:57 +0100

    RF: Reborn datalad.api
    To be extended with code that dynamically loads plugin commands

---
+++ datalad/api.py
@@ -0,0 +1,13 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the datalad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Python DataLad API exposing user-oriented commands (also available via CLI)"""
+
+from datalad.coreapi import *
+
+# TODO load plugin commands
