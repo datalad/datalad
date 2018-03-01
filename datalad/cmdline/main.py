e99f662d68d15f2126b2252f59e6b6768c594220
Yaroslav Halchenko <debian@onerussian.com>
2018-03-01 09:50:56 -0500

    DOC: just adjusted the year in the cmdline output
    [ci skip]

--- datalad/cmdline/main.py
+++ datalad/cmdline/main.py
@@ -37,7 +37,7 @@ from ..dochelpers import exc_str
 
 def _license_info():
     return """\
-Copyright (c) 2013-2017 DataLad developers
+Copyright (c) 2013-2018 DataLad developers
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
