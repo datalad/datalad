008b59b6b47ed9da03f58db7d0f42028b3792432
Kyle Meyer <kyle@kyleam.com>
2018-03-03 16:23:59 -0500

    RF: Convert export_archive plugin to a command

--- datalad/plugin/export_archive.py
+++ datalad/plugin/export_archive.py
@@ -10,122 +10,163 @@
 
 __docformat__ = 'restructuredtext'
 
+from datalad.interface.base import Interface
+from datalad.interface.base import build_doc
 
-# PLUGIN API
-def dlplugin(dataset, filename=None, archivetype='tar', compression='gz',
-             missing_content='error'):
+
+@build_doc
+class ExportArchive(Interface):
     """Export the content of a dataset as a TAR/ZIP archive.
-
-    Parameters
-    ----------
-    filename : str, optional
-      File name of the generated TAR archive. If no file name is given
-      the archive will be generated in the current directory and will
-      be named: datalad_<dataset_uuid>.(tar.*|zip).
-    archivetype : {'tar', 'zip'}
-      Type of archive to generate.
-    compression : {'', 'gz', 'bz2')
-      Compression method to use. 'bz2' is not supported for ZIP archives.
-    missing_content : {'error', 'continue', 'ignore'}, optional
-      By default, any discovered file with missing content will result in
-      an error and the plugin is aborted. Setting this to 'continue' will
-      issue warnings instead of failing on error. The value 'ignore' will
-      only inform about problem at the 'debug' log level. The latter two
-      can be helpful when generating a TAR archive from a dataset where
-      some file content is not available locally.
-
     """
-    import os
-    import tarfile
-    import zipfile
-    from mock import patch
-    from os.path import join as opj, dirname, normpath, isabs
-    from datalad.utils import file_basename
-    from datalad.support.annexrepo import AnnexRepo
-    from datalad.dochelpers import exc_str
+    from datalad.support.param import Parameter
+    from datalad.distribution.dataset import datasetmethod
+    from datalad.interface.utils import eval_results
+    from datalad.distribution.dataset import EnsureDataset
+    from datalad.support.constraints import EnsureNone, EnsureStr
 
-    import logging
-    lgr = logging.getLogger('datalad.plugin.export_archive')
+    _params_ = dict(
+        dataset=Parameter(
+            args=("-d", "--dataset"),
+            doc=""""specify the dataset to export. If no dataset is given, an
+            attempt is made to identify the dataset based on the current
+            working directory.""",
+            constraints=EnsureDataset() | EnsureNone()),
+        filename=Parameter(
+            args=("filename",),
+            metavar="PATH",
+            nargs='?',
+            doc="""File name of the generated TAR archive. If no file name is
+            given the archive will be generated in the current directory and
+            will be named: datalad_<dataset_uuid>.(tar.*|zip).""",
+            constraints=EnsureStr() | EnsureNone()),
+        archivetype=Parameter(
+            args=("-t", "--archivetype"),
+            metavar="tar|zip",
+            doc="""Type of archive to generate.""",
+            constraints=EnsureStr()),
+        compression=Parameter(
+            args=("-c", "--compression"),
+            metavar="gz|bz2|",
+            doc="""Compression method to use.  'bz2' is not supported for ZIP
+            archives.  No compression is used when an empty string is
+            given.""",
+            constraints=EnsureStr()),
+        missing_content=Parameter(
+            args=("--missing-content",),
+            metavar="error|continue|ignore",
+            doc="""By default, any discovered file with missing content will
+            result in an error and the export is aborted. Setting this to
+            'continue' will issue warnings instead of failing on error. The
+            value 'ignore' will only inform about problem at the 'debug' log
+            level. The latter two can be helpful when generating a TAR archive
+            from a dataset where some file content is not available
+            locally.""",
+            constraints=EnsureStr()),
+    )
 
-    repo = dataset.repo
-    committed_date = repo.get_committed_date()
+    @staticmethod
+    @datasetmethod(name='export_archive')
+    @eval_results
+    def __call__(dataset, filename=None, archivetype='tar', compression='gz',
+                 missing_content='error'):
+        import os
+        import tarfile
+        import zipfile
+        from mock import patch
+        from os.path import join as opj, dirname, normpath, isabs
 
-    # could be used later on to filter files by some criterion
-    def _filter_tarinfo(ti):
-        # Reset the date to match the one of the last commit, not from the
-        # filesystem since git doesn't track those at all
-        # TODO: use the date of the last commit when any particular
-        # file was changed -- would be the most kosher yoh thinks to the
-        # degree of our abilities
-        ti.mtime = committed_date
-        return ti
-    tar_args = dict(recursive=False, filter=_filter_tarinfo)
+        from datalad.distribution.dataset import require_dataset
+        from datalad.utils import file_basename
+        from datalad.support.annexrepo import AnnexRepo
+        from datalad.dochelpers import exc_str
 
-    file_extension = '.{}{}'.format(
-        archivetype,
-        '{}{}'.format(
-            '.' if compression else '',
-            compression) if archivetype == 'tar' else '')
+        import logging
+        lgr = logging.getLogger('datalad.plugin.export_archive')
 
-    if filename is None:
-        filename = "datalad_{}".format(dataset.id)
-    if not filename.endswith(file_extension):
-        filename += file_extension
+        dataset = require_dataset(dataset, check_installed=True,
+                                  purpose='export archive')
 
-    root = dataset.path
-    # use dir inside matching the output filename
-    # TODO: could be an option to the export plugin allowing empty value
-    # for no leading dir
-    leading_dir = file_basename(filename)
+        repo = dataset.repo
+        committed_date = repo.get_committed_date()
 
-    # workaround for inability to pass down the time stamp
-    with patch('time.time', return_value=committed_date), \
-            tarfile.open(filename, "w:{}".format(compression)) \
-            if archivetype == 'tar' \
-            else zipfile.ZipFile(
-                filename, 'w',
-                zipfile.ZIP_STORED if not compression else zipfile.ZIP_DEFLATED) \
-            as archive:
-        add_method = archive.add if archivetype == 'tar' else archive.write
-        repo_files = sorted(repo.get_indexed_files())
-        if isinstance(repo, AnnexRepo):
-            annexed = repo.is_under_annex(
-                repo_files, allow_quick=True, batch=True)
-            # remember: returns False for files in Git!
-            has_content = repo.file_has_content(
-                repo_files, allow_quick=True, batch=True)
-        else:
-            annexed = [False] * len(repo_files)
-            has_content = [True] * len(repo_files)
-        for i, rpath in enumerate(repo_files):
-            fpath = opj(root, rpath)
-            if annexed[i]:
-                if not has_content[i]:
-                    if missing_content in ('ignore', 'continue'):
-                        (lgr.warning if missing_content == 'continue' else lgr.debug)(
-                            'File %s has no content available, skipped', fpath)
-                        continue
-                    else:
-                        raise IOError('File %s has no content available' % fpath)
+        # could be used later on to filter files by some criterion
+        def _filter_tarinfo(ti):
+            # Reset the date to match the one of the last commit, not from the
+            # filesystem since git doesn't track those at all
+            # TODO: use the date of the last commit when any particular
+            # file was changed -- would be the most kosher yoh thinks to the
+            # degree of our abilities
+            ti.mtime = committed_date
+            return ti
+        tar_args = dict(recursive=False, filter=_filter_tarinfo)
 
-                # resolve to possible link target
-                link_target = os.readlink(fpath)
-                if not isabs(link_target):
-                    link_target = normpath(opj(dirname(fpath), link_target))
-                fpath = link_target
-            # name in the archive
-            aname = normpath(opj(leading_dir, rpath))
-            add_method(
-                fpath,
-                arcname=aname,
-                **(tar_args if archivetype == 'tar' else {}))
+        file_extension = '.{}{}'.format(
+            archivetype,
+            '{}{}'.format(
+                '.' if compression else '',
+                compression) if archivetype == 'tar' else '')
 
-    if not isabs(filename):
-        filename = opj(os.getcwd(), filename)
+        if filename is None:
+            filename = "datalad_{}".format(dataset.id)
+        if not filename.endswith(file_extension):
+            filename += file_extension
 
-    yield dict(
-        status='ok',
-        path=filename,
-        type='file',
-        action='export_archive',
-        logger=lgr)
+        root = dataset.path
+        # use dir inside matching the output filename
+        # TODO: could be an option to the export plugin allowing empty value
+        # for no leading dir
+        leading_dir = file_basename(filename)
+
+        # workaround for inability to pass down the time stamp
+        with patch('time.time', return_value=committed_date), \
+                tarfile.open(filename, "w:{}".format(compression)) \
+                if archivetype == 'tar' \
+                else zipfile.ZipFile(
+                    filename, 'w',
+                    zipfile.ZIP_STORED if not compression else zipfile.ZIP_DEFLATED) \
+                as archive:
+            add_method = archive.add if archivetype == 'tar' else archive.write
+            repo_files = sorted(repo.get_indexed_files())
+            if isinstance(repo, AnnexRepo):
+                annexed = repo.is_under_annex(
+                    repo_files, allow_quick=True, batch=True)
+                # remember: returns False for files in Git!
+                has_content = repo.file_has_content(
+                    repo_files, allow_quick=True, batch=True)
+            else:
+                annexed = [False] * len(repo_files)
+                has_content = [True] * len(repo_files)
+            for i, rpath in enumerate(repo_files):
+                fpath = opj(root, rpath)
+                if annexed[i]:
+                    if not has_content[i]:
+                        if missing_content in ('ignore', 'continue'):
+                            (lgr.warning if missing_content == 'continue' else lgr.debug)(
+                                'File %s has no content available, skipped', fpath)
+                            continue
+                        else:
+                            raise IOError('File %s has no content available' % fpath)
+
+                    # resolve to possible link target
+                    link_target = os.readlink(fpath)
+                    if not isabs(link_target):
+                        link_target = normpath(opj(dirname(fpath), link_target))
+                    fpath = link_target
+                # name in the archive
+                aname = normpath(opj(leading_dir, rpath))
+                add_method(
+                    fpath,
+                    arcname=aname,
+                    **(tar_args if archivetype == 'tar' else {}))
+
+        if not isabs(filename):
+            filename = opj(os.getcwd(), filename)
+
+        yield dict(
+            status='ok',
+            path=filename,
+            type='file',
+            action='export_archive',
+            logger=lgr)
+
+__datalad_plugin__ = ExportArchive
