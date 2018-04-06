0f4c1fba835ec731f8811aad785007788f92556d
Kyle Meyer <kyle@kyleam.com>
2018-04-06 15:03:52 -0400

    ENH: rerun: Add an option to output commands to a script
    Note: A large portion of lines in the diff come from indentation
    rather than a change in content.
    
    Re: #2291

--- datalad/interface/tests/test_run.py
+++ datalad/interface/tests/test_run.py
@@ -43,6 +43,7 @@ from datalad.tests.utils import eq_
 from datalad.tests.utils import assert_status
 from datalad.tests.utils import assert_result_count
 from datalad.tests.utils import assert_in
+from datalad.tests.utils import assert_not_in
 from datalad.tests.utils import swallow_logs
 from datalad.tests.utils import swallow_outputs
 from datalad.tests.utils import assert_in_results
@@ -520,6 +521,31 @@ def test_new_or_modified(path):
     assert_false(list(apfiles(new_or_modified(ds, "HEAD"))))
     eq_(set(apfiles(new_or_modified(ds, "HEAD~"))),
         {"to_modify", "d/to_modify"})
+
+
+@ignore_nose_capturing_stdout
+@skip_if_on_windows
+@with_tempfile(mkdir=True)
+@known_failure_direct_mode  #FIXME
+def test_rerun_script(path):
+    ds = Dataset(path).create()
+    ds.run("echo a >foo")
+    ds.run("echo b >bar")
+
+    script_file = opj(path, "commands.sh")
+
+    ds.rerun(script=script_file)
+    ok_exists(script_file)
+    with open(script_file) as sf:
+        lines = sf.readlines()
+        assert_in("echo b >bar\n", lines)
+        assert_not_in("echo a >foo\n", lines)
+
+    ds.rerun(since="", script=script_file)
+    with open(script_file) as sf:
+        lines = sf.readlines()
+        assert_in("echo b >bar\n", lines)
+        assert_in("echo a >foo\n", lines)
 
 
 @with_tempfile(mkdir=True)
