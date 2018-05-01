169b01f976400520b4ab195c268b1b5d2592f90c
Yaroslav Halchenko <debian@onerussian.com>
2018-05-01 11:00:39 -0400

    BF(TST): use OBSCURE_FILENAME instead of hardcoded unicode in test_add

--- datalad/distribution/tests/test_add.py
+++ datalad/distribution/tests/test_add.py
@@ -34,6 +34,7 @@ from datalad.tests.utils import assert_result_count
 from datalad.tests.utils import serve_path_via_http
 from datalad.tests.utils import SkipTest
 from datalad.tests.utils import create_tree
+from datalad.tests.utils import OBSCURE_FILENAME
 from datalad.utils import chpwd
 
 from ..dataset import Dataset
@@ -63,7 +64,7 @@ tree_arg = dict(tree={'test.txt': 'some',
                       'test1.dat': 'test file 1',
                       'test2.dat': 'test file 2',
                       'dir': {'testindir': 'someother',
-                              'äöü東': 'none'},
+                              OBSCURE_FILENAME: 'none'},
                       'dir2': {'testindir3': 'someother3'}})
 
 
@@ -76,7 +77,7 @@ def test_add_files(path):
     test_list_1 = ['test_annex.txt']
     test_list_2 = ['test.txt']
     test_list_3 = ['test1.dat', 'test2.dat']
-    test_list_4 = [opj('dir', 'testindir'), opj('dir', u'äöü東')]
+    test_list_4 = [opj('dir', 'testindir'), opj('dir', OBSCURE_FILENAME)]
     all_files = test_list_1 + test_list_2 + test_list_3 + test_list_4
     unstaged = set(all_files)
     staged = set()
@@ -204,10 +205,10 @@ def test_add_dirty_tree(path):
     # added to git, so parsed git output record
     assert_result_count(
         added2, 1,
-        path=opj(ds.path, 'dir', u'äöü東'), action='add',
+        path=opj(ds.path, 'dir', OBSCURE_FILENAME), action='add',
         message='non-large file; adding content to git repository',
         status='ok')
-    assert_in(u'äöü東', Dataset(opj(path, 'dir')).repo.get_indexed_files())
+    assert_in(OBSCURE_FILENAME, Dataset(opj(path, 'dir')).repo.get_indexed_files())
     ok_clean_git(ds.path)
 
     # We used to fail to add to pure git repository, but now it should all be
