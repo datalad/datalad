bf87bdde2f6b33b3bc5f200affe3f1a3f2ff89a2
Yaroslav Halchenko <debian@onerussian.com>
2018-04-30 22:52:28 -0400

    BF(TST): generate unique fake packages for testing imports

--- datalad/tests/test_utils.py
+++ datalad/tests/test_utils.py
@@ -977,12 +977,21 @@ def test_read_csv_lines_one_column(infile):
     )
 
 
-_testm_tree = {'dltestm1': {'__init__.py': '',
-                            'dlsub1': {'__init__.py': 'var = 1'},
-                            'dlsub2.py': 'var = 2'}}
+def _get_testm_tree(ind):
+    """Generate a fake package with submodules
 
+    We need to increment index for different tests since otherwise e.g.
+    import_modules fails to import submodule if first import_module_from_file
+    imports that one
+    """
+    return {
+        'dltestm%d' % ind: {
+            '__init__.py': '',
+            'dlsub1': {'__init__.py': 'var = 1'},
+            'dlsub2.py': 'var = 2'}
+    }
 
-@with_tree(tree=_testm_tree)
+@with_tree(tree=_get_testm_tree(1))
 def test_import_modules(topdir):
     try:
         sys.path.append(topdir)
@@ -993,23 +1002,23 @@ def test_import_modules(topdir):
     eq_(mods[0].__name__, 'dltestm1.dlsub1')
 
 
-@with_tree(tree=_testm_tree)
+@with_tree(tree=_get_testm_tree(2))
 def test_import_module_from_file(topdir):
     with assert_raises(AssertionError):
         # we support only submodule files ending with .py ATM. TODO
-        import_module_from_file(op.join(topdir, 'dltestm1', 'dlsub1'))
+        import_module_from_file(op.join(topdir, 'dltestm2', 'dlsub1'))
 
-    dlsub2_path = op.join(topdir, 'dltestm1', 'dlsub2.py')
+    dlsub2_path = op.join(topdir, 'dltestm2', 'dlsub2.py')
     mod = import_module_from_file(dlsub2_path)
     eq_(mod.__name__, 'dlsub2')  # we are not asking to import as submod of the dltestm1
     assert_in('dlsub2', sys.modules)
 
     try:
         sys.path.append(topdir)
-        import dltestm1
-        mod = import_module_from_file(dlsub2_path, pkg=dltestm1)
-        eq_(mod.__name__, 'dltestm1.dlsub2')
-        assert_in('dltestm1.dlsub2', sys.modules)
+        import dltestm2
+        mod = import_module_from_file(dlsub2_path, pkg=dltestm2)
+        eq_(mod.__name__, 'dltestm2.dlsub2')
+        assert_in('dltestm2.dlsub2', sys.modules)
     finally:
         sys.path.pop(sys.path.index(topdir))
 
