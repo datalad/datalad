a181807b2dff3124d737076abe8efa739d8b0b64
Michael Hanke <michael.hanke@gmail.com>
2018-06-04 09:30:02 +0200

    ENH: Any extension and Datalad itself can provide procedures
    Simply by installing them into `resources/procedures` underneath
    their own module.

--- datalad/interface/run_procedure.py
+++ datalad/interface/run_procedure.py
@@ -40,12 +40,6 @@ from datalad.interface.run import Run
 lgr = logging.getLogger('datalad.interface.run_procedures')
 
 
-BUILTIN_PROCEDURES_PATH = op.join(
-    op.dirname(op.dirname(__file__)),
-    'resources',
-    'procedures')
-
-
 def _get_file_match(name, dir):
     targets = (name, ('[!_]*.py'), ('[!_]*.sh'))
     lgr.debug("Looking for procedure '%s' in '%s'", name, dir)
@@ -58,6 +52,7 @@ def _get_file_match(name, dir):
 
 def _get_procedure_implementation(name, ds=None):
     ds = ds if isinstance(ds, Dataset) else Dataset(ds) if ds else None
+    # 1. check dataset for procedure
     if ds is not None and ds.is_installed():
         # could be more than one
         dirs = assure_list(ds.config.obtain('datalad.locations.dataset-procedures'))
@@ -66,14 +61,32 @@ def _get_procedure_implementation(name, ds=None):
             m = _get_file_match(name, op.join(ds.path, dir))
             if m:
                 return m
+    # 2. check system and user account for procedure
     for loc in (cfg.obtain('datalad.locations.user-procedures'),
-                cfg.obtain('datalad.locations.system-procedures'),
-                [BUILTIN_PROCEDURES_PATH]):
+                cfg.obtain('datalad.locations.system-procedures')):
         for dir in assure_list(loc):
             m = _get_file_match(name, dir)
             if m:
                 return m
-    return None
+    # 3. check extensions for procedure
+    # delay heavy import until here
+    from pkg_resources import iter_entry_points
+    from pkg_resources import resource_isdir
+    from pkg_resources import resource_filename
+    for entry_point in iter_entry_points('datalad.extensions'):
+        # use of '/' here is OK wrt to platform compatibility
+        if resource_isdir(entry_point.module_name, 'resources/procedures'):
+            m = _get_file_match(
+                name,
+                resource_filename(
+                    entry_point.module_name,
+                    'resources/procedures'))
+            if m:
+                return m
+    # 4. at last check datalad itself for procedure
+    return _get_file_match(
+        name,
+        resource_filename('datalad', 'resources/procedure'))
 
 
 def _guess_exec(script_file):
@@ -122,7 +135,6 @@ class RunProcedure(Interface):
             spec = shlex.split(spec)
         name = spec[0]
         args = spec[1:]
-        print("?????", name, args)
         procedure_file = _get_procedure_implementation(name, ds=dataset)
         if not procedure_file:
             # TODO error result
