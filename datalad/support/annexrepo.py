eca39f0c1a63faf237ee740a3b6ba6aa8e60f201
Kyle Meyer <kyle@kyleam.com>
2018-09-19 23:50:35 -0400

    ENH: annexrepo.readline_json: Return empty output as empty dict
    An upcoming commit will move batched find over to json (to handle
    filenames with trailing spaces), and that command has empty output for
    files that don't have content present.

--- datalad/support/annexrepo.py
+++ datalad/support/annexrepo.py
@@ -3469,7 +3469,8 @@ def readlines_until_ok_or_failed(stdout, maxlines=100):
 
 
 def readline_json(stdout):
-    return json_loads(stdout.readline().strip())
+    toload = stdout.readline().strip()
+    return json_loads(toload) if toload else {}
 
 
 @auto_repr
