5bd3dee1326b3235c5c393b7c70588ec9d4feaba
Yaroslav Halchenko <debian@onerussian.com>
2018-10-04 22:57:57 -0400

    ENH/RF(BK): provide class for Open context managers
    This way we could aggregate all the result records happen they
    come handy for analysis/output where/when needed.
    
    BK due to https://github.com/datalad/datalad/issues/1651
    since apparently neither add nor save can deal with files migrating
    between git and annex

--- datalad/interface/open.py
+++ datalad/interface/open.py
@@ -10,7 +10,7 @@
 
 __docformat__ = 'restructuredtext'
 
-
+import os
 import logging
 from contextlib import contextmanager
 
@@ -100,83 +100,139 @@ lgr = logging.getLogger('datalad.interface.open')
 
 _builtin_open = open
 
-if True:
-    @datasetmethod(name='open')
-    def open(
-            path=None,
-            mode=None,
-            buffering=None,
-            dataset=None,
-            save=True,
-            message=None
-            # , if_dirty='save-before'
-        ):
-        """TODO"""
-        # Pre-treat open parameters first
-        if mode is not None:
-            if mode[0] not in 'rwa':
-                raise ValueError("Mode must be either None or start with r, w, or a")
+@datasetmethod(name='open')
+def open(
+        path,
+        mode=None,
+        buffering=None,
+        dataset=None,
+        save=True,
+        message=None
+        # , if_dirty='save-before'
+    ):
+    """TODO"""
+    # Pre-treat open parameters first
+    if mode is not None:
+        if mode[0] not in 'rwa':
+            raise ValueError("Mode must be either None or start with r, w, or a")
 
-        open_args = []
-        if mode is not None:
-            open_args.append(mode)
-            if buffering is not None:
-                open_args.append(buffering)
-        elif buffering is not None:
-            # Do not bother messing with it
-            raise ValueError("When specifying buffering, provide mode for open")
+    open_args = []
+    if mode is not None:
+        open_args.append(mode)
+        if buffering is not None:
+            open_args.append(buffering)
+    elif buffering is not None:
+        # Do not bother messing with it
+        raise ValueError("When specifying buffering, provide mode for open")
 
-        # Probably will be useless in Python mode since we cannot return a
-        # context manager an yield all the result records at the same time.
-        # But if we make it into a class, we could store them in the instance
-        # so they could be inspected later on if desired?
-        all_results = []
-        path = assure_list(path)
-        resolved_paths = [resolve_path(p, dataset) for p in path] \
-            if dataset is not None else path
+    # Probably will be useless in Python mode since we cannot return a
+    # context manager an yield all the result records at the same time.
+    # But if we make it into a class, we could store them in the instance
+    # so they could be inspected later on if desired?
+    path = assure_list(path)
+    resolved_paths = [resolve_path(p, dataset) for p in path] \
+        if dataset is not None else path
 
-        import datalad.api as dl  # heavy import so delayed
+    import datalad.api as dl  # heavy import so delayed
 
-        @contextmanager
-        def open_read():
-            # TODO cannot yield all those wonderful reports here since it would
-            # be a context manager... nothing to be yielded into the Python code
-            res = dl.get(path, dataset=dataset
-                         # TODO: what should we do with files not under git at all?
-                         # here on_failure is not effective - fails during "rendering"
-                         # , on_failure='continue'
-            )
-            all_results.extend(res)
-            files = []
+    class OpenBase(object):
+        def __init__(self):
+            self.all_results = []
+            self.files = None
+
+        def __enter__(self):
+            pre_open = self.pre_open()
+            if pre_open:
+                self.all_results.extend(pre_open)
+
+            self.files = []
             for p in resolved_paths:
                 # TODO: actually do all that full path deduction here probably
                 # since we allow for ds.open
-                files.append(_builtin_open(p, *open_args))
-                all_results.append({
+                self.files.append(_builtin_open(p, *open_args))
+                self.all_results.append({
                     'status': 'ok',
                     'action': 'open',
+                    'mode': mode,
                     'path': p,
                     'type': 'file'
                 })
+            return self.files if len(self.files) > 1 else self.files[0]
 
-            yield files if len(files) > 1 else files[0]
-
-            # Nothing more to do for reading operation besides closing those
-            for f in files:
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            # TODO: handle if exception happened - I think we should "abort"
+            for f in self.files:
                 if not f.closed:
                     f.close()
-                    all_results.append({
-                        'status': 'ok',
-                        'action': 'close',
-                        'path': f.name,
-                        'type': 'file'
-                    })
+                    self.all_results.append(
+                        {
+                            'status': 'ok',
+                            'action': 'close',
+                            'path': f.name,
+                            'type': 'file'
+                        }
+                    )
+
+            post_close = self.post_close()
+            if post_close:
+                self.all_results.extend(post_close)
+
+        def pre_open(self):
+            raise NotImplementedError
+
+        def post_close(self):
+            raise NotImplementedError
+
+    class OpenRead(OpenBase):
+        def pre_open(self):
+            return dl.get(resolved_paths,
+                 # TODO: what should we do with files not under git at all?
+                 # here on_failure is not effective - fails during "rendering"
+                 # , on_failure='continue'
+            )
+
+        def post_close(self):
+            pass
 
 
-        mode_base = mode[0] if mode is not None else 'r'
-        res = {
-            'r': open_read,
-            #'w': open_write(),
-        }[mode_base]()
+    class OpenRewrite(OpenBase):
+        def pre_open(self):
+            for p in resolved_paths:
+                # TODO: do we need to store/apply to the new files anything?
+                # If file is under git - the only thing we could (re)store is
+                # executable bit
+                # If file is outside of git/annex control - we might better even
+                # not remove it at all
+                os.unlink(p)
 
-        return res
+        def post_close(self):
+            # add would crash in case file jumps between git and annex
+            # due to .gitattributes settings.
+            # .save seems to be more resilient BUT! retains git/annex so
+            # there would be no auto migrations
+            # TODO, see https://github.com/datalad/datalad/issues/1651
+            if save:
+                # mneh -- even this one (may be due to paths specification?)
+                # doesn't work if file jumps between git and annex
+                self.all_results.extend(
+                    dl.save(path=resolved_paths, message=message)
+                )
+
+
+    class OpenAppend(OpenRewrite):
+        def pre_open(self):
+            res = OpenRead.pre_open(self)
+            res.extend(dl.unlock(resolved_paths))
+            return res
+
+        # TODO: might need to track what was actually unlocked (v6) and do more
+        # in post_close here
+
+    mode_base = mode[0] if mode is not None else 'r'
+    res = {
+        'r': OpenRead,
+        'w': OpenRewrite,
+        'a': OpenAppend,
+    }[mode_base]()
+
+    return res
--- datalad/interface/tests/test_open.py
+++ datalad/interface/tests/test_open.py
@@ -36,25 +36,45 @@ from datalad.tests.utils import assert_cwd_unchanged
 from datalad.tests.utils import with_testrepos
 from datalad.tests.utils import on_windows, skip_if
 from datalad.tests.utils import assert_status, assert_result_count, assert_in_results
-from datalad.tests.utils import ok_file_has_content
+from datalad.tests.utils import (
+    ok_file_has_content,
+    ok_file_under_git,
+    ok_clean_git,
+)
 from datalad.support.path import exists, join as opj
 
+from functools import wraps
 
-@assert_cwd_unchanged
-@with_tree(tree={
-    'ds': {
-        'in-annex': '',
-        'in-git': 'text',
-        'untracked': 'buga',  # TODO: 'untracked' and 'outside' aren't supported yet
-    },
-    'outside': 'content'
-})
-def test_open_read(path):
-    ds_orig = Dataset(opj(path, 'ds')).create(text_no_annex=True, force=True)
-    ds_orig.add(['in-annex', 'in-git'])
-    # bleh -- clone in Python API we return the records, even with return_type
-    # ds = clone(path, path + '-clone', return_type='item-or-list')
-    ds = install(ds_orig.path + '-clone', source=ds_orig)
+
+def with_sample_ds(t):
+    """A "fixture" for the tests to provide a sample dataset
+    """
+    @wraps(t)
+    @assert_cwd_unchanged
+    @with_tree(tree={
+        'ds': {
+            'in-annex': '',
+            'in-git': 'text',
+            'untracked': 'buga',
+        # TODO: 'untracked' and 'outside' aren't supported yet
+        },
+        'outside': 'content'
+    })
+    def wrapped(path):
+        ds_orig = Dataset(opj(path, 'ds')).create(text_no_annex=True,
+                                                  force=True)
+        ds_orig.add(['in-annex', 'in-git'])
+        # bleh -- clone in Python API we return the records, even with
+        # return_type
+        # ds = clone(path, path + '-clone', return_type='item-or-list')
+        ds = install(ds_orig.path + '-clone', source=ds_orig)
+
+        return t(ds)
+    return wrapped
+
+
+@with_sample_ds
+def test_read(ds):
     in_annex = opj(ds.path, 'in-annex')
 
     assert not exists(in_annex)
@@ -78,3 +98,35 @@ def test_open_read(path):
         with open((in_annex, 'in-git'), 'r', buffering=1) as (f1, f2):
             eq_(f1.read(), '')
             eq_(f2.read(), 'text')
+
+
+@with_sample_ds
+def test_rewrite(ds):
+    in_annex = opj(ds.path, 'in-annex')
+
+    assert not exists(in_annex)
+    with ds.open('in-annex', 'w') as f:
+        f.write("stuff")
+    ok_file_has_content(in_annex, "stuff")
+    ok_file_under_git(in_annex, annexed=True)
+    ok_clean_git(ds, untracked=['untracked'])
+
+    with ds.open('in-git', 'w') as f:
+        f.write("1")
+        f.write("2")
+
+    # ds.drop('in-annex')
+    # assert not exists(in_annex)  # never know for sure with all those generators etc
+    # # multiple at once, including untracked
+    # with ds.open(['in-annex', 'in-git']) as (f1, f2):
+    #     eq_(f1.read(), '')
+    #     eq_(f2.read(), 'text')
+    # ds.drop('in-annex')
+    #
+    # # TODO: make code work also with 'untracked' and 'outside'
+    #
+    # # Let's test with full and local paths and also explicit mode and buffering
+    # with chpwd(ds.path):
+    #     with open((in_annex, 'in-git'), 'r', buffering=1) as (f1, f2):
+    #         eq_(f1.read(), '')
+    #         eq_(f2.read(), 'text')
