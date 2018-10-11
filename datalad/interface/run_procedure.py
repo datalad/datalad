5dbeb3cebe400e3556160715e471ac51fadfd9e4
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2018-10-11 14:54:58 +0200

    DOC: document added features

--- datalad/interface/run_procedure.py
+++ datalad/interface/run_procedure.py
@@ -61,6 +61,9 @@ def _get_file_match(dir, name='*'):
 def _get_proc_config(name, ds=None):
     """get configuration of named procedure
 
+    Figures call format string and help message for a given procedure name,
+    based on dataset.
+
     Returns
     -------
     tuple
@@ -151,7 +154,7 @@ class RunProcedure(Interface):
 
     Implementations of some procedures are shipped together with DataLad,
     but additional procedures can be provided by 1) any DataLad extension,
-    2) any dataset, 3) a local user, or 4) a local system administrator.
+    2) any (sub-)dataset, 3) a local user, or 4) a local system administrator.
     DataLad will look for procedures in the following locations and order:
 
     Directories identified by the configuration settings
@@ -166,6 +169,14 @@ class RunProcedure(Interface):
 
     and subsequently in the 'resources/procedures/' directories of any
     installed extension, and, lastly, of the DataLad installation itself.
+
+    Please note, that a dataset that defines
+    'datalad.locations.dataset-procedures' provides its procedures to
+    any dataset it is a subdataset of. That way you can have a collection of
+    such procedures in a dedicated dataset and install it as a subdataset into
+    any dataset you want to use those procedures with. In case of a naming
+    conflict with such a dataset hierarchy, the dataset you're calling
+    run-procedures on will take precedence over its subdatasets and so on.
 
     Each configuration setting can occur multiple times to indicate multiple
     directories to be searched. If a procedure matching a given name is found
@@ -188,6 +199,26 @@ class RunProcedure(Interface):
     of taking at least one positional argument (the absolute path to the
     dataset they shall operate on).
 
+    For further customization there are two configuration settings per procedure
+    available:
+
+    - 'datalad.procedures.<NAME>.call-format'
+      fully customizable format string to determine how to execute procedure
+      NAME (see also datalad-run).
+      It currently requires to include the following placeholders:
+      - '{script}'
+        will be replaced by the path to the procedure
+      - '{ds}'
+        will be replaced by the absolute path to the dataset the procedure
+        shall operate on
+      - '{args}'
+        will be replaced by all additional arguments passed into run-procedure
+        after NAME
+      As an example the default format string for a call to a python script is:
+      "python {script} {ds} {args}"
+    - 'datalad.procedures.<NAME>.help'
+      will be shown on `datalad run-procedure --help-proc NAME` to provide a
+      description and/or usage info for procedure NAME
 
     *Customize other commands with procedures*
 
