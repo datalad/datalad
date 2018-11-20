83b3640eca0700d4ae0c71132f2adeedc56e3139
Yaroslav Halchenko <debian@onerussian.com>
2018-11-20 16:39:19 -0500

    RF: Go to 50% margin

--- datalad/utils.py
+++ datalad/utils.py
@@ -98,8 +98,10 @@ except Exception as exc:
 # environment and what not, we still could not figure out "exact" way to
 # estimate it, but it was shown that 300k safety margin on linux was sufficient.
 # https://github.com/datalad/datalad/pull/2977#issuecomment-436264710
-# 300k is ~15%, so to be safe, we will just use up to 80% of the length
-CMD_MAX_ARG = int(0.8 * CMD_MAX_ARG)
+# 300k is ~15%, so to be safe, and for paranoid us we will just use up to 50%
+# of the length for "safety margin".  We might probably still blow due to
+# env vars, unicode, etc...  so any hard limit imho is not a proper solution
+CMD_MAX_ARG = int(0.5 * CMD_MAX_ARG)
 lgr.debug(
     "Maximal length of cmdline string (adjusted for safety margin): %d",
     CMD_MAX_ARG)
