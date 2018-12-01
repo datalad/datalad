f332c34f2c8b70f7a9a18e05f3b304986dec8387
Kyle Meyer <kyle@kyleam.com>
2018-12-01 13:33:22 -0500

    RF: announce another now failing windows test as known
    Like the tests in the previous commit, it looks like this test was
    skipped on appveyor even for v6+ repos because
    @known_failure_direct_mode caused a test to be skipped when in direct
    mode *or* on windows (b9a3a5c33).

--- datalad/interface/tests/test_run_procedure.py
+++ datalad/interface/tests/test_run_procedure.py
@@ -26,6 +26,7 @@ from datalad.tests.utils import assert_in_results
 from datalad.tests.utils import assert_not_in_results
 from datalad.tests.utils import skip_if
 from datalad.tests.utils import on_windows
+from datalad.tests.utils import known_failure_windows
 from datalad.distribution.dataset import Dataset
 from datalad.support.exceptions import InsufficientArgumentsError
 from datalad.api import run_procedure
@@ -41,7 +42,7 @@ def test_invalid_call():
     assert_in_results(res, status="impossible")
 
 
-@skip_if(cond=on_windows and cfg.obtain("datalad.repo.version") < 6)
+@known_failure_windows  #FIXME
 @with_tree(tree={
     'code': {'datalad_test_proc.py': """\
 import sys
