datalad/datalad

commit e18deb11856de7f20a381e289c6c99a864c26109

ENH: adjusted comment and sh_quote group itself (per @kyleam recommendation)

datalad/distribution/create_sibling.py
@@ -204,10 +204,10 @@ def _create_dataset_sibling(
             delayed_super, name, ssh)
 
     if group:
-        # Either it existed before or a new directory for a repo, set its
-        # group to a desired one if was provided
+        # Either repository existed before or a new directory was created for it,
+        # set its group to a desired one if was provided with the same chgrp
         ssh("chgrp -R {} {}".format(
-            group,
+            sh_quote(group),
             sh_quote(remoteds_path)))
     # don't (re-)initialize dataset if existing == reconfigure
     if not only_reconfigure:

commit 697daf34c0851292ef54e59b91f227849bc5870b

BF: create_sibling: Ensure that sh_quote is passed a string
Fixes test failures introduced by e18deb118.

datalad/distribution/create_sibling.py
@@ -207,7 +207,7 @@ def _create_dataset_sibling(
         # Either repository existed before or a new directory was created for it,
         # set its group to a desired one if was provided with the same chgrp
         ssh("chgrp -R {} {}".format(
-            sh_quote(group),
+            sh_quote(text_type(group)),
             sh_quote(remoteds_path)))
     # don't (re-)initialize dataset if existing == reconfigure
     if not only_reconfigure: