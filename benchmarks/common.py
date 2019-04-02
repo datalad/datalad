03e1bcec79ebadce86779e40eccac519e45e190d
Yaroslav Halchenko <debian@onerussian.com>
2019-04-02 12:08:31 -0400

    BF(BM): we need to monkey patch is_interactive for new asv and running benchmarks on older commits
    Proper fix within DataLad itself is in https://github.com/datalad/datalad/pull/3268 .
    This change to benchmarks should monkey patch older datalad "runtime" so it would
    not puke on those benchmarks relying on is_interactive

--- benchmarks/common.py
+++ benchmarks/common.py
@@ -7,7 +7,30 @@
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """Helpers for benchmarks of DataLad"""
 
+import sys
 import timeit
+
+############
+# Monkey patches
+
+# Robust is_interactive.  Should be not needed since 0.11.4
+# https://github.com/datalad/datalad/pull/3268
+def _is_stream_tty(stream):
+    try:
+        # TODO: check on windows if hasattr check would work correctly and
+        # add value:
+        return stream.isatty()
+    except ValueError as exc:
+        # Who knows why it is a ValueError, but let's try to be specific
+        # If there is a problem with I/O - non-interactive, otherwise reraise
+        if "I/O" in str(exc):
+            return False
+        raise
+
+
+def is_interactive():
+    """Return True if all in/outs are tty"""
+    return all(_is_stream_tty(s) for s in (sys.stdin, sys.stdout, sys.stderr))
 
 
 class SuprocBenchmarks(object):
@@ -19,4 +42,19 @@ class SuprocBenchmarks(object):
     # custom timer so we account for subprocess times
     timer = timeit.default_timer
 
+    _monkey_patched = False
+
+    def __init__(self):
+        if not self._monkey_patched:
+            # monkey patch things if needed
+            # ASV started to close one of the std streams since some point
+            # which caused our is_interactive to fail.  We need to provide
+            # more robust version
+            from datalad.support.external_versions import external_versions
+            if external_versions['datalad'] < '0.11.4':
+                from datalad import utils
+                from datalad.interface import ls
+                utils.is_interactive = is_interactive
+                ls.is_interactive = is_interactive
+            SuprocBenchmarks._monkey_patched = True
 
