datalad/datalad

commit eba3ccfd67c46ec086b9de19fa69c6e24d46f5ba

BF: run: Save results with rev-save
Unlike add, rev-save can handle deleted files.

Closes #2454.

datalad/interface/tests/test_run.py
@@ -27,6 +27,7 @@ from mock import patch
 
 from datalad.utils import assure_unicode
 from datalad.utils import chpwd
+from datalad.utils import on_windows
 
 from datalad.cmdline.main import main
 from datalad.distribution.dataset import Dataset
@@ -104,12 +105,12 @@ def test_basics(path, nodspath):
         last_state = ds.repo.get_hexsha()
         # now run a command that will not alter the dataset
         res = ds.run('touch empty', message='NOOP_TEST')
-        assert_result_count(res, 1, action='add', status='notneeded')
+        assert_result_count(res, 1, action='save', status='notneeded')
         eq_(last_state, ds.repo.get_hexsha())
         # We can also run the command via a single-item list because this is
         # what the CLI interface passes in for quoted commands.
         res = ds.run(['touch empty'], message='NOOP_TEST')
-        assert_result_count(res, 1, action='add', status='notneeded')
+        assert_result_count(res, 1, action='save', status='notneeded')
 
     # run outside the dataset, should still work but with limitations
     with chpwd(nodspath), \
@@ -440,6 +441,14 @@ def test_run_failure(path):
     with assert_raises(CommandError):
         ds.run("false", explicit=True, outputs=None)
     assert_false(op.exists(msgfile))
+
+
+@with_tree(tree={"to_remove": "abc"})
+def test_run_save_deletion(path):
+    ds = Dataset(path).create(force=True)
+    ds.rev_save()
+    ds.run("{} to_remove".format("del" if on_windows else "rm"))
+    ok_clean_git(ds.path)
 
 
 @known_failure_windows

datalad/core/local/run.py
@@ -6,38 +6,20 @@
 #   copyright and license terms.
 #
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-"""Thin wrapper around `run` from DataLad core"""
+"""Temporary rev-run -> run alias"""
 
 __docformat__ = 'restructuredtext'
 
-
-import logging
-
-# take everything from run, all we want to be is a thin variant
 from datalad.interface.run import (
     Run as _Run,
     run_command,
     build_doc,
     eval_results,
+    _save_outputs,
 )
 from datalad.distribution.dataset import (
-    Dataset,
     datasetmethod,
 )
-from .save import Save
-
-lgr = logging.getLogger('datalad.core.local.run')
-
-
-def _save_outputs(ds, to_save, msg):
-    """Helper to save results after command execution is completed"""
-    return Save.__call__(
-        to_save,
-        message=msg,
-        # need to convert any incoming dataset into a revolutionary one
-        dataset=Dataset(ds.path),
-        recursive=True,
-        return_type='generator')
 
 
 @build_doc

datalad/interface/run.py
@@ -23,6 +23,8 @@ from os.path import relpath
 from six.moves import map
 from six.moves import shlex_quote
 
+from datalad.core.local.save import Save
+
 from datalad.interface.base import Interface
 from datalad.interface.utils import eval_results
 from datalad.interface.base import build_doc
@@ -389,8 +391,9 @@ def _execute_command(command, pwd, expected_exit=None):
 
 def _save_outputs(ds, to_save, msg):
     """Helper to save results after command execution is completed"""
-    return ds.add(
+    return Save.__call__(
         to_save,
+        dataset=ds.path,
         recursive=True,
         message=msg,
         return_type='generator')
@@ -595,7 +598,7 @@ def run_command(cmd, dataset=None, inputs=None, outputs=None, expand=None,
                 ofh.write(assure_bytes(msg))
             lgr.info("The command had a non-zero exit code. "
                      "If this is expected, you can save the changes with "
-                     "'datalad add -d . -r -F %s .'",
+                     "'datalad rev-save -d . -r -F %s'",
                      msg_path)
         raise exc
     elif outputs_to_save:

commit 3dea65a9b89e9fde9625e3099ede36e52c36d408

BF: status: Provide special treatment of "." path
When status receives "." as the path, rev_resolve_path() resolves the
path to the dataset (either to the one explicitly provided as an
argument or to the one inferred from the current directory).  So,
using "." means we get the reference dataset as the path.

If a call includes an explicitly specified dataset _and_ "." as a path
argument, whether the call succeeds or fails depends on whether the
specified dataset is a subdataset.  When the reference dataset isn't a
subdataset, status queries within the dataset.  When it is a
subdataset, the call fails with a message saying the subdataset is not
underneath itself:

  $ datalad status -d. .
  [ERROR ] dataset containing given paths is not underneath the
  reference dataset [...]

Even though the desired behavior could be achieved by dropping "." as
a path argument, let's make using "." as the path argument work
because

  * it is consistent with the non-subdataset case, and it won't be
    obvious to a caller why it works in one case but not the other

  * based on the use of "." as a path argument in other places in
    datalad, it's be reasonable for a caller to expect, say,
    subds.status(".") to work

As shown by the added run test, this fixes a regression where calling
'datalad run' failed from within a subdataset, which was introduced by
eba3ccfd6 (BF: run: Save results with rev-save, 2019-03-18) because
the call to rev_save() retained the path="." that is necessary for the
add() call.

datalad/core/local/status.py
@@ -276,7 +276,8 @@ class Status(Interface):
                     continue
                 else:
                     if dataset and root == str(p) and \
-                            not orig_path.endswith(op.sep):
+                            not (orig_path.endswith(op.sep) or
+                                 orig_path == "."):
                         # the given path is pointing to a dataset
                         # distinguish rsync-link syntax to identify
                         # the dataset as whole (e.g. 'ds') vs its

datalad/core/local/tests/test_status.py
@@ -232,3 +232,12 @@ def test_subds_status(path):
         type='dataset',
         path=subds.path,
         refds=ds.path)
+
+    # path="." gets treated as "this dataset's content" without requiring a
+    # trailing "/".
+    assert_result_count(
+        subds.status(path="."),
+        1,
+        type="dataset",
+        path=op.join(subds.path, "someotherds"),
+        refds=subds.path)

datalad/interface/tests/test_run.py
@@ -482,6 +482,13 @@ def test_run_save_deletion(path):
     assert_repo_status(ds.path)
 
 
+@with_tempfile(mkdir=True)
+def test_run_from_subds(path):
+    subds = Dataset(path).rev_create().rev_create("sub")
+    subds.run("cd .> foo")
+    assert_repo_status(subds.path)
+
+
 @known_failure_windows
 @with_tempfile(mkdir=True)
 def test_rerun_branch(path):