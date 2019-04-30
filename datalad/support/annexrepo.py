8bd908e4f7d9d4cf971c433d91023273197cdc11
Kyle Meyer <kyle@kyleam.com>
2019-04-30 15:09:08 -0400

    RF: annexrepo: Tighten decision to commit with temporary index
    We decide to commit with a temporary index if there are any files with
    staged changes that should _not_ be committed or if any of the
    specified paths have unstaged changes.  The latter actually doesn't
    require a temporary index; we just need to update the paths in the
    current index, as we already do.
    
    As a side note, by adding the files, gh-3009 introduced a subtle
    change in behavior.  Before the specified files would be added to git
    by 'git commit'.  After gh-3009, they are added with 'git annex add'.
    This is unlikely to matter because the behavior was undefined and is
    only visible when calling AnnexRepo.commit() directly as save adds the
    paths before calling .commit().

--- datalad/support/annexrepo.py
+++ datalad/support/annexrepo.py
@@ -2937,15 +2937,15 @@ class AnnexRepo(GitRepo, RepoInterface):
                     # Files which were staged but not among files
                     staged_not_to_commit = all_changed_staged.difference(files_changed_staged)
 
-                    if staged_not_to_commit or files_changed_notstaged:
+                    if files_changed_notstaged:
+                        self.add(files=list(files_changed_notstaged))
+
+                    if staged_not_to_commit:
                         # Need an alternative index_file
                         with make_tempfile(dir=opj(self.path,
                                                    GitRepo.get_git_dir(self)),
                                            prefix="datalad-",
                                            suffix=".index") as index_file:
-                            # First add those which were changed but not staged yet
-                            if files_changed_notstaged:
-                                self.add(files=list(files_changed_notstaged))
 
                             alt_index_file = index_file
                             index_tree = self.repo.git.write_tree()
