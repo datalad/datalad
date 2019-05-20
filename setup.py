7619bdadb9285288cf954d7f58e8f1e3aebb14e6
Yaroslav Halchenko <debian@onerussian.com>
2019-05-20 11:41:12 -0400

    ENH: require "distro" for python >= 3.8

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ requires = {
         'appdirs',
         'chardet>=3.0.4',      # rarely used but small/omnipresent
         'colorama; platform_system=="Windows"',
+        'distro; python_version >= "3.8"',
         'GitPython>=2.1.8',
         'iso8601',
         'humanize',
