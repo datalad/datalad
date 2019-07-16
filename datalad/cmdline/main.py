9990513eaa3ae8eb43b998df884e7a0ddf908441
Michael Hanke <michael.hanke@gmail.com>
2019-07-16 09:43:42 +0200

    ENH: Support DataLad extensions that do not provide commands
    For example, extensions that provide metadata extractors or dataset
    procedures only.
    
    Such extensions might declare this in setup.py
    
    ```
    setup(
        ...
        entry_points = {
                'datalad.extensions': [
                    'extname=extmodule:spec',
                ],
            },
    ```
    
    with `spec` being:
    
    ```
    spec = ('My Extension Name', [])
    ```

--- datalad/cmdline/main.py
+++ datalad/cmdline/main.py
@@ -418,6 +418,11 @@ def add_entrypoints_to_interface_groups(interface_groups):
             ep.name)
         try:
             spec = ep.load()
+            if len(spec) < 2 or not spec[1]:
+                lgr.debug(
+                    'Extension does not provide a command suite: %s',
+                    ep.name)
+                continue
             interface_groups.append((ep.name, spec[0], spec[1]))
             lgr.debug('Loaded entrypoint %s', ep.name)
         except Exception as e:
