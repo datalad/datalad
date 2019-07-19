b7eb70354c4532279592037e53ec5a54982d95d9
Kyle Meyer <kyle@kyleam.com>
2019-07-19 11:21:48 -0400

    TST: benchmark-travis-pr: Move repeated config logic into a function

--- tools/ci/benchmark-travis-pr.sh
+++ tools/ci/benchmark-travis-pr.sh
@@ -2,12 +2,16 @@
 
 set -eu
 
+configure_asv () {
+    sed -i -e 's,"branches,//branches,g' -e 's,"pythons,//pythons,g' \
+        -e 's,.asv/,travisasv/,g' asv.conf.json
+}
+
 pip install asv
 asv machine --yes
 pip install -e .
 
-sed -i -e 's,"branches,//branches,g' -e 's,"pythons,//pythons,g' \
-    -e 's,.asv/,travisasv/,g' asv.conf.json
+configure_asv
 git tag '__bench_target__'
 git rev-parse HEAD __bench_target__
 asv run -E existing --set-commit-hash $(git rev-parse __bench_target__)
@@ -17,8 +21,7 @@ git checkout origin/master
 git rev-parse HEAD __bench_target__
 pip install -e .
 
-sed -i -e 's,"branches,//branches,g' -e 's,"pythons,//pythons,g' \
-    -e 's,.asv/,travisasv/,g' asv.conf.json
+configure_asv
 asv run -E existing --set-commit-hash $(git rev-parse origin/master)
 
 asv compare origin/master __bench_target__
