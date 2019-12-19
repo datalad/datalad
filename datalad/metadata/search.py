9e9e74e0e9129d9d5299e76ad108a210c9a148ea
Yaroslav Halchenko <debian@onerussian.com>
2019-12-18 21:00:11 -0500

    NF: search - --mode pyeval which evaluates python expression on provided metadata
    This mode is motivated by question on OpenNeuro
    https://neurostars.org/t/fast-tr-open-task-fmri-datasets/5601
    on how to find datasets/files with sub-second TR.
    
    Unfortunately
    
    - egrep mode is too crude since relies on dealing with string
      representation of values, so would be inconvenient to generate regular
      expression and less efficient than simply comparing numerical values.
    
    - whoosh based implementations relying on constructing index could work if
      custom index was first created. But we would need to implement
      interface for specifying index, and initial invocation would be quite slow
      since first it would need to establish index and then run the query.
      For one time invocation might be too much.  For any complicated query it
      would also require learning woosho syntax first.
    
    With pyeval we would enable an arbitrarily complex queries which could be
    expressed as a python expression.  That would even allow for use of types'
    methods (e.g. .lower() or even .format() on string values) and operations
    (+, -, etc) making it quite flexible and versatile.  Many users (if not most)
    would be able to easily construct such queries.
    
    TODOs:
    
    - [ ] Common base for egrep and pyeval.  This crude prototype was coded by
      taking egrep implementation.  Both are naive search by looping through
      provided to them records.  I think we might want
      RF to provide a common "looping" and possibly generalized treatment for
      keys reporting etc.  That would allow for centralized optimization (e.g.
      parallel execution). etc
    - [ ] docs

--- datalad/metadata/search.py
+++ datalad/metadata/search.py
@@ -978,6 +978,108 @@ class _EGrepSearch(_EGrepCSSearch):
             return q
 
 
+class _PyEvalSearch(_Search):
+    """A search engine which consumes a query
+    which is a Python code to be evaluated in an environment where metadata
+    keys are provided as variables (@id -> id).
+    """
+    _mode_label = 'pyeval'
+    _default_documenttype = 'datasets'
+
+    # def __init__(self, ds, **kwargs):
+    #     super(_PyEvalSearch, self).__init__(ds, **kwargs)
+    #     #self._queried_keys = None  # to be memoized by get_query
+
+    # TODO: I feel that we need some base class/mixin like a
+    #  _LoopySearch which would implement this main loop in a single location
+    #  with uniform treatment of metadata (adding those fields into doc etc)
+    # TODO2: that loop whould be two nested dataset/files so we could
+    #  quickly exit whenever enough per dataset (e.g. 1) found
+    def __call__(self, query, max_nresults=None, full_record=True):
+        query = assure_list(query)
+        if max_nresults is None:
+            # no limit by default
+            max_nresults = 0
+        # separate ones are considered to be independent clauses for "and"
+        composite_query = ' and '.join(['(%s)' % q for q in query])
+
+        def matcher(doc):
+            try:
+                return eval(composite_query, {}, doc)
+            except (KeyError, NameError) as exc:
+                # lgr.debug("Something was missing in the record -- should be generally ok")
+                # TODO: record if ANY matching resulted in not hitting this.
+                #  If not -- we must have misspecified something, and should
+                #  alert user similarly to get_nohits_msg of egrep.
+                #  So -- we need smth like _queried_keys here as well
+                pass
+            except Exception as exc:
+                # TODO: we could analyze each part of composite_query independently
+                # (if len(query) > 1) to provide a more specific pointer to what part
+                # has caused a failure
+                lgr.info("Failed to match %s: %s", query, exc_str())
+
+        nhits = 0
+        for res in query_aggregated_metadata(
+                reporton=self.documenttype,
+                ds=self.ds,
+                aps=[dict(path=self.ds.path, type='dataset')],
+                # MIH: I cannot see a case when we would not want recursion (within
+                # the metadata)
+                recursive=True):
+            # this assumes that files are reported after each dataset report,
+            # and after a subsequent dataset report no files for the previous
+            # dataset will be reported again
+            meta = res.get('metadata', {})
+            # produce a flattened metadata dict to search through
+            doc = meta.copy() #RF _meta2autofield_dict(meta, val2str=True, consider_ucn=consider_ucn)
+            # inject a few basic properties into the dict
+            # analog to what the other modes do in their index
+            doc.update({
+                # RF: @ replacement specific to here
+                k.replace('@', ''): res[k] for k in ('@id', 'type', 'path', 'parentds')
+                if k in res})
+            # use search instead of match to not just get hits at the start of the string
+            # this will be slower, but avoids having to use actual regex syntax at the user
+            # side even for simple queries
+            # DOTALL is needed to handle multiline description fields and such, and still
+            # be able to match content coming for a later field
+            lgr.log(7, "Querying %s among %d items", query, len(doc))
+            t0 = time()
+            is_a_match = matcher(doc)
+            dt = time() - t0
+            lgr.log(7, "Finished querying in %f sec", dt)
+            # The rest below seemed to be specific to the grep one, although
+            # TODO: check exactly what was intended
+            # # retain what actually matched
+            # matched = {k[1]: match.group() for k, match in matches.items() if match}
+            # # implement AND behavior across query expressions, but OR behavior
+            # # across queries matching multiple fields for a single query expression
+            # # for multiple queries, this makes it consistent with a query that
+            # # has no field specification
+            # if matched and len(query) == len(set(k[0] for k in matches if matches[k])):
+            if is_a_match:
+                hit = dict(
+                    res,
+                    action='search',
+                    # query_matched=matched,
+                )
+                yield hit
+                nhits += 1
+                if max_nresults and nhits == max_nresults:
+                    # report query stats
+                    topstr = '{} top {}'.format(
+                        max_nresults,
+                        single_or_plural('match', 'matches', max_nresults)
+                    )
+                    lgr.info(
+                        "Reached the limit of {}, there could be more which "
+                        "were not reported.".format(topstr)
+                    )
+                    break
+
+
+
 @build_doc
 class Search(Interface):
     """Search dataset metadata
@@ -1001,6 +1103,8 @@ class Search(Interface):
     - textblob
 
     - autofield
+
+    - pyeval
 
     An alternative default mode can be configured by tuning the
     configuration variable 'datalad.search.default-mode'::
@@ -1158,6 +1262,9 @@ class Search(Interface):
 
         % datalad search --mode autofield nifti1.description:FSL* type:file
 
+    *Mode: pyeval*
+
+    TODO
 
     *Reporting*
 
@@ -1205,7 +1312,8 @@ class Search(Interface):
             constraints=EnsureInt() | EnsureNone()),
         mode=Parameter(
             args=("--mode",),
-            choices=('egrep', 'textblob', 'autofield'),
+            # TODO egrepcs is missing
+            choices=('egrep', 'textblob', 'autofield', 'pyeval'),
             doc="""Mode of search index structure and content. See section
             SEARCH MODES for details."""),
         full_record=Parameter(
@@ -1270,6 +1378,7 @@ class Search(Interface):
             # default
             mode = ds.config.obtain('datalad.search.default-mode')
 
+        # RF avail from spagetti - we grew too many ;)
         if mode == 'egrep':
             searcher = _EGrepSearch
         elif mode == 'egrepcs':
@@ -1278,6 +1387,8 @@ class Search(Interface):
             searcher = _BlobSearch
         elif mode == 'autofield':
             searcher = _AutofieldSearch
+        elif mode == 'pyeval':
+            searcher = _PyEvalSearch
         else:
             raise ValueError(
                 'unknown search mode "{}"'.format(mode))
