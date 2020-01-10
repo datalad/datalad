7d5e28a4f68934810fb0ff753c750f1cc0f86eba
Michael Hanke <michael.hanke@gmail.com>
2020-01-10 20:19:00 +0100

    RF: Revert benchmark change to keep implementation compatible with old code

--- benchmarks/usecases.py
+++ benchmarks/usecases.py
@@ -51,27 +51,27 @@ class study_forrest(SuprocBenchmarks):
                                description='old-style, no connection to RAW')
         structural = public.create('structural', description='anatomy')
         tnt = public.create('tnt', description='image templates')
-        tnt.clone(source=phase1.path, path=opj('src', 'phase1'), reckless='auto')
-        tnt.clone(source=structural.path, path=opj('src', 'structural'), reckless='auto')
+        tnt.clone(source=phase1.path, path=opj('src', 'phase1'), reckless=True)
+        tnt.clone(source=structural.path, path=opj('src', 'structural'), reckless=True)
         aligned = public.create('aligned', description='aligned image data')
-        aligned.clone(source=phase1.path, path=opj('src', 'phase1'), reckless='auto')
-        aligned.clone(source=tnt.path, path=opj('src', 'tnt'), reckless='auto')
+        aligned.clone(source=phase1.path, path=opj('src', 'phase1'), reckless=True)
+        aligned.clone(source=tnt.path, path=opj('src', 'tnt'), reckless=True)
         # new acquisition
         labet = create(opj(path, 'private', 'labet'), description="raw data ET")
         phase2_dicoms = create(opj(path, 'private', 'p2dicoms'), description="raw data P2MRI")
         phase2 = public.create('phase2',
                                description='new-style, RAW connection')
-        phase2.clone(source=labet.path, path=opj('src', 'labet'), reckless='auto')
-        phase2.clone(source=phase2_dicoms.path, path=opj('src', 'dicoms'), reckless='auto')
+        phase2.clone(source=labet.path, path=opj('src', 'labet'), reckless=True)
+        phase2.clone(source=phase2_dicoms.path, path=opj('src', 'dicoms'), reckless=True)
         # add to derivatives
-        tnt.clone(source=phase2.path, path=opj('src', 'phase2'), reckless='auto')
-        aligned.clone(source=phase2.path, path=opj('src', 'phase2'), reckless='auto')
+        tnt.clone(source=phase2.path, path=opj('src', 'phase2'), reckless=True)
+        aligned.clone(source=phase2.path, path=opj('src', 'phase2'), reckless=True)
         # never to be published media files
         media = create(opj(path, 'private', 'media'), description="raw data ET")
         # assuming all annotations are in one dataset (in reality this is also
         # a superdatasets with about 10 subdatasets
         annot = public.create('annotations', description='stimulus annotation')
-        annot.clone(source=media.path, path=opj('src', 'media'), reckless='auto')
+        annot.clone(source=media.path, path=opj('src', 'media'), reckless=True)
         # a few typical analysis datasets
         # (just doing 2, actual status quo is just shy of 10)
         # and also the real goal -> meta analysis
@@ -79,12 +79,12 @@ class study_forrest(SuprocBenchmarks):
         for i in range(1, 2):
             ana = public.create('analysis{}'.format(i),
                                 description='analysis{}'.format(i))
-            ana.clone(source=annot.path, path=opj('src', 'annot'), reckless='auto')
-            ana.clone(source=aligned.path, path=opj('src', 'aligned'), reckless='auto')
-            ana.clone(source=tnt.path, path=opj('src', 'tnt'), reckless='auto')
+            ana.clone(source=annot.path, path=opj('src', 'annot'), reckless=True)
+            ana.clone(source=aligned.path, path=opj('src', 'aligned'), reckless=True)
+            ana.clone(source=tnt.path, path=opj('src', 'tnt'), reckless=True)
             # link to metaanalysis
             metaanalysis.clone(source=ana.path, path=opj('src', 'ana{}'.format(i)),
-                               reckless='auto')
+                               reckless=True)
             # simulate change in an input (but not raw) dataset
             create_tree(
                 aligned.path,
@@ -92,7 +92,7 @@ class study_forrest(SuprocBenchmarks):
             aligned.add('.')
         # finally aggregate data
         aggregate = public.create('aggregate', description='aggregate data')
-        aggregate.clone(source=aligned.path, path=opj('src', 'aligned'), reckless='auto')
+        aggregate.clone(source=aligned.path, path=opj('src', 'aligned'), reckless=True)
         # the toplevel dataset is intentionally left dirty, to reflect the
         # most likely condition for the joint dataset to be in at any given
         # point in time
