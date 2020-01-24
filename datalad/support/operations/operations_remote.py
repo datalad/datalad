c92cc5fe987e2f11494d8f2d1e119f6d7df902ed
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2020-01-24 18:14:30 +0100

    ENH: Add simple SSH execution + tests

--- datalad/support/operations/operations_remote.py
+++ datalad/support/operations/operations_remote.py
@@ -9,27 +9,69 @@
 """
 """
 from datalad.support.operations.operations_abstract import RemoteOperationsBase
+from datalad.support.exceptions import CommandError
+from datalad.utils import (
+    Path,
+    PurePath
+)
 from datalad.cmd import Runner
 from datalad.utils import (
     quote_cmdlinearg,
     on_windows,
 )
 
+# TODO: We actually need to add/overwrite quote_cmdlinearg/on_windows funtions
+#       to sense what's going on at the remote end. However, this requires a
+#       test setup where local+remote are different indeed. Note, that this
+#       would include to not resolve Path objects locally (prob. allow for
+#       PurePath only when addressing remote locations?).
 
 # TODO: What about Windows Servers? Does SSH give a POSIX Shell? Might need a
 #       dedicated class
-# TODO: in operations_local we integrate decision on what to use in import of
-#       `LocalOperation`. What criterion would we use here to decide what
-#       class to use?
+
 
 class RemoteSSHShellOperations(RemoteOperationsBase):
 
-    def __init__(self, cwd=None, remote_cwd=None):
-        super().__init__(cwd, remote_cwd)
+    def __init__(self, url,
+                 cwd=None,
+                 remote_cwd=None,
+                 use_remote_annex_bundle=True,
+                 force_ip=False):
 
+        super().__init__(cwd, remote_cwd)
         from datalad import ssh_manager
 
-        # TODO: For now basically use existing ssh execution
+        self.con = ssh_manager.get_connection(
+            url=url,
+            use_remote_annex_bundle=use_remote_annex_bundle,
+            force_ip=force_ip)
+
+        # TODO: Not sure yet about the following part. Might be better to
+        #   to deal with this per command
+        self.con.open()
+        if self._remote_cwd:
+            self.con("cd {}".format(quote_cmdlinearg(str(self._remote_cwd))))
+
+    def make_directory(self, path, force=False):
+        self.con("mkdir {} {}".format("-p" if force else "",
+                                      quote_cmdlinearg(str(path))))
+
+    def exists(self, path):
+        try:
+            self.con("test -e {}".format(quote_cmdlinearg(str(path))))
+            return True
+        except CommandError:
+            return False
+
+    def remove(self, path, recursive=False):
+
+        self.con('rm {} -f {}'.format('-r' if recursive else '',
+                                      quote_cmdlinearg(str(path))))
+
+    def rename(self, src, dst):
+
+        self.con('mv {} {}'.format(quote_cmdlinearg(str(src)),
+                                   quote_cmdlinearg(str(dst))))
 
 
 class RemotePersistentSSHShellOperations(RemoteOperationsBase):
@@ -38,3 +80,11 @@ class RemotePersistentSSHShellOperations(RemoteOperationsBase):
     #  everything through a persistent remote shell
 
     pass
+
+
+# TODO: in operations_local we integrate decision on what to use in import of
+#       `LocalOperation`. What criterion would we use here to decide what
+#       class to use?
+#       -> decision may well be based on "URL-scheme" including ones like
+#       "ria+ssh"
+RemoteOperation = RemoteSSHShellOperations
--- datalad/support/operations/tests/test_basic_operations.py
+++ datalad/support/operations/tests/test_basic_operations.py
@@ -6,12 +6,16 @@
 #   copyright and license terms.
 #
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-
-
+"""
+"""
 
 from datalad.support.operations.operations_local import LocalOperation
+from datalad.support.operations.operations_remote import RemoteOperation
 from datalad.support.exceptions import CommandError
-from datalad.utils import Path
+from datalad.utils import (
+    Path,
+    PurePath
+)
 from datalad.tests.utils import (
     assert_raises,
     assert_false,
@@ -38,7 +42,8 @@ from datalad.tests.utils import (
     with_testrepos,
     OBSCURE_FILENAME,
     SkipTest,
-    has_symlink_capability
+    has_symlink_capability,
+    skip_ssh
 )
 
 
@@ -65,6 +70,39 @@ def test_make_directory_local(cwd, path):
     rel_path = Path('another')
     ops.make_directory(rel_path)
     assert (cwd / rel_path).is_dir()
+
+
+@skip_ssh
+@with_tempfile(mkdir=True)
+@with_tempfile(mkdir=True)
+@with_tempfile
+def test_make_directory_remote(cwd, remote_cwd, path):
+    cwd = Path(cwd)
+    remote_cwd = Path(remote_cwd)
+    path = Path(path)
+    deep = path / 'deeper' / OBSCURE_FILENAME
+
+    # NOTE, that "datalad-test" is supposed to be localhost. So, absolute paths
+    # match on "local" and "remote" end.
+    ops = RemoteOperation("ssh://datalad-test:",
+                          cwd=cwd,
+                          remote_cwd=remote_cwd)
+
+    # raise on non-existing parents:
+    assert_raises(CommandError, ops.make_directory, deep)
+    # doesn't raise:
+    ops.make_directory(path)
+    ok_(path.is_dir())
+    # raise on existing target:
+    assert_raises(CommandError, ops.make_directory, path)
+    # doesn't raise:
+    ops.make_directory(deep, force=True)
+    ok_(deep.is_dir())
+    # relative resolves based on cwd:
+    rel_path = Path('another')
+    ops.make_directory(rel_path)
+    ok_((remote_cwd / rel_path).is_dir())
+    assert_false((cwd / rel_path).is_dir())
 
 
 @with_tempfile
@@ -103,6 +141,49 @@ def test_exists_local(file_path, dir_path, link_path, cwd):
     ok_(ops.exists(Path('relative')))
 
 
+@skip_ssh
+@with_tempfile
+@with_tempfile
+@with_tempfile
+@with_tempfile(mkdir=True)
+@with_tempfile(mkdir=True)
+def test_exists_remote(file_path, dir_path, link_path, cwd, remote_cwd):
+    file_path = Path(file_path)
+    dir_path = Path(dir_path)
+    link_path = Path(link_path)
+    cwd = Path(cwd)
+    remote_cwd = Path(remote_cwd)
+    # NOTE, that "datalad-test" is supposed to be localhost. So, absolute paths
+    # match on "local" and "remote" end.
+    ops = RemoteOperation("ssh://datalad-test:",
+                          cwd=cwd,
+                          remote_cwd=remote_cwd)
+
+    # regular file:
+    assert_false(ops.exists(file_path))
+    file_path.write_text('some')
+    ok_(ops.exists(file_path))
+
+    # symlink
+    if has_symlink_capability():
+        assert_false(ops.exists(link_path))
+        link_path.symlink_to('broken')
+        ok_(ops.exists(link_path))
+        ops.remove(link_path)
+        link_path.symlink_to(file_path)
+        ok_(ops.exists(link_path))
+
+    # directory
+    assert_false(ops.exists(dir_path))
+    ops.make_directory(dir_path)
+    ok_(ops.exists(dir_path))
+
+    # relative
+    assert_false(ops.exists(Path('relative')))
+    (remote_cwd / 'relative').write_text('content')
+    ok_(ops.exists(Path('relative')))
+
+
 @with_tempfile
 @with_tempfile
 @with_tempfile(mkdir=True)
@@ -132,7 +213,51 @@ def test_remove_local(dir_path, link_path, cwd):
         ops.remove(link_path)
 
     # relative to correct base:
-    relative = (cwd / OBSCURE_FILENAME)
+    relative = cwd / OBSCURE_FILENAME
+    relative.write_text("doesn't matter")
+    ok_(ops.exists(relative))
+    ops.remove(Path(OBSCURE_FILENAME))
+    assert_false(ops.exists(relative))
+
+
+@skip_ssh
+@with_tempfile
+@with_tempfile
+@with_tempfile(mkdir=True)
+@with_tempfile(mkdir=True)
+def test_remove_remote(dir_path, link_path, cwd, remote_cwd):
+
+    dir_path = Path(dir_path)
+    link_path = Path(link_path)
+    cwd = Path(cwd)
+    remote_cwd = Path(remote_cwd)
+
+    # NOTE, that "datalad-test" is supposed to be localhost. So, absolute paths
+    # match on "local" and "remote" end.
+    ops = RemoteOperation("ssh://datalad-test:",
+                          cwd=cwd,
+                          remote_cwd=remote_cwd)
+
+    # non-existent doesn't raise:
+    ops.remove(dir_path)
+    ops.make_directory(dir_path)
+    ok_(ops.exists(dir_path))
+    # directory, but w/o recursive
+    assert_raises(CommandError, ops.remove, dir_path)
+    ok_(ops.exists(dir_path))
+    underneath = (dir_path / 'some')
+    underneath.write_text('content')
+    # non-empty dir w/ recursive
+    ops.remove(dir_path, recursive=True)
+    assert_false(ops.exists(dir_path))
+
+    if has_symlink_capability():
+        # remove a link
+        link_path.symlink_to('broken')
+        ops.remove(link_path)
+
+    # relative to correct base:
+    relative = remote_cwd / OBSCURE_FILENAME
     relative.write_text("doesn't matter")
     ok_(ops.exists(relative))
     ops.remove(Path(OBSCURE_FILENAME))
@@ -164,6 +289,46 @@ def test_rename_local(cwd):
     if has_symlink_capability():
         # rename symlink
         link_path = cwd / "somelink"
+        link_path.symlink_to('broken')
+        ok_(ops.exists(link_path))
+        ops.rename(link_path, Path("newlink"))
+        ok_(ops.exists(Path("newlink")))
+        assert_false(ops.exists(link_path))
+
+
+@skip_ssh
+@with_tempfile(mkdir=True)
+@with_tempfile(mkdir=True)
+def test_rename_remote(cwd, remote_cwd):
+    cwd = Path(cwd)
+    remote_cwd = Path(remote_cwd)
+
+    # NOTE, that "datalad-test" is supposed to be localhost. So, absolute paths
+    # match on "local" and "remote" end.
+    ops = RemoteOperation("ssh://datalad-test:",
+                          cwd=cwd,
+                          remote_cwd=remote_cwd)
+
+    # rename regular file
+    file_path = remote_cwd / "first"
+    file_path.write_text("first")
+    ops.rename(Path("first"), Path("second"))
+    ok_(ops.exists(Path("second")))
+    assert_false(ops.exists(Path("first")))
+    eq_("first", (remote_cwd / "second").read_text())
+
+    # rename dir
+    dir_path = remote_cwd / "dir"
+    ops.make_directory(dir_path)
+    dir_path.is_dir()
+    ops.rename(Path("dir"), Path(OBSCURE_FILENAME))
+    ok_(ops.exists(Path(OBSCURE_FILENAME)))
+    (remote_cwd / OBSCURE_FILENAME).is_dir()
+    assert_false(ops.exists(dir_path))
+
+    if has_symlink_capability():
+        # rename symlink
+        link_path = remote_cwd / "somelink"
         link_path.symlink_to('broken')
         ok_(ops.exists(link_path))
         ops.rename(link_path, Path("newlink"))
