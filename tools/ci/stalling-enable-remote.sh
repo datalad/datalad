b817784cb36a33e7d451476a38175d4f50e32970
Yaroslav Halchenko <debian@onerussian.com>
2020-03-02 15:32:49 -0500

    TEMP: add date calls so we know when things happen

--- tools/ci/stalling-enable-remote.sh
+++ tools/ci/stalling-enable-remote.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -eu
+date
 #http_proxy= DATALAD_TESTS_SSH=1 DATALAD_LOG_LEVEL=1 DATALAD_LOG_OUTPUTS=1 python -m nose -s -v datalad/distribution/tests/test_publish.py:test_publish_depends
 http_proxy= DATALAD_TESTS_SSH=1 DATALAD_LOG_LEVEL=1 DATALAD_LOG_OUTPUTS=1 python -m nose -s -v ../datalad/distribution/tests/test_publish.py:test_publish_depends >| /tmp/nose-log.log 2>&1 & \
-while ! grep enableremote.*target2 < /tmp/nose-log.log; do sleep 1; done; sleep 1; echo "GOT THERE! "; ps auxw -H; 
+while ! grep enableremote.*target2 < /tmp/nose-log.log; do sleep 1; done; echo "GOT THERE! "; date; ps auxw -H; 
 
-
+echo "Waiting"
+date
 wait
 
