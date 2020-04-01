50b59293f2e505981b36b4e5ad67c87a29d86637
Kyle Meyer <kyle@kyleam.com>
2020-04-01 15:46:00 -0400

    RF: create-sibling: Avoid repeated .repo access
    The .repo property is more expensive than typical attribute lookup.
    Assign it to a variable.

--- datalad/distribution/create_sibling.py
+++ datalad/distribution/create_sibling.py
@@ -98,6 +98,7 @@ def _create_dataset_sibling(
         # see gh-1188
         remoteds_path = normpath(opj(target_dir, ds_name))
 
+    ds_repo = ds.repo
     # construct a would-be ssh url based on the current dataset's path
     ssh_url.path = remoteds_path
     ds_sshurl = ssh_url.as_str()
@@ -171,7 +172,7 @@ def _create_dataset_sibling(
                 # if we succeeded in removing it
                 path_exists = False
                 # Since it is gone now, git-annex also should forget about it
-                remotes = ds.repo.get_remotes()
+                remotes = ds_repo.get_remotes()
                 if name in remotes:
                     # so we had this remote already, we should announce it dead
                     # XXX what if there was some kind of mismatch and this name
@@ -181,9 +182,9 @@ def _create_dataset_sibling(
                         "Announcing existing remote %s dead to annex and removing",
                         name
                     )
-                    if isinstance(ds.repo, AnnexRepo):
-                        ds.repo.set_remote_dead(name)
-                    ds.repo.remove_remote(name)
+                    if isinstance(ds_repo, AnnexRepo):
+                        ds_repo.set_remote_dead(name)
+                    ds_repo.remove_remote(name)
             elif existing == 'reconfigure':
                 lgr.info(_msg + " Will only reconfigure")
                 only_reconfigure = True
