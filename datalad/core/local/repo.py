cbae12b193190afbe7bb6c29b2ebe5da3bb5cd6a
Michael Hanke <michael.hanke@gmail.com>
2020-07-11 10:32:35 +0200

    RF: Add compatibility module to ensure seamless upgrades

---
+++ datalad/core/local/repo.py
@@ -0,0 +1,15 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the datalad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+""" Backward-compatibility module
+
+Use datalad.core.dataset.utils instead.
+
+"""
+
+from datalad.core.dataset.utils import repo_from_path
