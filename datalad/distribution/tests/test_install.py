77d9e2996ffa4f95eee5494df387967edf724d2b
Yaroslav Halchenko <debian@onerussian.com>
2020-08-04 21:40:02 -0400

    ENH(TST): annotate another slow test as @slow

--- datalad/distribution/tests/test_install.py
+++ datalad/distribution/tests/test_install.py
@@ -307,6 +307,7 @@ def test_install_dataladri(src, topurl, path):
 
 
 # https://github.com/datalad/datalad/pull/3975/checks?check_run_id=369789022#step:8:338
+@slow   # 46sec on Yarik's laptop and tripped Travis CI
 @known_failure_windows
 @with_testrepos('submodule_annex', flavors=['local', 'local-url', 'network'])
 @with_tempfile(mkdir=True)
