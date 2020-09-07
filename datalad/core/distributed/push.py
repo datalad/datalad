359bb2f9a91a218aea098ad19e34c322aa6a2cb3
Kyle Meyer <kyle@kyleam.com>
2020-09-07 10:26:57 -0400

    BF: push: Fix GitWitlessRunner -> _git_runner substitution
    A couple of commits back, 92ce1669d (RF: Avoid multiple
    GitWitlessRunner instances) did an incomplete replacement in this
    spot.

--- datalad/core/distributed/push.py
+++ datalad/core/distributed/push.py
@@ -830,9 +830,7 @@ def _push_data(ds, target, content, data, force, jobs, res_kwargs,
         # and go
         # TODO try-except and yield what was captured before the crash
         #res = GitWitlessRunner(
-        res = ds_repo._git_runner(
-            cwd=ds.path,
-        ).run(
+        res = ds_repo._git_runner.run(
             cmd,
             # TODO report how many in total, and give global progress too
             protocol=TailoredPushAnnexJsonProtocol,
