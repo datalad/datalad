cd38d1f5728c7abe24b9a9588325abb8746fb58c
Michael Hanke <michael.hanke@gmail.com>
2020-09-07 11:41:25 +0200

    TST; Document gh-4888

--- datalad/core/distributed/tests/test_push.py
+++ datalad/core/distributed/tests/test_push.py
@@ -815,3 +815,17 @@ def test_push_git_annex_branch_many_paths_same_data(path):
     # 3 files point to content already covered by another file.
     assert_result_count(res, 3,
                         action="copy", type="file", status="notneeded")
+
+
+@with_tree(tree={"ds": {"f0": "0"}})
+def test_push_matching(path):
+    path = Path(path)
+    ds = Dataset(path / "ds").create(force=True)
+    ds.config.set('push.default', 'matching', where='local')
+    ds.save()
+    remote_ds = ds.create_sibling(path / 'dssibling', result_xfm='datasets')[0]
+    # that fact that the next one even runs makes sure that we are in a better
+    # place than https://github.com/datalad/datalad/issues/4888
+    ds.push(to='local')
+    # and we pushed the commit in the current branch
+    ok_(remote_ds.repo.get_hexsha(DEFAULT_BRANCH), ds.repo.get_hexsha())
