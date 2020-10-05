e9ab60bbf5157fad298a1e3693b7445d9eb6b4db
Adina Wagner <adina.wagner@t-online.de>
2020-10-05 13:56:53 +0200

    Workaround: Add list of common textfiles on Windows in text2git
    As Mimeencoding is not yet functional on Windows (see #3360), the text2git configuration is broken,
    and leaves the dataset in an unusable state once applied (until the configuration is removed by hand).
    This change sets largefile = nothing rules for a list of common text file extensions when on Windows
    instead of the standard text2git configuration.
    
    Note that this means that datasets created with text2git on non-Windows machines aren't portable to
    Windows, but vice-versa.

--- datalad/resources/procedures/cfg_text2git.py
+++ datalad/resources/procedures/cfg_text2git.py
@@ -2,6 +2,7 @@
 
 import sys
 import os.path as op
+import platform
 
 from datalad.distribution.dataset import require_dataset
 
@@ -10,12 +11,78 @@ ds = require_dataset(
     check_installed=True,
     purpose='configuration')
 
-annex_largefiles = '((mimeencoding=binary)and(largerthan=0))'
-attrs = ds.repo.get_gitattributes('*')
-if not attrs.get('*', {}).get(
-        'annex.largefiles', None) == annex_largefiles:
-    ds.repo.set_gitattributes([
-        ('*', {'annex.largefiles': annex_largefiles})])
+if platform.system().lower() == 'windows':
+    # mimeencoding isn't functional under Windows yet (see #3360), this adds
+    # common extensions of text files by hand
+    force_in_git = [
+        '*.txt',
+        '*.json*',
+        '*.log',
+        '*.tsv',
+        '*.csv',
+        '*html',
+        '*.css',
+        '*.xml',
+        '*.rss',
+        '*.md',
+        '*.markdown',
+        '*.rmd',
+        '*.rst',
+        '*.rest',
+        '*.yml',
+        '*.toml',
+        '*.adoc',
+        '*.asc',
+        '*.tex',
+        '*.sty',
+        '*.bib',
+        '*.cls',
+        '*.aux',
+        '*.bst',
+        '*.clo',
+        '*.ini',
+        '*.cfg',
+        '*.cgi',
+        '*.py',
+        '*.pyx',
+        '*.go',
+        '*.java',
+        '.*js',
+        '*.sh',
+        '*.bash',
+        '*.zsh',
+        '*.pl',
+        '*.R',
+        '*.jl',
+        '*.ipynb'
+        '*.m',
+        '*.matlab',
+        '*awk',
+        '*.bat',
+        '*.cmd',
+        '*.cake',
+        '*.cmake',
+        'Makefile',
+        '*.c++',
+        '*.cpp',
+        '*.h',
+        '*.hh',
+        '*.inc',
+        '*.cp',
+        '*.cu',
+        '*.emacs',
+        '*.emacs.desktop'
+    ]
+    ds.repo.set_gitattributes(
+        [(p, {'annex.largefiles': 'nothing'}) for p in force_in_git])
+else:
+    annex_largefiles = '((mimeencoding=binary)and(largerthan=0))'
+
+    attrs = ds.repo.get_gitattributes('*')
+    if not attrs.get('*', {}).get(
+            'annex.largefiles', None) == annex_largefiles:
+        ds.repo.set_gitattributes([
+            ('*', {'annex.largefiles': annex_largefiles})])
 
 git_attributes_file = op.join(ds.path, '.gitattributes')
 ds.save(
