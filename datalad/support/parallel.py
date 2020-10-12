52870f38dfff268d27d1a9b1109e30d589044109
Yaroslav Halchenko <debian@onerussian.com>
2020-10-12 17:15:37 -0400

    NF: Initial prototype for ConsumerProducer all-in-1 with logging of progress
    Based on Threads, it would parallelize execution of producer (e.g. of paths)
    and consumer (e.g. create) to operate on subdatasets in parallel threads.
    Given that underlying git/git-annex processes would also consume CPU, overall
    we can escape 1 CPU "performance" bottleneck in cases of parallel operation
    across (sub)datasets.

--- datalad/support/parallel.py
+++ datalad/support/parallel.py
@@ -15,6 +15,7 @@ __docformat__ = 'restructuredtext'
 import datetime as dt
 # import humanize
 import inspect
+import time
 import uuid
 from collections import defaultdict
 
@@ -22,6 +23,10 @@ from ..log import log_progress
 from .iterators import IteratorWithAggregation
 
 from . import ansi_colors as colors
+
+from queue import Queue, Empty
+from threading import Thread
+import concurrent.futures
 
 
 def _count_str(count, verb, omg=False):
@@ -72,7 +77,6 @@ def producer_consumer(
     count = Count()
     it = IteratorWithAggregation(producer, count)
     producer_finished = False
-    # t0 = dt.datetime.now()
     for item in it:
         # keep updating total as producer gives more items
         if not producer_finished:
@@ -123,3 +127,212 @@ def producer_consumer(
     #          # humanize.precisedelta(dt.datetime.now() - t0),
     #          humanize.naturaltime(dt.datetime.now() - t0)
     #          )
+
+
+class ProducerConsumer:
+    """
+    """
+
+    def __init__(self,
+                 producer, consumer,
+                 njobs=None,
+                 consumer_depchecker=None,
+                 reraise_immediately=False,
+                 agg=None,
+                 # TODO: "absorb" into some more generic "logging" helper
+                 # so could be used not only with consumers which yield our records
+                 # 'agg' from above could also more relate to i
+                 label="Total", unit="items",
+                 lgr=None,
+                 ):
+        """
+
+        Parameters
+        ----------
+        ...
+        reraise_immediately: bool, optional
+          If True, it would stop producer yielding values as soon as it detects that some
+          exception has occurred (although there might still be values in the queue to be yielded
+          which were collected before the exception was raised)
+        """
+        self.producer = producer
+        self.consumer = consumer
+        self.njobs = njobs
+        self.consumer_depchecker = consumer_depchecker
+        self.reraise_immediately = reraise_immediately
+        self.agg = agg
+        self.label = label
+        self.unit = unit
+        self.lgr = lgr
+
+        self.total = None
+        self.producer_finished = None
+        self._exc = None
+
+    def __iter__(self):
+        self.producer_finished = False
+        self._exc = None
+
+        producer_queue = Queue()
+        consumer_queue = Queue()
+
+        def producer_worker():
+            """That is the one which interrogates producer and updates .total"""
+            total = None if self.agg else 0
+            try:
+                for value in self.producer:
+                    producer_queue.put(value)
+                    if self.agg:
+                        total = (
+                            self.agg(value, total) if total is not None else self.agg(value)
+                        )
+                    else:
+                        total += 1
+                    self.total = total
+            except BaseException as e:
+                self._exc = e
+            finally:
+                self.producer_finished = True
+
+        def consumer_worker(callable, *args, **kwargs):
+            """Since jobs could return a generator and we cannot really "inspect" for that
+            """
+            try:
+                res = callable(*args, **kwargs)
+                if inspect.isgenerator(res):
+                    lgr.debug("Got consumer worker which returned a generator %s", res)
+                    didgood = False
+                    for r in res:
+                        didgood = True
+                        lgr.debug("Adding %s to queue", r)
+                        consumer_queue.put(r)
+                    if not didgood:
+                        lgr.error("Nothing was obtained from %s :-(", res)
+                else:
+                    lgr.debug("Got straight result %s, not a generator", res)
+                    consumer_queue.put(res)
+            except BaseException as e:
+                # TODO: avoid masking exceptions, collect all???
+                self._exc = e
+
+        producer_thread = Thread(target=producer_worker)
+        producer_thread.start()
+
+        # LOGGING
+        pid = str(uuid.uuid4())  # could be based on PID and time may be to be informative?
+        lgr = self.lgr
+        label = self.label
+        if lgr is None:
+            from .. import lgr
+        log_progress(lgr.info, pid,
+                     "%s: starting", self.label,
+                     # will become known only later total=len(items),
+                     label=self.label, unit=" " + self.unit)
+        counts = defaultdict(int)
+
+        futures = {}
+        total_announced = False
+        njobs = self.njobs or 1
+        lgr.debug("Initiating ThreadPoolExecutor with %d jobs", njobs)
+        t0 = time.time()
+        with concurrent.futures.ThreadPoolExecutor(njobs) as executor:
+            # yield from the producer_queue (.total and .finished could be accessed meanwhile)
+            while True:
+                done_useful = False
+                if self.reraise_immediately and self._exc is not None:
+                    # stop all possibly running futures
+                    # executor.shutdown()
+                    break
+                if (self.producer_finished and
+                        not futures and
+                        consumer_queue.empty() and
+                        producer_queue.empty()):
+                    break
+
+                # LOGGING
+                if not total_announced and self.producer_finished:
+                    log_progress(
+                        lgr.info,
+                        pid,
+                        None,  # I do not think there is something valuable to announce
+                        total=self.total,
+                        # unfortunately of no effect, so we cannot inform that more items to come
+                        # unit=("+" if not it.finished else "") + " " + unit,
+                        update=0  # not None, so it does not stop
+                    )
+                    total_announced = True
+
+                # important!  We are using threads, so worker threads will be sharing CPU time
+                # with this master thread. For it to become efficient, we should consume as much
+                # as possible from producer asap and push it to executor.  So drain the queue
+                while not producer_queue.empty():
+                    done_useful = True
+                    try:
+                        job_args = producer_queue.get() # timeout=0.001)
+                        if self.consumer_depchecker:
+                            # Sleep a little if we are not yet ready
+                            # TODO: add some .debug level reporting based on elapsed time
+                            # IIRC I did smth like growing exponentially delays somewhere (dandi?)
+                            while not self.consumer_depchecker(futures, job_args):
+                                time.sleep(0.01)
+                        # Current implementation, to provide depchecking, relies on unique
+                        # args for the job
+                        assert job_args not in futures
+                        lgr.debug("Submitting worker future for %s", job_args)
+                        futures[job_args] = executor.submit(consumer_worker, self.consumer, job_args)
+                    except Empty:
+                        pass
+
+                # check active futures
+                if not consumer_queue.empty():
+                    done_useful = True
+
+                    # ATM we do not bother of some "in order" reporting
+                    # Just report as soon as any new record arrives
+                    res = consumer_queue.get()
+                    lgr.debug("Got %s from consumer_queue", res)
+                    # LOGGING
+                    counts[res["status"]] += 1
+                    count_strs = (_count_str(*args)
+                                  for args in [(counts["notneeded"], "skipped", False),
+                                               (counts["error"], "failed", True)])
+                    if counts["notneeded"] or counts["error"]:
+                        label = "{} ({})".format(
+                            self.label,
+                            ", ".join(filter(None, count_strs)))
+
+                    log_progress(
+                        lgr.error if res["status"] == "error" else lgr.info,
+                        pid,
+                        "%s: processed result%s", self.label,
+                        " for " + res["path"] if "path" in res else "",
+                        label=label, update=1, increment=True)
+
+                    yield res
+
+                # remove futures which are done
+                for args, future in list(futures.items()):
+                    if future.done():
+                        done_useful = True
+                        future_ = futures.pop(args)
+                        msg = ""
+                        if future_.exception():
+                            msg = " with an exception %s" % future_.exception()
+                        lgr.debug("Future for %r is done%s", args, msg)
+
+                if not done_useful:  # you need some rest
+                    # TODO: same here -- progressive logging
+                    lgr.debug(
+                        "Did nothing useful, sleeping. Have %s %s %s %s",
+                        self.producer_finished,
+                        futures,
+                        consumer_queue.empty(),
+                        producer_queue.empty())
+                    time.sleep(0.2)
+
+        # LOGGING
+        log_progress(lgr.info, pid, "%s: done", self.label)
+        assert not futures
+        producer_thread.join()
+        if self._exc is not None:
+            raise self._exc
--- datalad/support/tests/test_parallel.py
+++ datalad/support/tests/test_parallel.py
@@ -10,10 +10,14 @@
 from time import sleep, time
 
 # absolute import only to be able to run test without `nose` so to see progress bar
-from datalad.support.parallel import producer_consumer
-from datalad.tests.utils import with_tempfile
+from datalad.support.parallel import ProducerConsumer
+from datalad.tests.utils import (
+    assert_equal,
+    with_tempfile,
+)
 
-def test_producer_consumer():
+
+def test_ProducerConsumer():
     def slowprod(n, secs=0.1):
         for i in range(n):
             yield i
@@ -27,25 +31,27 @@ def test_producer_consumer():
         sleep(0.2)
         #print(f"Consumed {i} in {time() - t0}")
         yield {
-            "status": "ok" if i % 2 else "error"
+            "i": i, "status": "ok" if i % 2 else "error"
         }
-    list(producer_consumer(
+    assert_equal(list(ProducerConsumer(
         slowprod(10),
         slowcons,
-    ))
+        njobs=10,
+    )), [{"i": i, "status": "ok" if i % 2 else "error"} for i in range(10)])
 
 
 @with_tempfile(mkdir=True)
 def test_creatsubdatasets(topds_path, n=10):
-    from datalad import lgr
     from datalad.distribution.dataset import Dataset
-    ds = Dataset(topds_path).create(cfg_proc="yoda")
-    list(producer_consumer(
+    ds = Dataset(topds_path).create() # cfg_proc="yoda")
+    list(ProducerConsumer(
         ("subds%d" % i for i in range(n)),
-        lambda n: ds.create(n, cfg_proc="yoda", result_xfm=None, return_type='generator')
+        lambda n: ds.create(n, #cfg_proc="yoda",
+                            result_xfm=None, return_type='generator'),
+        #njobs=5
     ))
 
 
 if __name__ == '__main__':
-    test_producer_consumer()
-    # test_creatsubdatasets()
+    test_ProducerConsumer()
+    #test_creatsubdatasets()
