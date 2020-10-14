9fb2e9ed7cb9ce9e093d0066fc1d7cb5f9875937
Yaroslav Halchenko <debian@onerussian.com>
2020-10-14 19:40:17 -0400

    ENH+RF: make bisect python script accept CP_COMMIT to CP a fix for building

--- tools/ci/bisect-python.sh
+++ tools/ci/bisect-python.sh
@@ -15,9 +15,9 @@
 # $> git bisect old v3.8.0a1
 # Bisecting: 1017 revisions left to test after this (roughly 10 steps)
 # [3880f263d2994fb1eba25835dddccb0cf696fdf0] bpo-36933: Remove sys.set_coroutine_wrapper (marked for removal in 3.8) (GH-13577)
-# $> git bisect run ~/proj/datalad/datalad-master/tools/ci/bisect-python.sh python ~/proj/datalad/datalad-master/datalad/support/tests/test_parallel.py
-# $> git bisect run ~/proj/datalad/datalad-master/tools/ci/bisect-python.sh bash -c 'python3 ~/proj/datalad/datalad-master/datalad/support/tests/test_parallel.py || exit 1'
-
+# $> CP_COMMIT=cb083f7cdf604c1d9d264f387f9e8846bc953eb3 bisect run ~/proj/datalad/datalad-master/tools/ci/bisect-python.sh 'python3 ~/proj/datalad/datalad-master/datalad/support/tests/test_parallel.py && exit 1 || exit 0'
+# Actually above example was never tried, I did use helper manually,
+# but should work
 set -eu
 export PS4='> '
 
@@ -26,50 +26,59 @@ _datalad_src=$(dirname "$0")
 _datalad_src=$(readlink -f "${_datalad_src}/../..")
 
 echo "Python source: $_cpython_src   DataLad: $_datalad_src"
-if [ -e "${_cpython_src}/configure" ] && [ -e "${_datalad_src}/setup.py" ]; then
-  _ver=$(git -C "$_cpython_src" describe)
+if [ ! -e "${_cpython_src}/configure" ] || [ ! -e "${_datalad_src}/setup.py" ]; then
+  echo "ERROR: no needed sources were found" >&2
+  exit 125
+fi
+_ver=$(git -C "$_cpython_src" describe)
+if [ ! -z "${CP_COMMIT:-}" ]; then
+  _ver="${_ver}+${CP_COMMIT:0:6}"
+fi
+_destdir="${_cpython_src}-builds/${_ver}"
+_python="${_destdir}/usr/local/bin/python3"
 
-  _destdir="${_cpython_src}-builds/${_ver}"
-  _python="${_destdir}/usr/local/bin/python3"
-
-  if [ ! -e "${_python}" ]; then
+_venv_d="${VIRTUALENV_PATH:-${_datalad_src}/venvs/build-${_ver}}";
+redopip=
+if [ ! -e "${_python}" ]; then
+  echo "INFO: Building python"
+  redopip=1
+  rm -rf "${_venv_d}"  # old one would not be good anyways
   (
       cd "${_cpython_src}"
       chronic git clean -dfx
+      if [ ! -z "${CP_COMMIT:-}" ]; then
+        git cherry-pick "${CP_COMMIT}" || exit 125
+      fi
       PATH=/usr/lib/ccache:$PATH chronic ./configure || exit 125
       PATH=/usr/lib/ccache:$PATH chronic make -j8 install DESTDIR="${_destdir}" || exit 125
   )
-  else
-    echo "SKIP: $_python is already there, skipping building python"
-  fi
-
-  # create virtualenv
-  _venv_d="${VIRTUALENV_PATH:-${_datalad_src}/venvs/build-${_ver}}";
-  if [ ! -e "${_venv_d}" ]; then
-    chronic virtualenv --python="${_python}" "${_venv_d}" || exit 125
-
-    source "${_venv_d}/bin/activate"
-    chronic pip3 install -e "${_datalad_src}/.[devel]" || exit 125
-  else
-    source "${_venv_d}/bin/activate"
-    echo "SKIP: $_venv_d already there, skipping virtualenv + pip call"
-  fi
-
-  echo "All ready:
-  build:   $_destdir
-  venv:    ${_venv_d}
-  source:  source \"${_venv_d}/bin/activate\"
-  python:  $(which python3)
-  ver:     $(python3 --version)
-  datalad: $(git -C "${_datalad_src}" describe)
-"
-  if [ "$#" != 0 ]; then
-    echo "INFO: running bisection command $*"
-    bash -c "$*"
-  else
-    echo "INFO: no bisection command given"
-  fi
 else
-  echo "ERROR: no needed sources were found" >&2
-  exit 125
+  echo "SKIP: $_python is already there, skipping building python"
+fi
+
+# create virtualenv
+if [ ! -z "$redopip" ] || [ ! -e "${_venv_d}" ]; then
+  echo "INFO: Creating virtualenv"
+  chronic virtualenv --python="${_python}" "${_venv_d}" || exit 125
+
+  source "${_venv_d}/bin/activate"
+  chronic pip3 install -e "${_datalad_src}/.[devel]" || exit 125
+else
+  source "${_venv_d}/bin/activate"
+  echo "SKIP: $_venv_d already there, skipping virtualenv + pip call"
+fi
+
+echo "All ready:
+build:   $_destdir
+venv:    ${_venv_d}
+source:  source \"${_venv_d}/bin/activate\"
+python:  $(which python3)
+ver:     $(python3 --version)
+datalad: $(git -C "${_datalad_src}" describe)
+"
+if [ "$#" != 0 ]; then
+  echo "INFO: running bisection command $*"
+  bash -c "$*"
+else
+  echo "INFO: no bisection command given"
 fi
