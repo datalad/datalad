b2132489407a530b70b8c7d4b7eca0f83b04c8b7
Kyle Meyer <kyle@kyleam.com>
2021-03-02 17:46:33 -0500

    TST: clone: Loosen an assert_logged() regexp
    test_cfg_originorigin() checks that git-annex-init is called by
    looking at the log output, but it doesn't account for the fact that
    _GIT_COMMON_OPTIONS can come between "git" and "annex".  At the
    moment, this passes because _GIT_COMMON_OPTIONS is empty, but that
    will no longer be the case with the next commit.

--- datalad/core/distributed/tests/test_clone.py
+++ datalad/core/distributed/tests/test_clone.py
@@ -624,7 +624,7 @@ def test_cfg_originorigin(path):
     with chpwd(path), swallow_logs(new_level=logging.DEBUG) as cml:
         clone_lev3 = clone('clone_lev2', 'clone_lev3')
         # we called git-annex-init; see gh-4367:
-        cml.assert_logged(msg=r"[^[]*Async run \[('git', 'annex'|'git-annex'), "
+        cml.assert_logged(msg=r"[^[]*Async run \[('git',.*'annex'|'git-annex'), "
                               r"'init'",
                           match=False,
                           level='DEBUG')
