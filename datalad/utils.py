55af1a5f5b4ddc121ea6027190de1571078002ef
Michael Hanke <michael.hanke@gmail.com>
2021-04-12 17:21:52 +0200

    RF: Move otherwise unused safe_print() helper fpr ls() to -deprecated

--- datalad/utils.py
+++ datalad/utils.py
@@ -2087,20 +2087,6 @@ def slash_join(base, extension):
          extension.lstrip('/')))
 
 
-def safe_print(s):
-    """Print with protection against UTF-8 encoding errors"""
-    # A little bit of dance to be able to test this code
-    print_f = getattr(builtins, "print")
-    try:
-        print_f(s)
-    except UnicodeEncodeError:
-        # failed to encode so let's do encoding while ignoring errors
-        # to print at least something
-        # explicit `or ascii` since somehow on buildbot it seemed to return None
-        s = s.encode(getattr(sys.stdout, 'encoding', 'ascii') or 'ascii', errors='ignore') \
-            if hasattr(s, 'encode') else s
-        print_f(s.decode())
-
 #
 # IO Helpers
 #
