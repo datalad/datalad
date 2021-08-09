8e3eafb7cc413c799b8f84461b3276a90f6d1ed6
Yaroslav Halchenko <debian@onerussian.com>
2021-08-09 15:04:25 -0400

    BF: take path from SSHRI, test URLs not only on Windows
    While extending test with SSH RIs I realized that
    
    a + b if False else d == d, so in effect those example URLs
    were tested only on Windows.  I have made it a bit more explicit so
    we do not fall into this trap again.
    
    Closes #5880

--- datalad/core/distributed/clone.py
+++ datalad/core/distributed/clone.py
@@ -48,13 +48,14 @@ from datalad.support.constraints import (
 from datalad.support.exceptions import DownloadError
 from datalad.support.param import Parameter
 from datalad.support.network import (
-    get_local_file_url,
-    download_url,
-    is_url,
-    URL,
-    RI,
     DataLadRI,
     PathRI,
+    RI,
+    SSHRI,
+    URL,
+    download_url,
+    get_local_file_url,
+    is_url,
 )
 from datalad.dochelpers import (
     exc_str,
@@ -1206,7 +1207,7 @@ def _get_installationpath_from_url(url):
     from a URL, analog to what `git clone` does.
     """
     ri = RI(url)
-    if isinstance(ri, (URL, DataLadRI)):  # decode only if URL
+    if isinstance(ri, (URL, DataLadRI, SSHRI)):  # decode only if URL
         path = ri.path.rstrip('/')
         path = urlunquote(path) if path else ri.hostname
         if '/' in path:
