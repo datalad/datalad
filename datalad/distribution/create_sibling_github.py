1dfbbabb5a2b7fbd481e202512ae92734040cc57
Michael Hanke <michael.hanke@gmail.com>
2021-09-01 15:12:08 +0200

    RF: Reimplement create-sibling-github analog to create-sibling-gogs

--- datalad/distribution/create_sibling_github.py
+++ datalad/distribution/create_sibling_github.py
@@ -13,40 +13,93 @@ __docformat__ = 'restructuredtext'
 
 
 import logging
-import re
+import requests
 import warnings
 
 from datalad.interface.base import (
     build_doc,
     Interface,
 )
-from datalad.interface.common_opts import (
-    recursion_flag,
-    recursion_limit,
-    publish_depends,
-)
-from datalad.interface.results import get_status_dict
 from datalad.interface.utils import eval_results
 from datalad.support.param import Parameter
 from datalad.support.constraints import (
-    EnsureChoice,
     EnsureNone,
     EnsureStr,
 )
 from datalad.distribution.dataset import (
     datasetmethod,
-    EnsureDataset,
-    require_dataset,
 )
-from datalad.distribution.siblings import Siblings
+from datalad.distributed.create_sibling_ghlike import (
+    _GitHubLike,
+    _create_sibling,
+)
 
 lgr = logging.getLogger('datalad.distribution.create_sibling_github')
 
 
-def normalize_reponame(path):
-    """Turn name (e.g. path) into a Github compliant repository name
-    """
-    return re.sub(r'\s+', '_', re.sub(r'[/\\]+', '-', path))
+class _GitHub(_GitHubLike):
+    name = 'github'
+    fullname = 'GitHub'
+    response_code_unauthorized = 401
+    create_org_repo_endpoint = 'orgs/{organization}/repos'
+    create_user_repo_endpoint = 'user/repos'
+    get_authenticated_user_endpoint = 'user'
+    get_repo_info_endpoint = 'repos/{user}/{repo}'
+    extra_remote_settings = {
+        # first make sure that annex doesn't touch this one
+        # but respect any existing config
+        'annex-ignore': 'true',
+        # first push should separately push active branch first
+        # to overcome github issue of choosing "default" branch
+        # alphabetically if its name does not match the default
+        # branch for the user (or organization) which now defaults
+        # to "main"
+        'datalad-push-default-first': 'true'
+    }
+
+    def repo_create_response(self, r):
+        """
+        At present the only difference from the GHlike implementation
+        is the detection of an already existing repo in a 422 response.
+        """
+        try:
+            response = r.json()
+        except Exception as e:
+            lgr.debug('Cannot get JSON payload of %s [%s]' , r, e)
+            response = {}
+        lgr.debug('%s responded with %s %s', self.fullname, r, response)
+        if r.status_code == requests.codes.created:
+            return dict(
+                status='ok',
+                preexisted=False,
+                # perform some normalization
+                reponame=response.get('name'),
+                private=response.get('private'),
+                clone_url=response.get('clone_url'),
+                ssh_url=response.get('ssh_url'),
+                html_url=response.get('html_url'),
+                # and also return in full
+                host_response=response,
+            )
+        elif r.status_code == requests.codes.unprocessable and \
+                any('already exist' in e.get('message', '')
+                    for e in response.get('errors', [])):
+            return dict(
+                status='impossible',
+                message='repository already exists',
+                preexisted=True,
+            )
+        elif r.status_code == self.response_code_unauthorized:
+            return dict(
+                status='error',
+                message=('unauthorized: %s', response.get('message')),
+            )
+        # make sure any error-like situation causes noise
+        r.raise_for_status()
+        # catch-all
+        raise RuntimeError(f'Unexpected host response: {response}')
+
+
 
 
 @build_doc
@@ -80,41 +133,9 @@ class CreateSiblingGithub(Interface):
     with that specific login name.
     """
 
-    _params_ = dict(
-        dataset=Parameter(
-            args=("--dataset", "-d",),
-            doc="""specify the dataset to create the publication target for. If
-                no dataset is given, an attempt is made to identify the dataset
-                based on the current working directory""",
-            constraints=EnsureDataset() | EnsureNone()),
-        reponame=Parameter(
-            args=('reponame',),
-            metavar='REPONAME',
-            doc="""GitHub repository name. When operating recursively,
-            a suffix will be appended to this name for each subdataset""",
-            constraints=EnsureStr()),
-        recursive=recursion_flag,
-        recursion_limit=recursion_limit,
-        name=Parameter(
-            args=('-s', '--name',),
-            metavar='NAME',
-            doc="""name to represent the GitHub repository in the local
-            dataset installation""",
-            constraints=EnsureStr()),
-        existing=Parameter(
-            args=("--existing",),
-            constraints=EnsureChoice('skip', 'error', 'reconfigure', 'replace'),
-            metavar='MODE',
-            doc="""desired behavior when already existing or configured
-            siblings are discovered. In this case, a dataset can be skipped
-            ('skip'), the sibling configuration be updated ('reconfigure'),
-            or process interrupts with error ('error'). DANGER ZONE: If 'replace'
-            is used, an existing github repository will be irreversibly removed,
-            re-initialized, and the sibling (re-)configured (thus implies 'reconfigure').
-            `replace` could lead to data loss, so use with care.  To minimize
-            possibility of data loss, in interactive mode DataLad will ask for
-            confirmation, but it would raise an exception in non-interactive mode.
-            """,),
+    _params_ = _GitHub.create_sibling_params
+    # deprecated option
+    _params_.update(
         github_login=Parameter(
             args=('--github-login',),
             constraints=EnsureStr() | EnsureNone(),
@@ -127,26 +148,6 @@ class CreateSiblingGithub(Interface):
             doc="""If provided, the repository will be created under this
             GitHub organization. The respective GitHub user needs appropriate
             permissions."""),
-        access_protocol=Parameter(
-            args=("--access-protocol",),
-            constraints=EnsureChoice('https', 'ssh'),
-            doc="""Which access protocol/URL to configure for the sibling"""),
-        publish_depends=publish_depends,
-        private=Parameter(
-            args=("--private",),
-            action="store_true",
-            default=False,
-            doc="""If this flag is set, the repository created on github
-            will be marked as private and only visible to those granted 
-            access or by membership of a team/organization/etc.
-            """),
-        dry_run=Parameter(
-            args=("--dry-run",),
-            action="store_true",
-            doc="""If this flag is set, no repositories will be created.
-            Instead tests for name collisions with existing projects will be
-            performed, and would-be repository names are reported for all
-            relevant datasets"""),
         dryrun=Parameter(
             args=("--dryrun",),
             action="store_true",
@@ -165,12 +166,14 @@ class CreateSiblingGithub(Interface):
             name='github',
             existing='error',
             github_login=None,
+            auth=None,
             github_organization=None,
             access_protocol='https',
             publish_depends=None,
             private=False,
             dryrun=False,
-            dry_run=False):
+            dry_run=False,
+            api='https://api.github.com'):
         if dryrun and not dry_run:
             # the old one is used, and not in agreement with the new one
             warnings.warn(
@@ -180,98 +183,22 @@ class CreateSiblingGithub(Interface):
                 DeprecationWarning)
             dry_run = dryrun
 
-        # this is an absolute leaf package, import locally to avoid
-        # unnecessary dependencies
-        from datalad.support.github_ import _make_github_repos_
+        # TODO robustify the deprecation mappings
+        auth = auth or github_login
 
-        if reponame != normalize_reponame(reponame):
-            raise ValueError('Invalid name for a GitHub project: {}'.format(
-                reponame))
+        if github_organization:
+            reponame = f'{github_organization}/{reponame}'
 
-        # what to operate on
-        ds = require_dataset(
-            dataset, check_installed=True, purpose='create GitHub sibling(s)')
-
-        res_kwargs = dict(
-            action='create_sibling_github [dry-run]' if dry_run else
-            'create_sibling_github',
-            logger=lgr,
-            refds=ds.path,
+        yield from _create_sibling(
+            platform=_GitHub(api, auth=auth),
+            reponame=reponame,
+            dataset=dataset,
+            recursive=recursive,
+            recursion_limit=recursion_limit,
+            name=name,
+            existing=existing,
+            access_protocol=access_protocol,
+            publish_depends=publish_depends,
+            private=private,
+            dry_run=dry_run,
         )
-        # gather datasets and essential info
-        # dataset instance and mountpoint relative to the top
-        toprocess = [ds]
-        if recursive:
-            for sub in ds.subdatasets(
-                    fulfilled=None,  # we want to report on missing dataset in here
-                    recursive=recursive,
-                    recursion_limit=recursion_limit,
-                    result_xfm='datasets'):
-                if not sub.is_installed():
-                    lgr.info('Ignoring unavailable subdataset %s', sub)
-                    continue
-                toprocess.append(sub)
-
-        # check for existing remote configuration
-        filtered = []
-        for d in toprocess:
-            if name in d.repo.get_remotes():
-                yield get_status_dict(
-                    ds=d,
-                    status='error' if existing == 'error' else 'notneeded',
-                    message=('already has a configured sibling "%s"', name),
-                    **res_kwargs)
-                continue
-            gh_reponame = reponame if d == ds else \
-                '{}-{}'.format(
-                    reponame,
-                    normalize_reponame(str(d.pathobj.relative_to(ds.pathobj))))
-            filtered.append((d, gh_reponame))
-
-        if not filtered:
-            # all skipped
-            return
-
-        # actually make it happen on GitHub
-        for res in _make_github_repos_(
-                github_login, github_organization, filtered,
-                existing, access_protocol, private, dry_run):
-            # blend reported results with standard properties
-            res = dict(
-                res,
-                **res_kwargs)
-            if 'message' not in res:
-                res['message'] = ("Dataset sibling '%s', project at %s", name, res['url'])
-            # report to caller
-            yield get_status_dict(**res)
-            if res['status'] not in ('ok', 'notneeded'):
-                # something went wrong, do not proceed
-                continue
-            # lastly configure the local datasets
-            if not dry_run:
-                extra_remote_vars = {
-                    # first make sure that annex doesn't touch this one
-                    # but respect any existing config
-                    'annex-ignore': 'true',
-                    # first push should separately push active branch first
-                    # to overcome github issue of choosing "default" branch
-                    # alphabetically if its name does not match the default
-                    # branch for the user (or organization) which now defaults
-                    # to "main"
-                    'datalad-push-default-first': 'true'
-                }
-                for var_name, var_value in extra_remote_vars.items():
-                    var = 'remote.{}.{}'.format(name, var_name)
-                    if var not in d.config:
-                        d.config.add(var, var_value, where='local')
-                yield from Siblings()(
-                    'configure',
-                    dataset=d,
-                    name=name,
-                    url=res['url'],
-                    recursive=False,
-                    # TODO fetch=True, maybe only if one existed already
-                    publish_depends=publish_depends,
-                    result_renderer='disabled')
-
-        # TODO let submodule URLs point to GitHub (optional)
