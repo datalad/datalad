c44869fafc6e9c11a9e5a252126c38956757e35f
Michael Hanke <michael.hanke@gmail.com>
2021-09-15 10:27:32 +0200

    Place import shim to ease create_sibling_github transition

---
+++ datalad/distribution/create_sibling_github.py
@@ -0,0 +1,17 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the datalad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""Import shim to ease the transition after the move under distributed/"""
+
+import warnings
+warnings.warn(
+    "CreateSiblingGithub has been moved to "
+    "datalad.distributed.create_sibling_github. Please adjust the import.",
+    DeprecationWarning)
+
+from datalad.distributed.create_sibling_github import CreateSiblingGithub
