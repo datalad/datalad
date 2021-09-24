0aa1f6aafa232e008a1b3d63a599e4e547999c79
Michael Hanke <michael.hanke@gmail.com>
2021-09-24 18:48:46 +0200

    Establish new main import module for Runner-related functionality

---
+++ datalad/runner/__init__.py
@@ -0,0 +1,20 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the datalad package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""DataLad command execution runner
+"""
+
+from .coreprotocols import (
+    KillOutput,
+    NoCapture,
+    StdErrCapture,
+    StdOutCapture,
+    StdOutErrCapture,
+)
+from .runner import WitlessRunner as Runner
+from .gitrunner import GitWitlessRunner as GitRunner
