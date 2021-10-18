6396ebd841081f88a02aedc0e67e170c1ed596b3
Michael Hanke <michael.hanke@gmail.com>
2021-10-18 08:31:47 +0200

    Report correct HTTP URL for RIA store content
    These URL do not only have annecdotal value. In efforts like
    https://github.com/datalad/datalad/issues/828 incorrect URL invalidate
    the result.
    
    The fix is not pretty (due to https://github.com/datalad/datalad/issues/6089
    and similar issues), but it comes with a test that hopefully indicates
    breakage when these are fixed.

--- datalad/distributed/tests/test_ora_http.py
+++ datalad/distributed/tests/test_ora_http.py
@@ -10,6 +10,7 @@ from datalad.tests.utils import (
     assert_raises,
     assert_repo_status,
     assert_result_count,
+    assert_status,
     known_failure_windows,
     serve_path_via_http,
     skip_if_adjusted_branch,
@@ -144,4 +145,9 @@ def test_read_access(store_path, store_url, ds_path):
     assert_equal(len(res), 2)
     assert_result_count(res, 2, status='ok', type='file', action='get',
                         message="from ora-remote...")
+
+    # try whether the reported access URL is correct
+    one_url = ds.repo.whereis('one.txt', output='full'
+        )[store_uuid]['urls'].pop()
+    assert_status('ok', ds.download_url(urls=[one_url], path=str(ds.pathobj / 'dummy')))
 
