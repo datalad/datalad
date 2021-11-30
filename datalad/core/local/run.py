22bf7fa8e01e76f626074eb67c748385ad9140f4
Sin Kim <AKSoo@users.noreply.github.com>
2021-11-30 18:05:47 +0900

    Update datalad/core/local/run.py
    Co-authored-by: Adina Wagner <adina.wagner@t-online.de>

--- datalad/core/local/run.py
+++ datalad/core/local/run.py
@@ -175,7 +175,7 @@ class Run(Interface):
              datalad run -m 'run my script' -i 'data/*' \\
              -i 'datafile.txt' -o 'output_dir/*' -o \\
              'outfile.txt' 'code/script.sh'"""),
-        dict(test="Use ** to match any file at any directory depth recursively. "
+        dict(text="Use ** to match any file at any directory depth recursively. "
                   "Single * does not check files within matched directories.",
              code_py="""\
              run(cmd='code/script.sh',
