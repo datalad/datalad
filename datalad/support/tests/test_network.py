bd0e104de3b878ca9ea0b55db8d22b0557753f5e
Yaroslav Halchenko <debian@onerussian.com>
2022-01-21 16:29:36 -0500

    RF: remove no longer needed conditionals for pythons before 3.7

--- datalad/support/tests/test_network.py
+++ datalad/support/tests/test_network.py
@@ -466,16 +466,8 @@ def test_get_local_file_url():
                 #('/a b/', 'file:///a%20b/'),
                 ('/a b/name', 'file:///a%20b/name'),
             ):
-        try:
-            # Yarik found no better way to trigger.  .decode() isn't enough
-            print("D: %s" % path)
-        except UnicodeEncodeError:
-            if sys.version_info < (3, 7):
-                # observed test failing on ubuntu 18.04 with python 3.6
-                # (reproduced in conda env locally with python 3.6.10 when LANG=C
-                # We will just skip this tricky one
-                continue
-            raise
+        # Yarik found no better way to trigger.  .decode() isn't enough
+        print("D: %s" % path)
         if isabs(path):
             eq_(get_local_file_url(path), url)
         else:
