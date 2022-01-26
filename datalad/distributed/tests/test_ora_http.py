datalad/datalad

commit a4b87f0c58047c9337e0cd1604f2b3eaeaae99a7

RF: Make ORA's HTTP backend not a special case
`HTTPRemoteIO` now behaves largely just like SSH and local access,
except for being read-only. Remove special casing throughout
`RIARemote`. This means, that RIA stores are validated by the ORA
special remote over HTTP the same way as over SSH or local access.
Therefore providing the ria-layout-version files is now mandatory for
HTTP stores as well.

This patch should streamline things a bit and closes #5451. Thus making
the reconfiguration business in `datalad clone` consistent across
protocols. Furthermore, since the version files are now requested and
used to construct the URLs, we can simplify server-side scripts dealing
wiht archives, etc. There's also hope that this way we can allow for a
version file to define a layout directly, instead of declaring a label
that needs to be known to released datalad, because all the version is
about is locations associated with the .git, annex/objects and archives
of datasets in the store. In theory these locations could be declared in
a JSON-record or alike in that version file, allowing store maintainers
to define their thing freely.

datalad/distributed/ora_remote.py
@@ -630,13 +630,12 @@ class HTTPRemoteIO(object):
     # NOTE: For now read-only. Not sure yet whether an IO class is the right
     # approach.
 
-    def __init__(self, ria_url, dsid, buffer_size=DEFAULT_BUFFER_SIZE):
-        assert ria_url.startswith("ria+http")
-        self.base_url = ria_url[4:]
-        if self.base_url[-1] == '/':
-            self.base_url = self.base_url[:-1]
+    def __init__(self, url, buffer_size=DEFAULT_BUFFER_SIZE):
+        if not url.startswith("http"):
+            raise RIARemoteError("Expected HTTP URL, but got {}".format(url))
 
-        self.base_url += "/" + dsid[:3] + '/' + dsid[3:]
+        self.store_url = url.rstrip('/')
+
         # make sure default is used when None was passed, too.
         self.buffer_size = buffer_size if buffer_size else DEFAULT_BUFFER_SIZE
 
@@ -644,17 +643,32 @@ class HTTPRemoteIO(object):
         # Note, that we need the path with hash dirs, since we don't have access
         # to annexremote.dirhash from within IO classes
 
-        url = self.base_url + "/annex/objects/" + str(key_path)
-        response = requests.head(url, allow_redirects=True)
-        return response.status_code == 200
+        return self.exists(key_path)
 
     def get(self, key_path, filename, progress_cb):
         # Note, that we need the path with hash dirs, since we don't have access
         # to annexremote.dirhash from within IO classes
 
-        url = self.base_url + "/annex/objects/" + str(key_path)
+        url = self.store_url + str(key_path)
         from datalad.support.network import download_url
         download_url(url, filename, overwrite=True)
+
+    def exists(self, path):
+        # use same signature as in SSH and Local IO, although validity is
+        # limited in case of HTTP.
+        url = self.store_url + path.as_posix()
+        try:
+            response = requests.head(url, allow_redirects=True)
+        except Exception as e:
+            raise RIARemoteError(str(e))
+
+        return response.status_code == 200
+
+    def read_file(self, file_path):
+
+        from datalad.support.network import download_url
+        content = download_url(self.store_url + file_path.as_posix())
+        return content
 
 
 def handle_errors(func):
@@ -1041,12 +1055,7 @@ class RIARemote(SpecialRemote):
                 # fall back on the UUID for the annex remote
                 self.archive_id = self.annex.getuuid()
 
-        if not isinstance(self.io, HTTPRemoteIO):
-            self.get_store()
-
-        # else:
-        # TODO: consistency with SSH and FILE behavior? In those cases we make
-        #       sure the store exists from within initremote
+        self.get_store()
 
         self.annex.setconfig('archive-id', self.archive_id)
         # make sure, we store the potentially rewritten URL
@@ -1103,9 +1112,18 @@ class RIARemote(SpecialRemote):
             if self._local_io():
                 self._io = LocalIO()
             elif self.ria_store_url.startswith("ria+http"):
-                self._io = HTTPRemoteIO(self.ria_store_url,
-                                        self.archive_id,
-                                        self.buffer_size)
+                # TODO: That construction of "http(s)://host/" should probably
+                #       be moved, so that we get that when we determine
+                #       self.storage_host. In other words: Get the parsed URL
+                #       instead and let HTTPRemoteIO + SSHRemoteIO deal with it
+                #       uniformly. Also: Don't forget about a possible port.
+
+                url_parts = self.ria_store_url[4:].split('/')
+                # we expect parts: ("http(s):", "", host:port, path)
+                self._io = HTTPRemoteIO(
+                    url_parts[0] + "//" + url_parts[2],
+                    self.buffer_size
+                )
             elif self.storage_host:
                 self._io = SSHRemoteIO(self.storage_host, self.buffer_size)
                 from atexit import register
@@ -1178,8 +1196,7 @@ class RIARemote(SpecialRemote):
         self.uuid = self.annex.getuuid()
         self._verify_config(gitdir)
 
-        if not isinstance(self.io, HTTPRemoteIO):
-            self.get_store()
+        self.get_store()
 
         # report active special remote configuration/status
         self.info = {
@@ -1235,12 +1252,6 @@ class RIARemote(SpecialRemote):
     @handle_errors
     def transfer_retrieve(self, key, filename):
 
-        if isinstance(self.io, HTTPRemoteIO):
-            self.io.get(PurePosixPath(self.annex.dirhash(key)) / key / key,
-                        filename,
-                        self.annex.progress)
-            return
-
         dsobj_dir, archive_path, key_path = self._get_obj_location(key)
         abs_key_path = dsobj_dir / key_path
         # sadly we have no idea what type of source gave checkpresent->true
@@ -1259,10 +1270,6 @@ class RIARemote(SpecialRemote):
 
     @handle_errors
     def checkpresent(self, key):
-
-        if isinstance(self.io, HTTPRemoteIO):
-            return self.io.checkpresent(
-                PurePosixPath(self.annex.dirhash(key)) / key / key)
 
         dsobj_dir, archive_path, key_path = self._get_obj_location(key)
         abs_key_path = dsobj_dir / key_path

datalad/distributed/tests/test_ora_http.py
@@ -48,6 +48,20 @@ def test_initremote(store_path, store_url, ds_path):
     url = "ria+" + store_url
     init_opts = common_init_opts + ['url={}'.format(url)]
 
+    # fail when there's no RIA store at the destination
+    assert_raises(CommandError, ds.repo.init_remote, 'ora-remote',
+                  options=init_opts)
+    # Doesn't actually create a remote if it fails
+    assert_not_in('ora-remote',
+                  [cfg['name']
+                   for uuid, cfg in ds.repo.get_special_remotes().items()]
+                  )
+
+    # now make it a store
+    io = LocalIO()
+    create_store(io, store_path, '1')
+    create_ds_in_store(io, store_path, ds.id, '2', '1')
+
     # fails on non-RIA URL
     assert_raises(CommandError, ds.repo.init_remote, 'ora-remote',
                   options=common_init_opts + ['url={}'

commit 6d88a57573a9633bc700dce6a7c62486287dd8b3

BF: ORA over HTTP tried to check archive
Commit a4b87f0c58047c9337e0cd1604f2b3eaeaae99a7 broke this
by removing to much. CHECKPRESENT and TRANSFER-RETRIEVE would
try to address a potential archive if key couldn't be
found in annex object tree.

This is wrong. In opposition to remote execution variants
(SSH and local access), HTTP is supposed to only send one request
per key - archives have to be dealt with server-side.

Also adjust the ORA HTTP test to actually look into the results
of `fsck`, since a failure of hte special remote would not lead to
the call to `fsck` failing itself, but reporting in "note" or
"error-messages". The issue woud have been caught that way.

(Closes #6354)

datalad/distributed/ora_remote.py
@@ -1286,6 +1286,12 @@ class RIARemote(SpecialRemote):
         try:
             self.io.get(abs_key_path, filename, self.annex.progress)
         except Exception as e1:
+            if isinstance(self.io, HTTPRemoteIO):
+                # no client-side archive access over HTTP
+                # Note: This is intentional, as it would mean one additional
+                # request per key. However, server response to the GET can
+                # consider archives on their end.
+                raise
             # catch anything and keep it around for a potential re-raise
             try:
                 self.io.get_from_archive(archive_path, key_path, filename,
@@ -1304,6 +1310,9 @@ class RIARemote(SpecialRemote):
         if self.io.exists(abs_key_path):
             # we have an actual file for this key
             return True
+        if isinstance(self.io, HTTPRemoteIO):
+            # no client-side archive access over HTTP
+            return False
         # do not make a careful check whether an archive exists, because at
         # present this requires an additional SSH call for remote operations
         # which may be rather slow. Instead just try to run 7z on it and let

datalad/distributed/tests/test_ora_http.py
@@ -115,7 +115,17 @@ def test_read_access(store_path, store_url, ds_path):
     create_store(io, store_path, '1')
     create_ds_in_store(io, store_path, ds.id, '2', '1')
     ds.repo.init_remote('ora-remote', options=init_opts)
-    ds.repo.fsck(remote='ora-remote', fast=True)
+    fsck_results = ds.repo.fsck(remote='ora-remote', fast=True)
+    # Note: Failures in the special remote will show up as a success=False
+    # result for fsck -> the call itself would not fail.
+    for r in fsck_results:
+        if "note" in r:
+            # we could simply assert "note" to not be in r, but we want proper
+            # error reporting - content of note, not just its unexpected
+            # existence.
+            assert_equal(r["success"], "true",
+                         msg="git-annex-fsck failed with ORA over HTTP: %s" % r)
+        assert_equal(r["error-messages"], [])
     store_uuid = ds.siblings(name='ora-remote',
                              return_type='item-or-list',
                              result_renderer='disabled')['annex-uuid']