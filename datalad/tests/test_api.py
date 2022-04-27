73d72fe05b06c598c9682904ae7b9bb7280f7970
John T. Wodder II <git@varonathe.org>
2022-04-27 16:10:51 -0400

    datalad.api.test is no more.

--- datalad/tests/test_api.py
+++ datalad/tests/test_api.py
@@ -27,7 +27,6 @@ def test_basic_setup():
 
     # random pick of something that should be there
     assert_true(hasattr(api, 'install'))
-    assert_true(hasattr(api, 'test'))
     assert_true(hasattr(api, 'create'))
     # make sure all helper utilities do not pollute the namespace
     # and we end up only with __...__ attributes
