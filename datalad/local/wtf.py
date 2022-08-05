e752294e66464a77d25f45afd47d4a3b25015f9d
Yaroslav Halchenko <debian@onerussian.com>
2022-08-04 20:40:37 -0400

    ENH: warn once if filesystem detail could not be determined
    Even if it would be too noisy on windows -- would still be better than
    just silently not working correctly (as it was due to a typo).
    
    And if psutil is not installed, we would get it logged only at debug level. But
    otherwise, if something abnormal -- at warning level and reported once
    (although could be different for different filesystems, but at least would
    alert to a possible next typo)

--- datalad/local/wtf.py
+++ datalad/local/wtf.py
@@ -146,7 +146,7 @@ def _describe_system():
     }
 
 
-def _get_fs_type(loc, path):
+def _get_fs_type(loc, path, _was_warned=[]):
     """Return file system info for given Paths. Provide pathlib path as input"""
     res = {'path': path}
     try:
@@ -169,6 +169,11 @@ def _get_fs_type(loc, path):
         ce = CapturedException(exc)
         # if an exception occurs, leave out the fs type. The result renderer can
         # display a hint based on its lack in the report
+        if not _was_warned:
+            (lgr.debug if isinstance(exc, ImportError) else lgr.warning) (
+                "Could not determine filesystem types due to %s", ce)
+            # Rely on side-effect of [] as default arg
+            _was_warned.append("warned")
     return res
 
 
