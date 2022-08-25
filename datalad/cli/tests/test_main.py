datalad/datalad

commit 0b4937de63c44509001cd9c64bbeb7b24176685c

Make CLI autocompletion snappy
It basically inspects the incomplete argument list now, and
based on that can make more clever decisions what to load.

There are more possibilities for making things faster. For
example, we could load the extensions one by one, while searching
for unknown commands. But already now it feels great.

This change should have no impact on the full help generation. But it
breaks the interal operations needed to build manpages and docs.
This needs fixing, but was always ugly anyways.

datalad/cli/parser.py
@@ -109,45 +109,83 @@ def setup_parser(
     # try to figure out whether the parser construction can be limited to
     # a single (sub)command -- don't even try to do this, when we are in
     # any of the doc-building capacities -- timing is not relevant there
-    target_subparser_name = single_subparser_possible(
+    status, parseinfo = single_subparser_possible(
         cmdlineargs,
         parser,
-        interface_groups,
         completing,
-    ) if not (return_subparsers or help_ignore_extensions) else None
+    )
 
-    if target_subparser_name is None:
-        # we cannot be lean, we must built the full parser
-        # and for that must load all extensions too, e.g., to
-        # be able to list extension command in --help output
-        # (in case of a help request, the extension loading will not
-        #  have happened yet in single_subparser_possible())
+    command_provider = 'core'
+
+    if status == 'help' and not help_ignore_extensions or (
+        status == 'subcommand' and parseinfo not in
+            get_commands_from_groups(interface_groups)):
+        # we know the command is not in the core package
+        # still a chance it could be in an extension
+        command_provider = 'extension'
+        # we need the full help, or we have a potential command that
+        # lives in an extension, must load all extension, expensive
         from .helpers import add_entrypoints_to_interface_groups
+        # need to load all the extensions and try again
+        # TODO load extensions one-by-one and stop when a command was found
         add_entrypoints_to_interface_groups(interface_groups)
 
-    # --help specification was delayed since it causes immediate printout of
-    # --help output before we setup --help for each command
-    parser_add_common_opt(parser, 'help')
+        if status == 'subcommand':
+            known_commands = get_commands_from_groups(interface_groups)
+            if parseinfo not in known_commands:
+                # certainly not possible to identify a single parser that
+                # could be constructed, but we can be helpful
+                # will sys.exit() unless we are completing
+                try_suggest_extension_with_command(
+                    parser, parseinfo, completing, known_commands)
+                # in completion mode we can get here, even for a command
+                # that does not exist at all!
+                command_provider = None
 
-    grp_short_descriptions = defaultdict(list)
-    # create subparser, use module suffix as cmd name
+    # TODO check if not needed elsewhere
+    if status == 'help' or completing  and status in ('allknown', 'unknownopt'):
+        # --help specification was delayed since it causes immediate
+        # printout of
+        # --help output before we setup --help for each command
+        parser_add_common_opt(parser, 'help')
+
     all_parsers = {}  # name: (sub)parser
-    subparsers = parser.add_subparsers()
-    for group_name, _, _interfaces \
-            in sorted(interface_groups, key=lambda x: x[1]):
-        for _intfspec in _interfaces:
-            cmd_name = get_cmdline_command_name(_intfspec)
-            if target_subparser_name and cmd_name != target_subparser_name:
-                continue
-            subparser = add_subparser(
-                _intfspec, subparsers, cmd_name, formatter_class, group_name,
-                grp_short_descriptions
-            )
-            if subparser:  # interface can fail to load
-                all_parsers[cmd_name] = subparser
+
+    if status == 'help' or status == 'subcommand':
+        # parseinfo could be None here, when we could not identify
+        # a subcommand, but need to locate matching ones for
+        # completion
+        grp_short_descriptions = defaultdict(list)
+        # create subparser, use module suffix as cmd name
+        subparsers = parser.add_subparsers()
+        for group_name, _, _interfaces \
+                in sorted(interface_groups, key=lambda x: x[1]):
+            for _intfspec in _interfaces:
+                cmd_name = get_cmdline_command_name(_intfspec)
+                if status != 'help':
+                    if command_provider and cmd_name != parseinfo:
+                        # a known command, but know what we are looking for
+                        continue
+                    if command_provider is None and not cmd_name.startswith(
+                            parseinfo):
+                        # an unknown command, and has no common prefix with
+                        # the current command candidate, not even good
+                        # for completion
+                        continue
+                subparser = add_subparser(
+                    _intfspec,
+                    subparsers,
+                    cmd_name,
+                    formatter_class,
+                    group_name,
+                    grp_short_descriptions
+                )
+                if subparser:  # interface can fail to load
+                    all_parsers[cmd_name] = subparser
 
     # create command summary
-    if '--help' in cmdlineargs or '--help-np' in cmdlineargs:
+    if not completing and status == 'help' and (
+            '--help' in cmdlineargs or '--help-np' in cmdlineargs):
         from .helpers import get_description_with_cmd_summary
         parser.description = get_description_with_cmd_summary(
             grp_short_descriptions,
@@ -270,15 +308,11 @@ def _amend_action_args_for_parameter_constraint(param, args, help):
                 if isinstance(p, str))
 
 
-def single_subparser_possible(cmdlineargs, parser, interface_groups,
-                              completing):
+def single_subparser_possible(cmdlineargs, parser, completing):
     """Performs early analysis of the cmdline
 
     Looks at the first unparsed argument and if a known command,
     would return only that one.
-
-    For the analysis to be complete etc, would also load commands from
-    entrypoints
 
     When a plain command invocation with `--version` is detected, it will be
     acted on directly (until sys.exit(0) to avoid wasting time on unnecessary
@@ -286,7 +320,14 @@ def single_subparser_possible(cmdlineargs, parser, interface_groups,
 
     Returns
     -------
-    None or str
+    {'error', 'allknown', 'help', 'unknownopt', 'subcommand'}, None or str
+        Returns a status label and a parameter for this status.
+        'error': parsing failed, 'allknown': the parser successfully
+        identified all arguments, 'help': a help request option was found,
+        'unknownopt': an unknown or incomplete option was found,
+        'subcommand': a potential subcommand name was found. For the latter
+        two modes the second return value is the option or command name.
+        For all other modes the second return value is None.
     """
     # Before doing anything additional and possibly expensive see may be that
     # we have got the command already
@@ -311,11 +352,11 @@ def single_subparser_possible(cmdlineargs, parser, interface_groups,
         from datalad.support.exceptions import CapturedException
         ce = CapturedException(exc)
         lgr.debug("Early parsing failed with %s", ce)
-        return
+        return 'error', None
 
     if not unparsed_args:
         # cannot possibly be a subcommand
-        return
+        return 'allknown', None
 
     unparsed_arg = unparsed_args[0]
 
@@ -325,30 +366,17 @@ def single_subparser_possible(cmdlineargs, parser, interface_groups,
     if unparsed_arg in ('--help', '--help-np', '-h'):
         # not need to try to tune things, all these will result in everything
         # to be imported and parsed
-        return
-    elif not completing and unparsed_arg.startswith('-'):  # unknown option
+        return 'help', None
+    elif unparsed_arg.startswith('-'):  # unknown or incomplete option
+        if completing:
+            return 'unknownopt', unparsed_arg
         # will sys.exit
         fail_with_short_help(parser,
                              msg=f"unrecognized argument {unparsed_arg}",
                              # matches exit code of InsufficientArgumentsError
                              exit_code=2)
-    else:  # the command to handle
-        cmd = unparsed_arg
-        known_commands = get_commands_from_groups(interface_groups)
-        if cmd not in known_commands:
-            from .helpers import add_entrypoints_to_interface_groups
-            # need to load all the extensions and try again
-            add_entrypoints_to_interface_groups(interface_groups)
-            known_commands = get_commands_from_groups(interface_groups)
-
-        if cmd not in known_commands:
-            # certainly not possible to identify a single parser that
-            # could be constructed, but we can be helpful
-            # will sys.exit() unless we are completing
-            try_suggest_extension_with_command(
-                parser, cmd, completing, known_commands)
-        else:
-            return cmd
+    else:  # potential command to handle
+        return 'subcommand', unparsed_arg
 
 
 def try_suggest_extension_with_command(parser, cmd, completing, known_cmds):

datalad/cli/main.py
@@ -62,6 +62,11 @@ def main(args=sys.argv):
     lgr.log(5, "Starting main(%r)", args)
     # record that we came in via the cmdline
     datalad.__api = 'cmdline'
+    completing = "_ARGCOMPLETE" in os.environ
+    if completing:
+        import shlex
+        # TODO support posix=False too?
+        args = shlex.split(os.environ.get('COMP_LINE')) or args
 
     if _on_msys_tainted_paths():
         # Possibly present DataLadRIs were stripped of a leading /
@@ -71,7 +76,7 @@ def main(args=sys.argv):
     # PYTHON_ARGCOMPLETE_OK
     # TODO possibly construct a dedicated parser just for autocompletion
     # rather than lobotomizing the normal one
-    parser = setup_parser(args, completing="_ARGCOMPLETE" in os.environ)
+    parser = setup_parser(args, completing=completing)
     try:
         import argcomplete
         argcomplete.autocomplete(parser)

commit 9b2101051576fb6daf695ac6ef1714e9dbe8b4b8

BF: if early attempt to parse resulted in error, setup subparsers
Without that using a short option for a command which is also
present on top level results in early attempt error and then no
parsers even tried, thus giving that error from the main/top parser to the user

Fixes 6814

Regression was introduced in 0b4937de63c44509001cd9c64bbeb7b24176685c

datalad/cli/parser.py
@@ -165,7 +165,7 @@ def setup_parser(
     all_parsers = {}  # name: (sub)parser
 
     if (completing and status == 'allknown') or status \
-            in ('allparsers', 'subcommand'):
+            in ('allparsers', 'subcommand', 'error'):
         # parseinfo could be None here, when we could not identify
         # a subcommand, but need to locate matching ones for
         # completion

datalad/cli/tests/test_main.py
@@ -204,6 +204,15 @@ def test_combined_short_option():
     assert_in("too few arguments", stderr)
 
 
+# https://github.com/datalad/datalad/issues/6814
+@with_tempfile(mkdir=True)
+def test_conflicting_short_option(tempdir=None):
+    # datalad -f|--format   requires a value. regression made parser ignore command
+    # and its options
+    with chpwd(tempdir):  # can't just use -C tempdir since we do "in process" run_main
+        run_main(['create', '-f'])
+
+
 # apparently a bit different if following a good one so let's do both
 err_invalid = "error: (invalid|too few arguments|unrecognized argument)"
 err_insufficient = err_invalid  # "specify"