f2b4830855e37293cdf6781cd4ef514cb9c6d44b
Christian Monch <christian.moench@web.de>
2022-11-15 10:52:37 +0100

    do not use newline in test directory names
    This commit modifies the tests to use newline
    only in file names and not in directory names.
    To this end the function
    `get_most_obscure_supported_name` has been
    extended to distinguish between file names
    and directory names.

--- datalad/tests/utils_pytest.py
+++ datalad/tests/utils_pytest.py
@@ -1469,10 +1469,12 @@ if sys.getfilesystemencoding().lower() == 'utf-8':
         OBSCURE_FILENAME_PARTS.append(UNICODE_FILENAME)
 # space before extension, simple extension and trailing space to finish it up
 OBSCURE_FILENAME_PARTS += [' ', '.datc', ' ']
+# Parts that should not be used in directory names
+DIRECTORY_EXCLUDED_PARTS = ['\n']
 
 
 @with_tempfile(mkdir=True)
-def get_most_obscure_supported_name(tdir, return_candidates=False):
+def get_most_obscure_supported_name(tdir, return_candidates=False, directory_name=False):
     """Return the most obscure filename that the filesystem would support under TEMPDIR
 
     Parameters
@@ -1480,46 +1482,59 @@ def get_most_obscure_supported_name(tdir, return_candidates=False):
     return_candidates: bool, optional
       if True, return a tuple of (good, candidates) where candidates are "partially"
       sorted from trickiest considered
+
+    directory_name: bool, optional
+      if True, return an obscure directory name. Directories might have
+      different obscurity profiles. Currently, directory names will not
+      contain newline characters.
+
     TODO: we might want to use it as a function where we would provide tdir
     """
     # we need separate good_base so we do not breed leading/trailing spaces
     initial = good = OBSCURE_PREFIX
     system = platform.system()
 
-    OBSCURE_FILENAMES = []
+    obscure_filenames = []
     def good_filename(filename):
-        OBSCURE_FILENAMES.append(candidate)
+        obscure_filenames.append(candidate)
         try:
-            # Windows seems to not tollerate trailing spaces and
+            # Windows seems to not tolerate trailing spaces and
             # ATM we do not distinguish obscure filename and dirname.
             # So here we will test for both - being able to create dir
             # with obscure name and obscure filename under
-            os.mkdir(opj(tdir, filename))
-            with open(opj(tdir, filename, filename), 'w') as f:
-                f.write("TEST LOAD")
+            if directory_name is True:
+                os.mkdir(opj(tdir, filename))
+            else:
+                with open(opj(tdir, filename), 'w') as f:
+                    f.write("TEST LOAD")
             return True
         except:
-            lgr.debug("Filename %r is not supported on %s under %s",
-                      filename, system, tdir)
+            lgr.debug(
+                f"{'Filename' if directory_name is False else 'Dirname'} %r is "
+                "not supported on %s under %s",
+                filename, system, tdir)
             return False
 
     # incrementally build up the most obscure filename from parts
     for part in OBSCURE_FILENAME_PARTS:
+        if directory_name is True and part in DIRECTORY_EXCLUDED_PARTS:
+            continue
         candidate = good + part
         if good_filename(candidate):
             good = candidate
 
     if good == initial:
         raise RuntimeError("Could not create any of the files under %s among %s"
-                       % (tdir, OBSCURE_FILENAMES))
-    lgr.debug("Tested %d obscure filename candidates. The winner: %r", len(OBSCURE_FILENAMES), good)
+                       % (tdir, obscure_filenames))
+    lgr.debug("Tested %d obscure filename candidates. The winner: %r", len(obscure_filenames), good)
     if return_candidates:
-        return good, OBSCURE_FILENAMES[::-1]
+        return good, obscure_filenames[::-1]
     else:
         return good
 
 
 OBSCURE_FILENAME, OBSCURE_FILENAMES = get_most_obscure_supported_name(return_candidates=True)
+OBSCURE_DIRNAME, OBSCURE_DIRNAMES = get_most_obscure_supported_name(return_candidates=True, directory_name=True)
 
 
 @optional_args
@@ -1832,7 +1847,7 @@ def get_deeply_nested_structure(path):
     |      ├── subdir
     |      │   └── annexed_file.txt -> ../.git/annex/objects/...
     |      └── subds_lvl1_modified
-    |          └── OBSCURE_FILENAME_directory_untracked
+    |          └── OBSCURE_DIRNAME_directory_untracked
     |              └── untracked_file
 
     When a system has no symlink support, the link2... components are not
@@ -1859,7 +1874,7 @@ def get_deeply_nested_structure(path):
     )
     create_tree(
         str(ds.pathobj / 'subds_modified' / 'subds_lvl1_modified'),
-        {OBSCURE_FILENAME + u'_directory_untracked': {"untracked_file": ""}}
+        {OBSCURE_DIRNAME + u'_directory_untracked': {"untracked_file": ""}}
     )
     (ut.Path(subds.path) / 'subdir').mkdir()
     (ut.Path(subds.path) / 'subdir' / 'annexed_file.txt').write_text(u'dummy')
