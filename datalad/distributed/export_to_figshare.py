670f33f5e8040d05993a8057a9b481c71ca5ed48
Adina Wagner <adina.wagner@t-online.de>
2022-11-17 13:29:35 +0100

    MNT: Fix internal add_archive_content call
    the annex parameter is deprecated, in its place we can use the
    dataset parameter.

--- datalad/distributed/export_to_figshare.py
+++ datalad/distributed/export_to_figshare.py
@@ -354,7 +354,7 @@ class ExportToFigshare(Interface):
             lgr.info("Registering links back for the content of the archive")
             add_archive_content(
                 fname,
-                annex=dataset.repo,
+                dataset=dataset,
                 delete_after=True,  # just remove extracted into a temp dir
                 allow_dirty=True,  # since we have a tarball
                 commit=False  # we do not want to commit anything we have done here
