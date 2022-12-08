5d6dffcd45d297a6b1dde334864d0fc10d5b5b2f
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2022-12-08 14:04:30 +0100

    Don't be scary when failing to annex-enable
    `datalad-siblings` would unconditionally issue an INFO level or even
    WARNING level log, whenever `git annex enableremote` returned non-zero.
    Those messages would also say, that this may be expected with a pure git
    remote or an empty repository on the remote end.
    
    The main issue here is, that datalad users may not even have any
    expectation and is left with a message that suggests they should. This
    is confusing. At best we are training users to ignore our output.
    
    Another aspect to this is, that when used as an internal command (most
    notably from within `create-sibling-*`), _we_ are the ones calling and
    having an expectation. The user didn't even call `siblings configure`
    let alone `git annex enableremote`. At the same time, with
    `create-sibling-*` we know that there is an empty repo at the remote
    end - we just created it. The only reason to "probe" via `enableremote`
    is to figure whether the remote end *in principle* is usable with annex.
    In that case, `enableremote` will still fail, but it will leave the
    `annex-ignore` setting untouched.
    
    Hence, inform the user only, we the failure was consequential in that
    `annex-ignore=true` was actually set.
    Note, that `CapturedException` logs the original error anyway (level 8
    by default).

--- datalad/distribution/siblings.py
+++ datalad/distribution/siblings.py
@@ -562,19 +562,18 @@ def _configure_remote(
                         store=False):
                     repo.enable_remote(name)
             except (CommandError, DownloadError) as exc:
-                ce = CapturedException(exc)
-                # TODO yield
-                # this is unlikely to ever happen, now done for AnnexRepo
-                # instances only
                 # Note: CommandError happens with git-annex
                 # 6.20180416+gitg86b18966f-1~ndall+1 (prior 6.20180510, from
                 # which starts to fail with AccessFailedError) if URL is bogus,
                 # so enableremote fails. E.g. as "tested" in test_siblings
-                lgr.info(
-                    "Could not enable annex remote %s. This is expected if %s "
-                    "is a pure Git remote, or happens if it is not accessible.",
-                    name, name)
-                lgr.debug("Exception was: %s", ce)
+                # TODO yield
+                ce = CapturedException(exc)
+                repo.config.reload()
+                if repo.is_remote_annex_ignored(name):
+                    # Only inform user about the failure, if it's actually
+                    # consequential, because annex decided to set
+                    # annex-ignore=true.
+                    lgr.info("Could not annex-enable %s: %s", name, exc.stderr)
 
             if as_common_datasrc:
                 # we need a fully configured remote here
@@ -709,17 +708,16 @@ def _query_remotes(ds, repo, name, known_remotes, get_annex_info=True,
                         info['annex-groupwanted'] = groupwanted
                 except CommandError as exc:
                     if 'cannot determine uuid' in exc.stderr:
-                        # not an annex (or no connection), would be marked as
-                        #  annex-ignore
-                        msg = "Could not detect whether %s carries an annex. " \
-                              "If %s is a pure Git remote, this is expected. " %\
-                              (remote, remote)
+                        ce = CapturedException(exc)
                         repo.config.reload()
                         if repo.is_remote_annex_ignored(remote):
-                            msg += "Remote was marked by annex as annex-ignore. " \
-                                   "Edit .git/config to reset if you think that was done by mistake due to absent connection etc"
-                        lgr.warning(msg)
-                        info['annex-ignore'] = True
+                            lgr.warning(
+                                "%s was marked by git-annex as annex-ignore."
+                                "Edit .git/config to reset if you think that "
+                                "was done by mistake due to absent "
+                                "connection etc.",
+                                remote)
+                            info['annex-ignore'] = True
                     else:
                         raise
             else:
