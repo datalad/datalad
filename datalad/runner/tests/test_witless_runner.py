e15e69c8ea5fa4e90afdc2418e8e740280889328
Christian Monch <christian.moench@web.de>
2023-06-26 09:48:13 +0200

    add unit test for Runner._get_adjusted_env
    This commit adds a unit test for
    Runner._get_adjusted_env to ensure the
    correct copy-behavior, i.e. copy an env
    only if `copy` is `True`.

--- datalad/runner/tests/test_witless_runner.py
+++ datalad/runner/tests/test_witless_runner.py
@@ -336,6 +336,33 @@ def test_path_to_str_conversion() -> None:
     assert str(test_path) == adjusted_env['PWD']
 
 
+def test_env_copying() -> None:
+    # Regression test to ensure environments are only copied
+    # if `copy=True` is given to `Runner._get_adjusted_env.`
+    # Test also for path adjustments, if not-`None` `pwd`-value
+    # is given to `Runner._get_adjusted_env`.
+    runner = Runner()
+    for original_env in (None, dict(some_key='value')):
+        for cwd in (None, Path('a/b/c')):
+            for do_copy in (True, False):
+                adjusted_env = runner._get_adjusted_env(
+                    cwd=cwd,
+                    env=original_env,
+                    copy=do_copy
+                )
+                if original_env is None:
+                    assert adjusted_env is None
+                else:
+                    if do_copy is True:
+                        assert adjusted_env is not original_env
+                    else:
+                        assert adjusted_env is original_env
+                    if cwd is None:
+                        assert 'PWD' not in adjusted_env
+                    else:
+                        assert 'PWD' in adjusted_env
+
+
 @with_tempfile(mkdir=True)
 def test_environment(temp_dir_path: str = "") -> None:
     # Ensure that the subprocess sees a string in `$PWD`, even if a Path-object
